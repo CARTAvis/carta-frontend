e8277e7eeecc68888da027d55c6ea0178a436061
veggiesaurus <accomrie@gmail.com>
2018-07-10 10:07:20 +0200

    switched to static lib for ZFP

--- wasm_libs/build_zfp.sh
+++ wasm_libs/build_zfp.sh
@@ -9,13 +9,15 @@ fi
 mkdir -p zfp; tar -xf zfp-0.5.3.tar.gz --directory ./zfp --strip-components=1
 
 cd zfp
+echo "Patching for static build"
+patch src/CMakeLists.txt -i ../zfp_static_build.patch -N
 mkdir -p build
 cd build
 echo "Building ZFP using Emscripten"
 CC=emcc CXX=emcc cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_FLAGS="-s WASM=1" -DZFP_WITH_OPENMP=OFF -DBUILD_UTILITIES=OFF -DBUILD_TESTING=OFF ../
 make -j4
-echo "Checking for ZFP shared lib..."
-if [[ $(find -L ./lib/libzfp.so -type f -size +192000c 2>/dev/null) ]]; then
+echo "Checking for ZFP static lib..."
+if [[ $(find -L ./lib/libzfp.a -type f -size +192000c 2>/dev/null) ]]; then
     echo "Found"
 else
     echo "Not found!"
