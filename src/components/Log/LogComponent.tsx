0029f228cf21416db5ceef9352b9114e5b235877
veggiesaurus <accomrie@gmail.com>
2018-07-12 23:41:59 +0200

    refined log component styling

--- src/components/Log/LogComponent.tsx
+++ src/components/Log/LogComponent.tsx
@@ -26,6 +26,30 @@ export class LogComponent extends React.Component<{ logState: LogState }> {
         return (this.props.logState.hiddenTags.indexOf(tag) !== -1);
     };
 
+    private colorFromSeverity(severity: CARTA.ErrorSeverity): string {
+        switch (severity) {
+            case CARTA.ErrorSeverity.WARNING:
+                return Colors.ORANGE3;
+            case CARTA.ErrorSeverity.ERROR:
+            case CARTA.ErrorSeverity.CRITICAL:
+                return Colors.RED3;
+            default:
+                return Colors.GRAY1;
+        }
+    }
+
+    private intentFromSeverity(severity: CARTA.ErrorSeverity): Intent {
+        switch (severity) {
+            case CARTA.ErrorSeverity.WARNING:
+                return "warning";
+            case CARTA.ErrorSeverity.ERROR:
+            case CARTA.ErrorSeverity.CRITICAL:
+                return "danger";
+            default:
+                return "none";
+        }
+    }
+
     render() {
         const logState = this.props.logState;
         const entries = logState.logEntries;
@@ -40,28 +64,12 @@ export class LogComponent extends React.Component<{ logState: LogState }> {
                 continue;
             }
 
-            let intent: Intent;
-            let color: string;
-            switch (entry.level) {
-                case CARTA.ErrorSeverity.WARNING:
-                    intent = "warning";
-                    color = Colors.ORANGE4;
-                    break;
-                case CARTA.ErrorSeverity.ERROR:
-                case CARTA.ErrorSeverity.CRITICAL:
-                    intent = "danger";
-                    color = Colors.RED4;
-                    break;
-                default:
-                    intent = "none";
-            }
-
             let entryTagSpans = [];
             let visibleTags = entry.tags.filter(v => hiddenTags.indexOf(v) === -1);
             for (let j = 0; j < entry.tags.length; j++) {
                 const tag = entry.tags[j];
 
-                entryTagSpans.push(<Tag key={j} intent={intent}>{entry.tags[j]}</Tag>);
+                entryTagSpans.push(<Tag key={j} intent={this.intentFromSeverity(entry.level)}>{entry.tags[j]}</Tag>);
                 if (tagList.indexOf(tag) === -1) {
                     tagList.push(tag);
                 }
@@ -70,41 +78,52 @@ export class LogComponent extends React.Component<{ logState: LogState }> {
                 entryElements.push(
                     <div key={i}>
                         {entryTagSpans}
-                        <Code style={{color: color}}>{entry.message}</Code>
+                        <Code style={{color: this.colorFromSeverity(entry.level)}}>{entry.message}</Code>
                     </div>
                 );
             }
         }
 
-        if (!entries.length) {
-            return <NonIdealState icon="application" title="No log entries"/>;
-        }
-
-        const allTagSpans = tagList.map((tag, index) => <Tag interactive={true} minimal={this.isTagHidden(tag)} intent={this.isTagHidden(tag) ? "none" : "primary"} key={index} onClick={() => this.onTagClicked(tag)}>{tag}</Tag>);
+        const allTagSpans = tagList.map((tag, index) => {
+            return (
+                <Tag
+                    interactive={true}
+                    minimal={this.isTagHidden(tag)}
+                    intent={this.isTagHidden(tag) ? "none" : "primary"}
+                    className={this.isTagHidden(tag) ? "tag-hidden" : ""}
+                    key={index}
+                    onClick={() => this.onTagClicked(tag)}
+                >
+                    {tag}
+                </Tag>
+            );
+        });
 
         return (
             <div className="log">
-                <ScrollToBottom className="log-entry-list" followButtonClassName="log-entry-follow">
-                    {entryElements}
-                </ScrollToBottom>
+                {entryElements.length ? (
+                    <ScrollToBottom className="log-entry-list" followButtonClassName="log-entry-follow">
+                        {entryElements}
+                    </ScrollToBottom>
+                ) : <NonIdealState className="log-entry-list" icon="application" title="No log entries"/>}
 
                 <div className="log-footer">
-                    <div className="tag-list">
-                        {allTagSpans.length > 0 &&
-                        allTagSpans
-                        }
-                    </div>
+                    <FormGroup inline={true} label="Log level:" className="log-level">
+                        <HTMLSelect value={logState.logLevel} onChange={this.onLogLevelChanged} style={{color: this.colorFromSeverity(logState.logLevel)}}>
+                            <option value={-1} style={{color: this.colorFromSeverity(-1)}}>Debug</option>
+                            <option value={CARTA.ErrorSeverity.INFO} style={{color: this.colorFromSeverity(CARTA.ErrorSeverity.INFO)}}>Info</option>
+                            <option value={CARTA.ErrorSeverity.WARNING} style={{color: this.colorFromSeverity(CARTA.ErrorSeverity.WARNING)}}>Warning</option>
+                            <option value={CARTA.ErrorSeverity.ERROR} style={{color: this.colorFromSeverity(CARTA.ErrorSeverity.ERROR)}}>Error</option>
+                            <option value={CARTA.ErrorSeverity.CRITICAL} style={{color: this.colorFromSeverity(CARTA.ErrorSeverity.CRITICAL)}}>Critical</option>
+                        </HTMLSelect>
+                    </FormGroup>
+                    <FormGroup inline={true} label="Visible tags:" className="tag-list">
+                        <div>
+                            {allTagSpans.length > 0 ? allTagSpans : "None"}
+                        </div>
+                    </FormGroup>
                     <div className="log-footer-right">
-                        <FormGroup inline={true} label="Log level">
-                            <HTMLSelect value={logState.logLevel} onChange={this.onLogLevelChanged}>
-                                <option value={-1}>Debug</option>
-                                <option value={CARTA.ErrorSeverity.INFO}>Info</option>
-                                <option value={CARTA.ErrorSeverity.WARNING}>Warning</option>
-                                <option value={CARTA.ErrorSeverity.ERROR}>Error</option>
-                                <option value={CARTA.ErrorSeverity.CRITICAL}>Critical</option>
-                            </HTMLSelect>
-                            <Button minimal={true} intent={"warning"} icon="delete" onClick={this.onClearClicked}>Clear</Button>
-                        </FormGroup>
+                        <Button minimal={true} intent={"warning"} icon="trash" onClick={this.onClearClicked}/>
                     </div>
                 </div>
             </div>);
