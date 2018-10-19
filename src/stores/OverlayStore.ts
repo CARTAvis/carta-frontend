df12d278364f2bd67136d20c144f2723fd84320b
Adrianna Pińska <adrianna.pinska@gmail.com>
2018-10-19 12:20:15 +0200

    semicolons

--- src/stores/OverlayStore.ts
+++ src/stores/OverlayStore.ts
@@ -614,8 +614,8 @@ export class OverlayStore {
         astString.addSection(this.numbers.styleString);
         astString.addSection(this.labels.styleString);
         
-        astString.add("LabelUp(2)", "0")
-        astString.add("DrawTitle", "0")
+        astString.add("LabelUp(2)", "0");
+        astString.add("DrawTitle", "0");
         
         return astString.toString();
     }
