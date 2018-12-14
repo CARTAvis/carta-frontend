ca46a380489b883427c6e64524f4a82f946d400d
veggiesaurus <accomrie@gmail.com>
2018-12-14 18:34:58 +0200

    updated to maintain compatibility with latest version of emcc

--- wasm_src/build_ast_wrapper.sh
+++ wasm_src/build_ast_wrapper.sh
@@ -8,7 +8,7 @@ npx tsc post.ts --outFile build/post.js
 emcc -std=c++11 -o build/ast_wrapper.js ast_wrapper.cc grf_debug.cc --pre-js build/pre.js --post-js build/post.js \
     -I../../wasm_libs/ast -L../../wasm_libs/ast/.libs -last -last_pal -lm -g0 -O2 \
     -s WASM=1 -s ALLOW_MEMORY_GROWTH=1 -s NO_EXIT_RUNTIME=1 -s EXTRA_EXPORTED_RUNTIME_METHODS='["ccall", "cwrap", "UTF8ToString"]' \
-    -s EXPORTED_FUNCTIONS='["_plotGrid", "_initFrame", "_initDummyFrame", "_format", "_set", "_transform", "_getString", "_normalize"]'
+    -s EXPORTED_FUNCTIONS='["_plotGrid", "_initFrame", "_initDummyFrame", "_format", "_set", "_transform", "_getString", "_norm"]'
 
 
 printf "Checking for AST wrapper WASM..."
