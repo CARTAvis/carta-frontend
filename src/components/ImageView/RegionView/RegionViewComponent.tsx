11999c2ee4ebf9ad02e884ffef9be7c2cf6d9d2e
veggiesaurus <accomrie@gmail.com>
2019-01-16 15:20:45 +0200

    fixed resizing when using corer-resizing

--- src/components/ImageView/RegionView/RegionViewComponent.tsx
+++ src/components/ImageView/RegionView/RegionViewComponent.tsx
@@ -64,29 +64,33 @@ export class RegionViewComponent extends React.Component<RegionViewComponentProp
                     } else if (anchor.indexOf("right") >= 0) {
                         center.x += deltaWidth / 2.0;
                     }
+
+                    if (anchor.indexOf("top") >= 0) {
+                        center.y += deltaHeight / 2.0;
+                    } else if (anchor.indexOf("bottom") >= 0) {
+                        center.y -= deltaHeight / 2.0;
+                    }
+
+
                     region.setControlPoints([center, {x: newWidth, y: newHeight}]);
                 }
-                // const lb = region.controlPoints[0];
-                // const rt = region.controlPoints[1];
-                // const width = rt.x - lb.x;
-                // const height = rt.y - lb.y;
-                // let newRT = {x: rt.x, y: rt.y};
-                // let newLB = {x: lb.x, y: lb.y};
-                // // X anchors are switched due to rotation
-                // if (anchor.indexOf("left") >= 0) {
-                //     newRT.x = lb.x + width * nodeScale.x;
-                // } else if (anchor.indexOf("right") >= 0) {
-                //     newLB.x = rt.x - width * nodeScale.x;
-                // }
-                //
-                // if (anchor.indexOf("top") >= 0) {
-                //     newRT.y = lb.y + height * nodeScale.y;
-                // } else if (anchor.indexOf("bottom") >= 0) {
-                //     newLB.y = rt.y - height * nodeScale.y;
-                // }
-                //
-                // region.setControlPoints([newLB, newRT]);
             }
+        }
+    };
+
+    handleDrag = (evt) => {
+        if (evt.target) {
+            const node = evt.target as Konva.Node;
+            const region = this.props.frame.regionSet.regions[0];
+            const frame = this.props.frame;
+            const currentView = frame.requiredFrameView;
+            const centerImageSpace = region.controlPoints[0];
+            const viewWidth = currentView.xMax - currentView.xMin;
+            const viewHeight = currentView.yMax - currentView.yMin;
+            const currentCenterPixelSpace = {x: Math.floor((centerImageSpace.x - currentView.xMin) / viewWidth * this.props.width), y: this.props.height - Math.ceil((centerImageSpace.y - currentView.yMin) / viewHeight * this.props.height)};
+            const newCenterPixelSpace = node.position();
+            const deltaPositionImageSpace = {x: (newCenterPixelSpace.x - currentCenterPixelSpace.x) / frame.zoomLevel, y: (newCenterPixelSpace.y - currentCenterPixelSpace.y) / frame.zoomLevel};
+            region.setControlPoint(0, {x: centerImageSpace.x + deltaPositionImageSpace.x, y: centerImageSpace.y + deltaPositionImageSpace.y});
         }
     };
 
@@ -106,28 +110,29 @@ export class RegionViewComponent extends React.Component<RegionViewComponentProp
 
         if (regionSet && regionSet.regions.length) {
             regionRects = regionSet.regions.filter(r => r.regionType === RegionType.RECTANGLE && r.isValid).map((r, index) => {
-                const cxImageSpace = r.controlPoints[0];
+                const centerImageSpace = r.controlPoints[0];
                 const viewWidth = currentView.xMax - currentView.xMin;
                 const viewHeight = currentView.yMax - currentView.yMin;
 
-                const center = {x: Math.floor((cxImageSpace.x - currentView.xMin) / viewWidth * this.props.width), y: this.props.height - Math.ceil((cxImageSpace.y - currentView.yMin) / viewHeight * this.props.height)};
+                const centerPixelSpace = {x: Math.floor((centerImageSpace.x - currentView.xMin) / viewWidth * this.props.width), y: this.props.height - Math.ceil((centerImageSpace.y - currentView.yMin) / viewHeight * this.props.height)};
                 const width = Math.floor(r.controlPoints[1].x * frame.zoomLevel);
                 const height = Math.floor(r.controlPoints[1].y * frame.zoomLevel);
                 return (
                     <Group key={index}>
                         <Rect
                             rotation={r.rotation}
-                            x={center.x + 0.5}
-                            y={center.y + 0.5}
+                            x={centerPixelSpace.x + 0.5}
+                            y={centerPixelSpace.y + 0.5}
                             width={width}
                             height={height}
                             offsetX={width / 2.0}
                             offsetY={height / 2.0}
                             key={index}
                             stroke={"white"}
-                            strokeWidth={1}
+                            strokeWidth={3}
                             draggable={true}
-                            fillEnabled={false}
+                            onDragMove={this.handleDrag}
+                            fillEnabled={true}
                             ref={this.handleRef}
                         />
                     </Group>
@@ -145,6 +150,8 @@ export class RegionViewComponent extends React.Component<RegionViewComponentProp
                         rotateAnchorOffset={20}
                         keepRatio={false}
                         centeredScaling={this.props.centeredScaling}
+                        draggable={false}
+                        borderEnabled={false}
                         onTransform={this.handleTransform}
                         onTransformEnd={() => console.log("transform finished")}
                     />
