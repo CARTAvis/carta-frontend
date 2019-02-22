13b6fedea52cc281954e663c880bcebfe0e563b6
duidae <duidae@gmail.com>
2019-02-22 17:38:32 +0800

    Fix style warning from TSLint

--- src/utilities/units.ts
+++ src/utilities/units.ts
@@ -34,27 +34,31 @@ export function formattedNotation(value: number): string {
 }
 
 export function binarySearchByX(array: Array<Point2D>, x: number): Point2D {
-    if (array === undefined || array.length === 0 || x === undefined)
+    if (array === undefined || array.length === 0 || x === undefined) {
         return null;
+    }
 
-    if (x < array[0].x)
+    if (x < array[0].x) {
         return array[0];
+    }
 
-    if (x > array[array.length - 1].x)
+    if (x > array[array.length - 1].x) {
         return array[array.length - 1];
+    }
 
     // binary search for the nearest point by x
     let start = 0;
     let end = array.length - 1;
 
-    while(start <= end) {
+    while (start <= end) {
         let middle = Math.floor((start + end) / 2);
-        if (x < array[middle].x)
+        if (x < array[middle].x) {
             end = middle - 1;
-        else if (x > array[middle].x)
+        } else if (x > array[middle].x) {
             start = middle + 1;
-        else
+        } else {
             return array[middle];
+        }
     }
     return ((array[start].x - x) < (x - array[end].x)) ? array[start] : array[end];
-};
+}
