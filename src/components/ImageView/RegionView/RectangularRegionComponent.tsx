cbf667f7e1fd4af52fa3e18f7e667323d318d5ec
veggiesaurus <accomrie@gmail.com>
2019-02-26 09:10:58 +0200

    experimental alternative handling of anchors

--- src/components/ImageView/RegionView/RectangularRegionComponent.tsx
+++ src/components/ImageView/RegionView/RectangularRegionComponent.tsx
@@ -5,6 +5,7 @@ import {observer} from "mobx-react";
 import {Ellipse, Group, Rect, Transformer} from "react-konva";
 import {FrameStore, RegionStore, RegionType} from "../../../stores";
 import {Colors} from "@blueprintjs/core";
+import {Point2D} from "../../../models";
 
 export interface RectangularRegionComponentProps {
     region: RegionStore;
@@ -20,7 +21,11 @@ export interface RectangularRegionComponentProps {
 @observer
 export class RectangularRegionComponent extends React.Component<RectangularRegionComponentProps> {
     @observable selectedRegionRef;
-    @observable centeredScaling;
+
+    private editAnchor: string;
+    private editStartAnchorPoint: Point2D;
+    private editOppositeAnchorPoint: Point2D;
+    private editStartCenterPoint: Point2D;
 
     handleRef = (ref) => {
         if (ref && this.selectedRegionRef !== ref) {
@@ -43,7 +48,32 @@ export class RectangularRegionComponent extends React.Component<RectangularRegio
     };
 
     handleTransformStart = (konvaEvent) => {
-        this.centeredScaling = !konvaEvent.evt.evt.ctrlKey;
+        this.editAnchor = konvaEvent.currentTarget.movingResizer;
+        const controlPoints = this.props.region.controlPoints;
+
+        this.editStartCenterPoint = {x: controlPoints[0].x, y: controlPoints[0].y};
+        this.editStartAnchorPoint = {x: controlPoints[0].x, y: controlPoints[0].y};
+        this.editOppositeAnchorPoint = {x: controlPoints[0].x, y: controlPoints[0].y};
+
+        const sizeFactor = this.props.region.regionType === RegionType.RECTANGLE ? 0.5 : 1.0;
+
+        if (this.editAnchor.indexOf("left") >= 0) {
+            this.editStartAnchorPoint.x = controlPoints[0].x - controlPoints[1].x * sizeFactor;
+        } else if (this.editAnchor.indexOf("right") >= 0) {
+            this.editStartAnchorPoint.x = controlPoints[0].x + controlPoints[1].x * sizeFactor;
+        }
+
+        if (this.editAnchor.indexOf("top") >= 0) {
+            this.editStartAnchorPoint.y = controlPoints[0].y + controlPoints[1].y * sizeFactor;
+        } else if (this.editAnchor.indexOf("bottom") >= 0) {
+            this.editStartAnchorPoint.y = controlPoints[0].y - controlPoints[1].y * sizeFactor;
+        }
+
+        this.editOppositeAnchorPoint = {
+            x: 2 * this.editStartCenterPoint.x - this.editStartAnchorPoint.x,
+            y: 2 * this.editStartCenterPoint.y - this.editStartAnchorPoint.y,
+        };
+
         this.props.region.beginEditing();
     };
 
@@ -63,40 +93,92 @@ export class RectangularRegionComponent extends React.Component<RectangularRegio
             } else {
                 // handle scaling
                 let nodeScale = node.scale();
-                const center = this.getCanvasPos(region.controlPoints[0].x, region.controlPoints[0].y);
-                node.x(center.x);
-                node.y(center.y);
+                const centerCanvasPos = this.getCanvasPos(region.controlPoints[0].x, region.controlPoints[0].y);
+                node.x(centerCanvasPos.x);
+                node.y(centerCanvasPos.y);
                 node.scaleX(1);
                 node.scaleY(1);
 
-                const newWidth = region.controlPoints[1].x * Math.abs(nodeScale.x);
-                const newHeight = region.controlPoints[1].y * Math.abs(nodeScale.y);
-                if (this.centeredScaling) {
-                    region.setControlPoint(1, {x: newWidth, y: newHeight});
+                // alternative anchor handling:
+                const newAnchorPoint = this.getImagePos(konvaEvent.evt.offsetX, konvaEvent.evt.offsetY);
+                const centerPoint = region.controlPoints[0];
+
+                const deltaAnchorPoint = {x: newAnchorPoint.x - centerPoint.x, y: newAnchorPoint.y - centerPoint.y};
+                const cosX = Math.cos(region.rotation * Math.PI / 180.0);
+                const sinX = Math.sin(region.rotation * Math.PI / 180.0);
+
+                const deltaAnchorPointRotated = {x: cosX * deltaAnchorPoint.x - sinX * deltaAnchorPoint.y, y: sinX * deltaAnchorPoint.x + cosX * deltaAnchorPoint.y};
+                const anchorPointRotated = {x: centerPoint.x + deltaAnchorPointRotated.x, y: centerPoint.y + deltaAnchorPointRotated.y};
+
+
+                const cornerScaling = konvaEvent.evt.ctrlKey;
+
+                if (cornerScaling) {
+                    let w = region.controlPoints[1].x;
+                    let h = region.controlPoints[1].y;
+                    const sizeFactor = region.regionType === RegionType.RECTANGLE ? 1.0 : 0.5;
+
+                    let newCenter = {x: region.controlPoints[0].x, y: region.controlPoints[0].y};
+
+                    if (anchor.indexOf("left") >= 0 || anchor.indexOf("right") >= 0) {
+                        w = Math.abs(this.editOppositeAnchorPoint.x - anchorPointRotated.x) * sizeFactor;
+                        newCenter.x = (anchorPointRotated.x + this.editOppositeAnchorPoint.x) / 2.0;
+                    }
+                    if (anchor.indexOf("top") >= 0 || anchor.indexOf("bottom") >= 0) {
+                        h = Math.abs(this.editOppositeAnchorPoint.y - anchorPointRotated.y) * sizeFactor;
+                        newCenter.y = cosX * (anchorPointRotated.y + this.editOppositeAnchorPoint.y) / 2.0;
+                        newCenter.y = cosX * (anchorPointRotated.y + this.editOppositeAnchorPoint.y) / 2.0;
+                    }
+                    region.setControlPoints([newCenter, {x: Math.max(1e-3, w), y: Math.max(1e-3, h)}]);
                 } else {
-                    const deltaWidth = (newWidth - region.controlPoints[1].x) * (region.regionType === RegionType.ELLIPSE ? 2 : 1);
-                    const deltaHeight = (newHeight - region.controlPoints[1].y) * (region.regionType === RegionType.ELLIPSE ? 2 : 1);
-                    let center = {x: region.controlPoints[0].x, y: region.controlPoints[0].y};
-                    const cosX = Math.cos(region.rotation * Math.PI / 180.0);
-                    const sinX = Math.sin(region.rotation * Math.PI / 180.0);
-                    if (anchor.indexOf("left") >= 0) {
-                        center.x -= cosX * deltaWidth / 2.0;
-                        center.y += sinX * deltaWidth / 2.0;
-                    } else if (anchor.indexOf("right") >= 0) {
-                        center.x += cosX * deltaWidth / 2.0;
-                        center.y -= sinX * deltaWidth / 2.0;
+                    let w = region.controlPoints[1].x;
+                    let h = region.controlPoints[1].y;
+                    const sizeFactor = region.regionType === RegionType.RECTANGLE ? 2.0 : 1.0;
+
+                    if (anchor.indexOf("left") >= 0 || anchor.indexOf("right") >= 0) {
+                        w = Math.abs(this.editStartCenterPoint.x - anchorPointRotated.x) * sizeFactor;
+                    }
+                    if (anchor.indexOf("top") >= 0 || anchor.indexOf("bottom") >= 0) {
+                        h = Math.abs(this.editStartCenterPoint.y - anchorPointRotated.y) * sizeFactor;
                     }
 
-                    if (anchor.indexOf("top") >= 0) {
-                        center.y += cosX * deltaHeight / 2.0;
-                        center.x += sinX * deltaHeight / 2.0;
-                    } else if (anchor.indexOf("bottom") >= 0) {
-                        center.y -= cosX * deltaHeight / 2.0;
-                        center.x -= sinX * deltaHeight / 2.0;
+                    if (konvaEvent.evt.shiftKey) {
+                        const maxSide = Math.max(w, h);
+                        w = maxSide;
+                        h = maxSide;
                     }
 
-                    region.setControlPoints([center, {x: newWidth, y: newHeight}]);
+                    region.setControlPoints([this.editStartCenterPoint, {x: Math.max(1e-3, w), y: Math.max(1e-3, h)}]);
                 }
+
+                // const newWidth = region.controlPoints[1].x * Math.abs(nodeScale.x);
+                // const newHeight = region.controlPoints[1].y * Math.abs(nodeScale.y);
+                // if (this.centeredScaling) {
+                //     region.setControlPoint(1, {x: newWidth, y: newHeight});
+                // } else {
+                //     const deltaWidth = (newWidth - region.controlPoints[1].x) * (region.regionType === RegionType.ELLIPSE ? 2 : 1);
+                //     const deltaHeight = (newHeight - region.controlPoints[1].y) * (region.regionType === RegionType.ELLIPSE ? 2 : 1);
+                //     let center = {x: region.controlPoints[0].x, y: region.controlPoints[0].y};
+                //     const cosX = Math.cos(region.rotation * Math.PI / 180.0);
+                //     const sinX = Math.sin(region.rotation * Math.PI / 180.0);
+                //     if (anchor.indexOf("left") >= 0) {
+                //         center.x -= cosX * deltaWidth / 2.0;
+                //         center.y += sinX * deltaWidth / 2.0;
+                //     } else if (anchor.indexOf("right") >= 0) {
+                //         center.x += cosX * deltaWidth / 2.0;
+                //         center.y -= sinX * deltaWidth / 2.0;
+                //     }
+                //
+                //     if (anchor.indexOf("top") >= 0) {
+                //         center.y += cosX * deltaHeight / 2.0;
+                //         center.x += sinX * deltaHeight / 2.0;
+                //     } else if (anchor.indexOf("bottom") >= 0) {
+                //         center.y -= cosX * deltaHeight / 2.0;
+                //         center.x -= sinX * deltaHeight / 2.0;
+                //     }
+                //
+                //     region.setControlPoints([center, {x: newWidth, y: newHeight}]);
+                // }
             }
         }
     };
@@ -126,6 +208,15 @@ export class RectangularRegionComponent extends React.Component<RectangularRegio
             region.setControlPoint(0, newPosition);
         }
     };
+
+    private getImagePos(canvasX: number, canvasY: number): Point2D {
+        const frameView = this.props.frame.requiredFrameView;
+        return {
+            x: (canvasX / this.props.layerWidth) * (frameView.xMax - frameView.xMin) + frameView.xMin - 1,
+            // y coordinate is flipped in image space
+            y: (canvasY / this.props.layerHeight) * (frameView.yMin - frameView.yMax) + frameView.yMax - 1
+        };
+    }
 
     private getCanvasPos(imageX: number, imageY: number) {
         const currentView = this.props.frame.requiredFrameView;
@@ -200,7 +291,7 @@ export class RectangularRegionComponent extends React.Component<RectangularRegio
                     borderStroke={Colors.TURQUOISE5}
                     borderDash={region.regionType === RegionType.ELLIPSE ? borderDash : null}
                     keepRatio={false}
-                    centeredScaling={this.centeredScaling}
+                    centeredScaling={true}
                     draggable={false}
                     borderEnabled={false}
                     onTransformStart={this.handleTransformStart}
