58ba257e7b244bee4e8df67b5d6c3f160e68b251
duidae <duidae@gmail.com>
2019-05-24 17:22:27 +0800

    Wired scaling in render config

--- src/components/Dialogs/PreferenceDialog/PreferenceDialogComponent.tsx
+++ src/components/Dialogs/PreferenceDialog/PreferenceDialogComponent.tsx
@@ -1,13 +1,53 @@
 import * as React from "react";
 import {observer} from "mobx-react";
-import {Select, ItemRenderer} from "@blueprintjs/select";
-import {Button, IDialogProps, Intent, Tab, Tabs, IconName, MenuItem, FormGroup} from "@blueprintjs/core";
+import {Select} from "@blueprintjs/select";
+import {Button, IDialogProps, Intent, Tab, Tabs, IPopoverProps, MenuItem, FormGroup} from "@blueprintjs/core";
 import {DraggableDialogComponent} from "components/Dialogs";
 import {AppStore} from "stores";
 import "./PreferenceDialogComponent.css";
+import {FrameScaling, RenderConfigStore} from "stores/RenderConfigStore";
+
+// Static assets
+import allMaps from "static/allmaps.png";
+// Equation PNG images
+import linearPng from "static/equations/linear.png";
+import logPng from "static/equations/log.png";
+import sqrtPng from "static/equations/sqrt.png";
+import squaredPng from "static/equations/squared.png";
+import gammaPng from "static/equations/gamma.png";
+import powerPng from "static/equations/power.png";
+
+const equationPngMap = new Map([
+    [FrameScaling.LINEAR, linearPng],
+    [FrameScaling.LOG, logPng],
+    [FrameScaling.SQRT, sqrtPng],
+    [FrameScaling.SQUARE, squaredPng],
+    [FrameScaling.GAMMA, gammaPng],
+    [FrameScaling.POWER, powerPng]
+]);
+
+const ScalingSelect = Select.ofType<FrameScaling>();
+const SCALING_KEYS = Array.from(RenderConfigStore.SCALING_TYPES.keys());
+const SCALING_POPOVER_PROPS: Partial<IPopoverProps> = {minimal: true, position: "auto-end", popoverClassName: "colormap-select-popover"};
 
 @observer
 export class PreferenceDialogComponent extends React.Component<{ appStore: AppStore }> {
+    renderScalingSelectItem = (scaling: FrameScaling, {handleClick, modifiers, query}) => {
+        if (!modifiers.matchesPredicate || !RenderConfigStore.SCALING_TYPES.has(scaling)) {
+            return null;
+        }
+        return (
+            <MenuItem
+                active={modifiers.active}
+                disabled={modifiers.disabled}
+                label={RenderConfigStore.SCALING_TYPES.get(scaling)}
+                key={scaling}
+                onClick={handleClick}
+                text={<div className="equation-div" style={{backgroundImage: `url(${equationPngMap.get(scaling)}`}}/>}
+            />
+        );
+    };
+
     public render() {
         const appStore = this.props.appStore;
         const preferenceStore = appStore.preferencesStore;
@@ -20,7 +60,18 @@ export class PreferenceDialogComponent extends React.Component<{ appStore: AppSt
 
         const renderConfigPanel = (
             <div className="panel-container">
-                <FormGroup inline={true} label="Scaling"></FormGroup>
+                <FormGroup inline={true} label="Scaling">
+                    <ScalingSelect
+                        activeItem={preferenceStore.scaling}
+                        popoverProps={SCALING_POPOVER_PROPS}
+                        filterable={false}
+                        items={SCALING_KEYS}
+                        onItemSelect={preferenceStore.setScaling}
+                        itemRenderer={this.renderScalingSelectItem}
+                    >
+                        <Button text={preferenceStore.scalingName} rightIcon="double-caret-vertical" alignText={"right"}/>
+                    </ScalingSelect>
+                </FormGroup>
                 <FormGroup inline={true} label="Color map"></FormGroup>
                 <FormGroup inline={true} label="Percentile Ranks"></FormGroup>
             </div>
--- src/stores/PreferencesStore.ts
+++ src/stores/PreferencesStore.ts
@@ -1,9 +1,25 @@
-import {action, observable} from "mobx";
+import {action, computed, observable} from "mobx";
+import {FrameScaling, RenderConfigStore} from "stores/RenderConfigStore";
 
 export class PreferencesStore {
     // user configurable settings
-    @observable scaling: string;
+    @observable scaling: FrameScaling;
     @observable colorMap: string;
+
+    @computed get scalingName() {
+        const scalingType = RenderConfigStore.SCALING_TYPES.get(this.scaling);
+        if (scalingType) {
+            return scalingType;
+        } else {
+            return "Unknown";
+        }
+    }
+
+    @action setScaling = (newScaling: FrameScaling) => {
+        if (RenderConfigStore.SCALING_TYPES.has(newScaling)) {
+            this.scaling = newScaling;
+        }
+    };
 
     // for preference UI
     @observable perferenceSelectedTab = "renderConfig";
@@ -12,7 +28,7 @@ export class PreferencesStore {
     }
 
     constructor() {
-        this.scaling= "Linear";
+        this.scaling= FrameScaling.SQUARE;
         this.colorMap = "Blues";
     }
 }
