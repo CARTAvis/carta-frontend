e9535bd9c24ec582c3c37cd4f792d5b0e73ec7c1
duidae <duidae@gmail.com>
2019-07-28 16:32:34 +0800

    Rename Layout.ts to PresetLayout.ts

---
+++ src/models/PresetLayout.ts
@@ -0,0 +1,14 @@
+export class PresetLayout {
+    public static readonly DEFAULT = "Default";
+    public static readonly CUBEVIEW = "Cube View";
+    public static readonly CUBEANALYSIS = "Cube Analysis";
+    public static readonly CONTINUUMANALYSIS = "Continuum Analysis";
+
+    public static isValid = (layout: string): boolean => {
+        return layout && (layout === PresetLayout.DEFAULT || layout === PresetLayout.CUBEVIEW || layout === PresetLayout.CUBEANALYSIS || layout === PresetLayout.CONTINUUMANALYSIS);
+    };
+
+    public static getPresetLayouts = (): string[] => {
+        return [PresetLayout.DEFAULT, PresetLayout.CUBEVIEW, PresetLayout.CUBEANALYSIS, PresetLayout.CONTINUUMANALYSIS];
+    };
+}
