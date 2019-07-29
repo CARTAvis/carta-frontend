d541cca04597bcfba1ade44c6c2594dc938ff455
duidae <duidae@gmail.com>
2019-07-29 14:53:18 +0800

    Remove comment

--- src/stores/WidgetsStore.ts
+++ src/stores/WidgetsStore.ts
@@ -307,7 +307,6 @@ export class WidgetsStore {
             // Clean up removed widget's store (ignoring items that have been floated)
             if (config.component !== "floated") {
                 const id = config.id as string;
-                // TODO: uncomment when dynamically adding store is integrated
                 console.log(`itemDestroyed: ${id}`);
                 this.removeWidget(id, config.component);
             }
