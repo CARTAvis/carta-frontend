ef4f283856f5adba89dadeff94b3b0c98e1eb29e
acdo2002 <acdo2002@gmail.com>
2019-08-07 11:33:26 +0800

    remove cursor info bar when the cursor is outside the image

--- src/models/CursorInfo.ts
+++ src/models/CursorInfo.ts
@@ -3,6 +3,7 @@ import {Point2D} from "./Point2D";
 export interface CursorInfo {
     posCanvasSpace: Point2D;
     posImageSpace: Point2D;
+    IsInImage: boolean;
     posWCS: Point2D;
     infoWCS: { x: string, y: string };
 }
