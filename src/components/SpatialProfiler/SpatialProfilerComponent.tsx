26230ea9854e23539e6c178b7abf469f8393a186
veggiesaurus <accomrie@gmail.com>
2019-08-20 10:44:41 +0200

    backport: #335

--- src/components/SpatialProfiler/SpatialProfilerComponent.tsx
+++ src/components/SpatialProfiler/SpatialProfilerComponent.tsx
@@ -283,13 +283,13 @@ export class SpatialProfilerComponent extends React.Component<WidgetProps> {
             for (let i = 0; i < values.length; i++) {
                 const pointWCS = AST.pixToWCS(this.frame.wcsInfo, values[i] + 1, this.profileStore.y + 1);
                 const normVals = AST.normalizeCoordinates(this.frame.wcsInfo, pointWCS.x, pointWCS.y);
-                this.cachedFormattedCoordinates[i] = AST.getFormattedCoordinates(this.frame.wcsInfo, normVals.x, undefined, astString.toString()).x;
+                this.cachedFormattedCoordinates[i] = AST.getFormattedCoordinates(this.frame.wcsInfo, normVals.x, undefined, astString.toString(), true).x;
             }
         } else {
             for (let i = 0; i < values.length; i++) {
                 const pointWCS = AST.pixToWCS(this.frame.wcsInfo, this.profileStore.x + 1, values[i] + 1);
                 const normVals = AST.normalizeCoordinates(this.frame.wcsInfo, pointWCS.x, pointWCS.y);
-                this.cachedFormattedCoordinates[i] = AST.getFormattedCoordinates(this.frame.wcsInfo, undefined, normVals.y, astString.toString()).y;
+                this.cachedFormattedCoordinates[i] = AST.getFormattedCoordinates(this.frame.wcsInfo, undefined, normVals.y, astString.toString(), true).y;
             }
         }
         this.trimDecimals();
