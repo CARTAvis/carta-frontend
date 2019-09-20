e50ffb871a5ca020510db1d594048339d3729d3a
duidae <duidae@gmail.com>
2019-09-20 17:12:23 +0800

    Refactor hexStringToRgb a little bit

--- src/utilities/color.ts
+++ src/utilities/color.ts
@@ -5,20 +5,20 @@ export function isColorValid(colorString: string): boolean {
 
 // adapted from https://stackoverflow.com/questions/21646738/convert-hex-to-rgba
 export function hexStringToRgb(colorString: string) {
-    if (isColorValid(colorString)) {
-        let c = colorString.substring(1).split("");
-        if (c.length === 3) {
-            c = [c[0], c[0], c[1], c[1], c[2], c[2]];
-        }
-        const hex = Number("0x" + c.join(""));
-
-        return {
-            r: (hex >> 16) & 255,
-            g: (hex >> 8) & 255,
-            b: hex & 255
-        };
+    if (!isColorValid(colorString)) {
+        return null;
     }
-    return null;
-}
 
+    let c = colorString.substring(1).split("");
+    if (c.length === 3) { // shorthand hex color
+        c = [c[0], c[0], c[1], c[1], c[2], c[2]];
+    }
+    const hex = Number("0x" + c.join(""));
+
+    return {
+        r: (hex >> 16) & 255,
+        g: (hex >> 8) & 255,
+        b: hex & 255
+    };
+}
 // end stolen from https://stackoverflow.com/questions/21646738/convert-hex-to-rgba
