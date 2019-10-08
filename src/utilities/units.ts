2099356801bfada1c8082a214736948afad34a1d
duidae <duidae@gmail.com>
2019-10-09 03:33:11 +0800

    Add toFixed wrapper function

--- src/utilities/units.ts
+++ src/utilities/units.ts
@@ -24,6 +24,15 @@ export function frequencyStringFromVelocity(velocity: number, refFreq: number): 
     return null;
 }
 
+// According to MDN, toFixed only works for up to 20 decimals
+export function toFixed(val: number, decimals: number) {
+    if (isFinite(val) && isFinite(decimals) && decimals >= 0 && decimals <= 20) {
+      return val.toFixed(decimals);
+    }
+    // leave undefined or non-finite values as is (+- INF, NaN and undefined will still appear properly)
+    return val;
+}
+
 export function formattedNotation(value: number): string {
     if (value === null || isNaN(value)) {
         return null;
