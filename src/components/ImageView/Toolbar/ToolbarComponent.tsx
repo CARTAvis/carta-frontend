7d931ddc9b93ca927cb9655cf1441d488f12447a
Hengtai Jan <hjan@asiaa.sinica.edu.tw>
2019-10-21 18:05:06 +0800

    Fixed bug and implemented specific naming and included the Toggle grid functionality

--- src/components/ImageView/Toolbar/ToolbarComponent.tsx
+++ src/components/ImageView/Toolbar/ToolbarComponent.tsx
@@ -15,6 +15,22 @@ export class ToolbarComponentProps {
     vertical: boolean;
 }
 
+const coordinateSystemName = new Map<SystemType, string>([
+    [SystemType.Native, "AUTO"],
+    [SystemType.FK5, "FK5"],
+    [SystemType.FK4, "FK4"],
+    [SystemType.Galactic, "GAL"],
+    [SystemType.Ecliptic, "ECL"],
+    [SystemType.ICRS, "ICRS"],
+]);
+const coordinateSystemTooltip = new Map<SystemType, string>([
+    [SystemType.Native, "Automatically select the coordinate system based on file headers"],
+    [SystemType.FK5, "FK5 coordinates, J2000.0 equinox"],
+    [SystemType.FK4, "FK4 coordinates, B1950.0 equinox"],
+    [SystemType.Galactic, "Galactic coordinates"],
+    [SystemType.Ecliptic, "Ecliptic coordinates"],
+    [SystemType.ICRS, "International Celestial Reference System"],
+]);
 @observer
 export class ToolbarComponent extends React.Component<ToolbarComponentProps> {
 
@@ -35,8 +51,15 @@ export class ToolbarComponent extends React.Component<ToolbarComponentProps> {
         this.props.appStore.activeFrame.regionSet.setMode(RegionMode.CREATING);
     };
 
-    handleCoordinateSystemClicked = (coordinateSystem: string) => {
-        this.props.appStore.overlayStore.global.setSystem(coordinateSystem as SystemType);
+    private coordinateSystemLebel: string = coordinateSystemName.get(SystemType.Native);
+    handleCoordinateSystemClicked = (coordinateSystem: SystemType) => {
+        if (coordinateSystem === SystemType.Native) {
+            this.props.appStore.overlayStore.global.setSystem(this.props.appStore.overlayStore.global.defaultSystem);
+            this.coordinateSystemLebel = coordinateSystemName.get(SystemType.Native);
+        } else {
+            this.props.appStore.overlayStore.global.setSystem(coordinateSystem);
+            this.coordinateSystemLebel = coordinateSystemName.get(coordinateSystem);
+        }
     }
 
     render() {
@@ -73,11 +96,16 @@ export class ToolbarComponent extends React.Component<ToolbarComponentProps> {
             </Menu>
         );
 
+        let coordinateSystem = this.props.appStore.overlayStore.global.system;
+        
         const coordinateSystemMenu = (
             <Menu>
-                {Object.keys(SystemType).map(key => (
-                    <MenuItem text={key} onClick={() => this.handleCoordinateSystemClicked(key)}/>
-                ))}
+                <MenuItem text={coordinateSystemName.get(SystemType.Native)} onClick={() => this.handleCoordinateSystemClicked(SystemType.Native)}/>
+                <MenuItem text={coordinateSystemName.get(SystemType.FK5)} onClick={() => this.handleCoordinateSystemClicked(SystemType.FK5)}/>
+                <MenuItem text={coordinateSystemName.get(SystemType.FK4)} onClick={() => this.handleCoordinateSystemClicked(SystemType.FK4)}/>
+                <MenuItem text={coordinateSystemName.get(SystemType.Galactic)} onClick={() => this.handleCoordinateSystemClicked(SystemType.Galactic)}/>
+                <MenuItem text={coordinateSystemName.get(SystemType.Ecliptic)} onClick={() => this.handleCoordinateSystemClicked(SystemType.Ecliptic)}/>
+                <MenuItem text={coordinateSystemName.get(SystemType.ICRS)} onClick={() => this.handleCoordinateSystemClicked(SystemType.ICRS)}/>
             </Menu>
         );
 
@@ -129,13 +157,10 @@ export class ToolbarComponent extends React.Component<ToolbarComponentProps> {
                 <Tooltip position={tooltipPosition} content={<span>Zoom to fit{currentZoomSpan}</span>}>
                     <Button icon="zoom-to-fit" onClick={frame.fitZoom}/>
                 </Tooltip>
-                <Tooltip position={tooltipPosition} content={<span>Overlay Coordinate</span>}>
+                <Tooltip position={tooltipPosition} content={<span>Overlay Coordinate <br/><small>Current: {coordinateSystemTooltip.get(coordinateSystem)}</small><br/><i><small>Double-click to Toggle grid</small></i></span>}>
                     <Popover content={coordinateSystemMenu} position={Position.TOP} minimal={true}>
-                        <Button text={this.props.appStore.overlayStore.global.system}/>
+                        <Button text={this.coordinateSystemLebel} active={grid.visible} onDoubleClick={() => grid.setVisible(!grid.visible)}/>
                     </Popover>
-                </Tooltip>
-                <Tooltip position={tooltipPosition} content="Toggle grid">
-                    <Button icon="grid" active={grid.visible} onClick={() => grid.setVisible(!grid.visible)}/>
                 </Tooltip>
                 <Tooltip position={tooltipPosition} content="Toggle labels">
                     <Button icon="numerical" active={!overlay.labelsHidden} onClick={overlay.toggleLabels}/>
