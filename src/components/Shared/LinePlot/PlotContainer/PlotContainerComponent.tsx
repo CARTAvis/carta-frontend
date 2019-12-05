c7ed49e817b0238bf9b063e0c6c92348aa442385
qi pang <qipang@qis-imac.ccis.ualberta.ca>
2019-12-05 16:25:56 -0700

    fixed stokes line plot bugs with chartjs 2.9.3

--- src/components/Shared/LinePlot/PlotContainer/PlotContainerComponent.tsx
+++ src/components/Shared/LinePlot/PlotContainer/PlotContainerComponent.tsx
@@ -111,6 +111,8 @@ export class PlotContainerComponent extends React.Component<PlotContainerProps> 
             xScale.left = 85;
             xScale.right = currentWidth - 1;
             xScale.width = xScale.right - xScale.left;
+            xScale._startPixel = xScale.left;
+            xScale._length = xScale.width;
 
             chart.chartArea.left = 85;
             chart.chartArea.right = currentWidth - 1;
@@ -470,6 +472,8 @@ export class PlotContainerComponent extends React.Component<PlotContainerProps> 
                 if (this.props.multiColorMultiLinesColors && this.props.multiColorMultiLinesColors.size) {
                     if (this.props.usePointSymbols) {
                         multiPlotDatasetConfig.pointBackgroundColor = this.props.multiColorMultiLinesColors.get(key);
+                        multiPlotDatasetConfig.borderColor = currentLineColor;
+                        multiPlotDatasetConfig.pointBorderColor = "rgba(0, 0, 0, 0)";
                     } else {
                         multiPlotDatasetConfig.multicolorLineColors = this.props.multiColorMultiLinesColors.get(key);
                     }
