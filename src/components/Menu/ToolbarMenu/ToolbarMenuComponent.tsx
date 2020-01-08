0bafd0ca385006c60205d47d49a4c109c9c9f609
thchang <tienhaochang703@gmail.com>
2020-01-08 17:23:51 +0800

    Add file info, preference and overlay settings in toolbar

--- src/components/Menu/ToolbarMenu/ToolbarMenuComponent.tsx
+++ src/components/Menu/ToolbarMenu/ToolbarMenuComponent.tsx
@@ -23,47 +23,64 @@ export class ToolbarMenuComponent extends React.Component<{ appStore: AppStore }
 
     public render() {
         let className = "toolbar-menu";
+        let dialogClassName = "dialog-toolbar-menu";
         if (this.props.appStore.darkTheme) {
             className += " bp3-dark";
+            dialogClassName += " bp3-dark";
         }
+
+        const dialogStore = this.props.appStore.dialogStore;
 
         const commonTooltip = <span><br/><i><small>Drag to place docked widget<br/>Click to place a floating widget</small></i></span>;
         return (
-            <ButtonGroup className={className}>
-                <Tooltip content={<span>Region List Widget{commonTooltip}</span>}>
-                    <Button icon={"th-list"} id="regionListButton" onClick={this.props.appStore.widgetsStore.createFloatingRegionListWidget}/>
-                </Tooltip>
-                <Tooltip content={<span>Log Widget{commonTooltip}</span>}>
-                    <Button icon={"application"} id="logButton" onClick={this.props.appStore.widgetsStore.createFloatingLogWidget}/>
-                </Tooltip>
-                <Tooltip content={<span>Spatial Profiler{commonTooltip}</span>}>
-                    <Button icon={"pulse"} id="spatialProfilerButton" className={"profiler-button"} onClick={this.props.appStore.widgetsStore.createFloatingSpatialProfilerWidget}>
-                        xy
-                    </Button>
-                </Tooltip>
-                <Tooltip content={<span>Spectral Profiler{commonTooltip}</span>}>
-                    <Button icon={"pulse"} id="spectralProfilerButton" className={"profiler-button"} onClick={this.props.appStore.widgetsStore.createFloatingSpectralProfilerWidget}>
-                        &nbsp;z
-                    </Button>
-                </Tooltip>
-                <Tooltip content={<span>Statistics Widget{commonTooltip}</span>}>
-                    <Button icon={"calculator"} id="statsButton" onClick={this.props.appStore.widgetsStore.createFloatingStatsWidget}/>
-                </Tooltip>
-                <Tooltip content={<span>Histogram Widget{commonTooltip}</span>}>
-                    <Button icon={"timeline-bar-chart"} id="histogramButton" onClick={this.props.appStore.widgetsStore.createFloatingHistogramWidget}/>
-                </Tooltip>
-                <Tooltip content={<span>Animator Widget{commonTooltip}</span>}>
-                    <Button icon={"video"} id="animatorButton" onClick={this.props.appStore.widgetsStore.createFloatingAnimatorWidget}/>
-                </Tooltip>
-                <Tooltip content={<span>Render Config Widget{commonTooltip}</span>}>
-                    <Button icon={"style"} id="renderConfigButton" onClick={this.props.appStore.widgetsStore.createFloatingRenderWidget}/>
-                </Tooltip>
-                <Tooltip content={<span>Stokes Analysis Widget{commonTooltip}</span>}>
-                    <Button icon={"pulse"} id="stokesAnalysisButton" className={"profiler-button"} onClick={this.props.appStore.widgetsStore.createFloatingStokesWidget}>
-                        &nbsp;s
-                    </Button>
-                </Tooltip>
-            </ButtonGroup>
+            <React.Fragment>
+                <ButtonGroup className={className}>
+                    <Tooltip content={<span>Region List Widget{commonTooltip}</span>}>
+                        <Button icon={"th-list"} id="regionListButton" onClick={this.props.appStore.widgetsStore.createFloatingRegionListWidget}/>
+                    </Tooltip>
+                    <Tooltip content={<span>Log Widget{commonTooltip}</span>}>
+                        <Button icon={"application"} id="logButton" onClick={this.props.appStore.widgetsStore.createFloatingLogWidget}/>
+                    </Tooltip>
+                    <Tooltip content={<span>Spatial Profiler{commonTooltip}</span>}>
+                        <Button icon={"pulse"} id="spatialProfilerButton" className={"profiler-button"} onClick={this.props.appStore.widgetsStore.createFloatingSpatialProfilerWidget}>
+                            xy
+                        </Button>
+                    </Tooltip>
+                    <Tooltip content={<span>Spectral Profiler{commonTooltip}</span>}>
+                        <Button icon={"pulse"} id="spectralProfilerButton" className={"profiler-button"} onClick={this.props.appStore.widgetsStore.createFloatingSpectralProfilerWidget}>
+                            &nbsp;z
+                        </Button>
+                    </Tooltip>
+                    <Tooltip content={<span>Statistics Widget{commonTooltip}</span>}>
+                        <Button icon={"calculator"} id="statsButton" onClick={this.props.appStore.widgetsStore.createFloatingStatsWidget}/>
+                    </Tooltip>
+                    <Tooltip content={<span>Histogram Widget{commonTooltip}</span>}>
+                        <Button icon={"timeline-bar-chart"} id="histogramButton" onClick={this.props.appStore.widgetsStore.createFloatingHistogramWidget}/>
+                    </Tooltip>
+                    <Tooltip content={<span>Animator Widget{commonTooltip}</span>}>
+                        <Button icon={"video"} id="animatorButton" onClick={this.props.appStore.widgetsStore.createFloatingAnimatorWidget}/>
+                    </Tooltip>
+                    <Tooltip content={<span>Render Config Widget{commonTooltip}</span>}>
+                        <Button icon={"style"} id="renderConfigButton" onClick={this.props.appStore.widgetsStore.createFloatingRenderWidget}/>
+                    </Tooltip>
+                    <Tooltip content={<span>Stokes Analysis Widget{commonTooltip}</span>}>
+                        <Button icon={"pulse"} id="stokesAnalysisButton" className={"profiler-button"} onClick={this.props.appStore.widgetsStore.createFloatingStokesWidget}>
+                            &nbsp;s
+                        </Button>
+                    </Tooltip>
+                </ButtonGroup>
+                <ButtonGroup className={dialogClassName}>
+                    <Tooltip content={<span>File Info</span>}>
+                        <Button icon={"info-sign"} onClick={dialogStore.showFileInfoDialog} className={dialogStore.fileInfoDialogVisible ? "bp3-active" : ""}/>
+                    </Tooltip>
+                    <Tooltip content={<span>Preference</span>}>
+                        <Button icon={"properties"} onClick={dialogStore.showPreferenceDialog} className={dialogStore.preferenceDialogVisible ? "bp3-active" : ""}/>
+                    </Tooltip>
+                    <Tooltip content={<span>Overlay Settings</span>}>
+                        <Button icon={"settings"} onClick={this.props.appStore.overlayStore.showOverlaySettings} className={this.props.appStore.overlayStore.overlaySettingsDialogVisible ? "bp3-active" : ""}/>
+                    </Tooltip>
+                </ButtonGroup>
+            </React.Fragment>
         );
     }
 }
