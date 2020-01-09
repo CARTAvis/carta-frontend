267c8f6004d5f9fb016846520da0258f52f50ab4
duidae <duidae@gmail.com>
2020-01-09 16:44:46 +0800

    Add new schema for widget settings

--- src/models/LayoutSchema.ts
+++ src/models/LayoutSchema.ts
@@ -1,6 +1,6 @@
 export class LayoutSchema {
     public static readonly INITIAL_LAYOUT_SCHEMA_VERSION = 1;
-    public static readonly CURRENT_LAYOUT_SCHEMA_VERSION = 1;
+    public static readonly CURRENT_LAYOUT_SCHEMA_VERSION = 2;
 
     public static isLayoutVersionValid = (version: number): boolean => {
         return Number.isInteger(version) && version >= LayoutSchema.INITIAL_LAYOUT_SCHEMA_VERSION && version <= LayoutSchema.CURRENT_LAYOUT_SCHEMA_VERSION;
@@ -8,7 +8,59 @@ export class LayoutSchema {
 
     // key: layout schema version, value: schema
     public static readonly LAYOUT_SCHEMAS = {
+        // TODO: details of schema
         "1" : {
+            "required": ["layoutVersion", "docked", "floating"],
+            "properties": {
+                "layoutVersion": {
+                    "type": "integer",
+                    "minimum": LayoutSchema.INITIAL_LAYOUT_SCHEMA_VERSION,
+                    "maximum": LayoutSchema.CURRENT_LAYOUT_SCHEMA_VERSION
+                },
+                "docked":  {
+                    "type": "object",
+                    "properties": {
+                        "type": {
+                            "type": "string"
+                        },
+                        "content": {
+                            "type": "array",
+                            "items": {
+                                "type": "object"
+                            }
+                        }
+                    }
+                },
+                "floating": {
+                    "type": "array",
+                    "items": [
+                        {
+                            "type": "object",
+                            "required": ["type"],
+                            "properties": {
+                                "type": {
+                                    "type": "string"
+                                },
+                                "defaultWidth": {
+                                    "type": "integer"
+                                },
+                                "defaultHeight": {
+                                    "type": "integer"
+                                },
+                                "defaultX": {
+                                    "type": "integer"
+                                },
+                                "defaultY": {
+                                    "type": "integer"
+                                }
+                                // TODO: extend with widget properties
+                            }
+                        }
+                    ]
+                }
+            }
+        },
+        "2" : {
             "required": ["layoutVersion", "docked", "floating"],
             "properties": {
                 "layoutVersion": {
