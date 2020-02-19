3c208bde57a20efbdc1607b8394fa37d4ac1df7b
thchang <tienhaochang703@gmail.com>
2020-02-19 13:31:00 +0800

    add HelpContent to store help content
    * copyed help content from manual and removed tags <a>, <img>. <video>

--- src/components/HelpDrawer/HelpDrawerComponent.tsx
+++ src/components/HelpDrawer/HelpDrawerComponent.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import {observer} from "mobx-react";
 import {Drawer, IDrawerProps, Position, Classes} from "@blueprintjs/core";
-import {AppStore} from "stores";
+import {AppStore, HelpType} from "stores";
 import "./HelpDrawerComponent.css";
 
 @observer
@@ -23,7 +23,7 @@ export class HelpDrawerComponent extends React.Component<{ appStore: AppStore }>
             lazy: true,
             isOpen: helpStore.helpVisible,
             onClose: helpStore.hideHelpDrawer,
-            title: helpStore.helpTitle,
+            title: HELP_CONFIG.get(helpStore.type) ? HELP_CONFIG.get(helpStore.type).title : "",
             position: Position.RIGHT,
             size: "33%",
             hasBackdrop: true
@@ -32,10 +32,117 @@ export class HelpDrawerComponent extends React.Component<{ appStore: AppStore }>
         return (
             <Drawer {...drawerProps} >
                 <div className={Classes.DRAWER_BODY}>
-                    <div className={Classes.DIALOG_BODY} dangerouslySetInnerHTML={{__html: helpStore.helpContext}}/>
+                    <div className={Classes.DIALOG_BODY} dangerouslySetInnerHTML={{__html: HELP_CONFIG.get(helpStore.type) ? HELP_CONFIG.get(helpStore.type).conten : "" }}/>
                 </div>
-                <div className={Classes.DRAWER_FOOTER}>Footer</div>
             </Drawer>
         );
     }
 }
+
+const HELP_CONFIG = new Map<HelpType, { title: string, conten: string }>([
+    // Dialog
+    [
+        HelpType.CONTOUR, {
+            title: "Contour Configuration",
+            conten: ""
+    }], [
+        HelpType.FILE_Browser, {
+            title: "File Browser",
+            conten: require("!raw-loader!./HelpContent/file_browser.html")
+    }], [
+        HelpType.FILE_INFO, {
+            title: "File Info",
+            conten: ""
+    }], [
+        HelpType.SAVE_LAYOUT, {
+            title: "Save Layout",
+            conten: ""
+    }], [
+        HelpType.OVERLAY_SETTINGS, {
+            title: "Overlay Settings",
+            conten: ""
+    }], [
+        HelpType.PREFERENCES, {
+            title: "Preferences",
+            conten: ""
+    }], [
+        HelpType.REGION_DIALOG, {
+            title: "Region Dialog",
+            conten: ""
+    }],
+
+    // Widgets
+    [
+        HelpType.ANIMATOR, {
+            title: "Animator",
+            conten: ""
+    }], [
+        HelpType.HISTOGRAM, {
+            title: "Histogram",
+            conten: ""
+    }], [
+        HelpType.HISTOGRAM_SETTINGS, {
+            title: "Histogram Settings",
+            conten: ""
+    }], [
+        HelpType.ANIMATOR, {
+            title: "Animator",
+            conten: ""
+    }], [
+        HelpType.IMAGE_VIEW, {
+            title: "Image View",
+            conten: require("!raw-loader!./HelpContent/image_view.html")
+    }], [
+        HelpType.LAYER_LIST, {
+            title: "Layer List",
+            conten: ""
+    }], [
+        HelpType.LOG, {
+            title: "Log",
+            conten: ""
+    }], [
+        HelpType.PLACEHOLDER, {
+            title: "Placeholder",
+            conten: ""
+    }], [
+        HelpType.REGION_LIST, {
+            title: "Region List",
+            conten: ""
+    }], [
+        HelpType.RENDER_CONFIG, {
+            title: "Render Configuration",
+            conten: ""
+    }], [
+        HelpType.RENDER_CONFIG_SETTINGS, {
+            title: "Render Configuration Settings",
+            conten: ""
+    }], [
+        HelpType.SPATIAL_PROFILER, {
+            title: "Spatial Profiler",
+            conten: ""
+    }], [
+        HelpType.SPATIAL_PROFILER_SETTINGS, {
+            title: "Spatial Profiler Settings",
+            conten: ""
+    }], [
+        HelpType.SPECTRAL_PROFILER, {
+            title: "Spectral Profiler",
+            conten: ""
+    }], [
+        HelpType.SPECTRAL_PROFILER_SETTINGS, {
+            title: "Spectral Profiler Settings",
+            conten: ""
+    }], [
+        HelpType.STATS, {
+            title: "Statistics",
+            conten: ""
+    }], [
+        HelpType.STOKES_ANALYSIS, {
+            title: "Stokes Analysis",
+            conten: ""
+    }], [
+        HelpType.STOKES_ANALYSIS_SETTINGS, {
+            title: "Stokes Settings",
+            conten: ""
+    }]
+]);
--- src/stores/HelpStore.ts
+++ src/stores/HelpStore.ts
@@ -1,13 +1,11 @@
 import {action, observable} from "mobx";
 
 export class HelpStore {
+    @observable type: HelpType;
     @observable helpVisible: boolean = false;
-    @observable helpTitle: string;
-    @observable helpContext: string;
 
     @action showHelpDrawer = (helpType: HelpType) => {
-        this.helpTitle = HELP_CONFIG.get(helpType).title;
-        this.helpContext = HELP_CONFIG.get(helpType).context;
+        this.type = helpType;
         this.helpVisible = true;
     };
 
@@ -45,107 +43,3 @@ export enum HelpType {
     STOKES_ANALYSIS = "stoke-analysis",
     STOKES_ANALYSIS_SETTINGS = "stoke-analysis-settings"
 }
-
-const HELP_CONFIG = new Map<HelpType, any>([
-    [
-        HelpType.CONTOUR, {
-            title: "Contour Configuration",
-            context: "<h2>Drawer Test</h2><p><strong>Data integration is the seminal problem of the digital age. For over ten years,we’ve helped the world’s premier organizations rise to the challenge.</strong></p><p>Palantir Foundry radically reimagines the way enterprises interact with data by amplifying and extending the power of data integration. With Foundry, anyone can source, fuse, and transform data into any shape they desire. Business analysts become data engineers — and leaders in their organization’s data revolution.</p>"
-    }], [
-        HelpType.FILE_Browser, {
-            title: "File Browser",
-            context: ""
-    }], [
-        HelpType.FILE_INFO, {
-            title: "File Info",
-            context: ""
-    }], [
-        HelpType.SAVE_LAYOUT, {
-            title: "Save Layout",
-            context: ""
-    }], [
-        HelpType.OVERLAY_SETTINGS, {
-            title: "Overlay Settings",
-            context: ""
-    }], [
-        HelpType.PREFERENCES, {
-            title: "Preferences",
-            context: ""
-    }], [
-        HelpType.REGION_DIALOG, {
-            title: "Region Dialog",
-            context: ""
-    }], [
-        HelpType.ANIMATOR, {
-            title: "Animator",
-            context: ""
-    }], [
-        HelpType.HISTOGRAM, {
-            title: "Histogram",
-            context: ""
-    }], [
-        HelpType.HISTOGRAM_SETTINGS, {
-            title: "Histogram Settings",
-            context: ""
-    }], [
-        HelpType.ANIMATOR, {
-            title: "Animator",
-            context: ""
-    }], [
-        HelpType.IMAGE_VIEW, {
-            title: "Image View",
-            context: ""
-    }], [
-        HelpType.LAYER_LIST, {
-            title: "Layer List",
-            context: ""
-    }], [
-        HelpType.LOG, {
-            title: "Log",
-            context: ""
-    }], [
-        HelpType.PLACEHOLDER, {
-            title: "Placeholder",
-            context: ""
-    }], [
-        HelpType.REGION_LIST, {
-            title: "Region List",
-            context: ""
-    }], [
-        HelpType.RENDER_CONFIG, {
-            title: "Render Configuration",
-            context: ""
-    }], [
-        HelpType.RENDER_CONFIG_SETTINGS, {
-            title: "Render Configuration Settings",
-            context: ""
-    }], [
-        HelpType.SPATIAL_PROFILER, {
-            title: "Spatial Profiler",
-            context: ""
-    }], [
-        HelpType.SPATIAL_PROFILER_SETTINGS, {
-            title: "Spatial Profiler Settings",
-            context: ""
-    }], [
-        HelpType.SPECTRAL_PROFILER, {
-            title: "Spectral Profiler",
-            context: ""
-    }], [
-        HelpType.SPECTRAL_PROFILER_SETTINGS, {
-            title: "Spectral Profiler Settings",
-            context: ""
-    }], [
-        HelpType.STATS, {
-            title: "Statistics",
-            context: ""
-    }], [
-        HelpType.STOKES_ANALYSIS, {
-            title: "Stokes Analysis",
-            context: ""
-    }], [
-        HelpType.STOKES_ANALYSIS_SETTINGS, {
-            title: "Stokes Settings",
-            context: ""
-    }]
-]);
