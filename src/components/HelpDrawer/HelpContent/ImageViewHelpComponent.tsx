62ef9ed21911853edac7076649cb43bb522d2b1b
kswang1029 <kswang@asiaa.sinica.edu.tw>
2020-02-26 22:45:54 +0800

    fix

--- src/components/HelpDrawer/HelpContent/ImageViewHelpComponent.tsx
+++ src/components/HelpDrawer/HelpContent/ImageViewHelpComponent.tsx
@@ -26,7 +26,7 @@ export class ImageViewHelpComponent extends React.Component {
                     <li>Enable/disable grid lines and coordinate labels</li>
                     <li>Export image</li>
                 </ul>
-                <p><img src={imageTools} style={{ width: "70%", height: "auto" }}/></p>
+                <p><img src={imageTools} style={{ width: "80%", height: "auto" }}/></p>
                     <h3 id="zoom-and-pan">Zoom and pan</h3>
                     <p>Zoom actions can be triggered in different ways. The most common one is to use mouse and scroll wheel. By scrolling up, image is zoomed in, while by scrolling down, image is zoomed out. Alternatively, users may use the tool  buttons at the bottom-right corner of the image viewer to zoom in, zoom out, zoom to fit screen resolution, or zoom to fit image view.</p>
                     <p><img src={zoomButton} style={{ width: "70%", height: "auto" }}/></p>
@@ -44,7 +44,7 @@ export class ImageViewHelpComponent extends React.Component {
                             <p>Note that images are spatially matched by applying translation, rotation, and scaling. Inconsistency may be prominent when attempting to match wide field images or images with different projection types. However, grid lines are still accurate per image. If contour layers exist, they will match the raster image in the current image view with high position accuracy. Spectral matching is performed with nearest interpolation.</p>
                             <h3 id="contour-layers">Contour layers</h3>
                             <p>A contour layer can be generated via the contour configuration dialogue. Contours of spatially matched image are re-projected to other spatially matched raster image.</p>
-                            <p><img src={contourButton} style={{ width: "70%", height: "auto" }}/></p>
+                            <p><img src={contourButton} style={{ width: "30%", height: "auto" }}/></p>
                                 <h3 id="region-of-interest">Region of interest</h3>
                                 <p>Four types of region of interest are supported, including:</p>
                                 <ul>
@@ -57,13 +57,13 @@ export class ImageViewHelpComponent extends React.Component {
                                     <p>The default region type and the default region creation mode are customizable in the preferences dialogue.</p>
                                     <h3 id="customizing-the-image-plot">Customizing the image plot</h3>
                                     <p>The image overlay can be customized via the overlay settings dialogue. Plenty amounts of options are provided.</p>
-                                    <p><img src={overlaySettingButton} style={{ width: "70%", height: "auto" }}/></p>
+                                    <p><img src={overlaySettingButton} style={{ width: "30%", height: "auto" }}/></p>
                                         <h3 id="exports">Exports</h3>
                                         <p>What users see in the current image view can be exported as a PNG file with the &quot;Export image&quot; button in the image tool bar.</p>
                                         <p><img src={exportPNGButton} style={{ width: "70%", height: "auto" }}/></p>
                                         <h3 id="image-information-and-header">Image information and header</h3>
                                         <p>Basic image informaion and full image headers are displayed in the image information dialogue.</p>
-                                        <p><img src={imageInfoButton} style={{ width: "70%", height: "auto" }}/></p>
+                                        <p><img src={imageInfoButton} style={{ width: "30%", height: "auto" }}/></p>
             </div>
                                         );
                                     }
