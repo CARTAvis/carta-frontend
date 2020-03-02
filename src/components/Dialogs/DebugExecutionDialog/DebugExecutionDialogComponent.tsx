0638ef9edd46cb8dbe6a19d3c58b6874a6367223
veggiesaurus <accomrie@gmail.com>
2020-03-02 20:49:52 +0200

    more flexibility with parameters and targets due to improved regex; targets and parameters determined at execution time

--- src/components/Dialogs/DebugExecutionDialog/DebugExecutionDialogComponent.tsx
+++ src/components/Dialogs/DebugExecutionDialog/DebugExecutionDialogComponent.tsx
@@ -13,7 +13,8 @@ class ExecutionEntry {
         });
     }
 
-    action: any;
+    target: string;
+    action: string;
     parameters: any[];
     valid: boolean;
     async: boolean;
@@ -21,8 +22,38 @@ class ExecutionEntry {
 
     constructor(entry: string, appStore: AppStore) {
         this.appStore = appStore;
-
         entry = entry.trim();
+
+        const entryRegex = /^(\+?)((?:[\w\[\]]+\.)*)(\w+)\(([^)]*)\);?$/gm;
+        const matches = entryRegex.exec(entry);
+        // Four matching groups, first entry is the full match
+        if (matches && matches.length === 5 && matches[3].length) {
+            this.async = matches[1].length > 0;
+            if (matches[2].length) {
+                this.target = matches[2].substring(0, matches[2].length - 1);
+            }
+            this.action = matches[3];
+            const parameterRegex = /(\$(?:[\w\[\]]+\.)*)([\w\[\]]+)/gm;
+            try {
+                const substitutedParameterString = matches[4].replace(parameterRegex, "{\"macroTarget\": \"$1\", \"macroVariable\": \"$2\"}");
+                const parameterArray = JSON.parse(`[${substitutedParameterString}]`);
+                this.parameters = parameterArray.map(parameter => {
+                    if (typeof parameter === "object" && parameter.macroTarget && parameter.macroVariable) {
+                        parameter.macroTarget = parameter.macroTarget.slice(1, -1);
+                    }
+                    return parameter;
+                });
+            } catch (e) {
+                console.log(e);
+                this.valid = false;
+                return;
+            }
+            this.valid = true;
+        } else {
+            this.valid = false;
+        }
+
+        /*
         if (entry.length && entry.charAt(0) === "+") {
             this.async = true;
             entry = entry.substring(1);
@@ -73,62 +104,64 @@ class ExecutionEntry {
                 }
             }
             this.valid = true;
-        }
+        }*/
     }
 
     async execute() {
-        try {
-            let response;
-            if (this.parameters && this.parameters.length) {
-                const currentParameters = this.parameters.map(this.mapMacro);
-
-                if (this.async) {
-                    response = await this.action(...currentParameters);
-                    await ExecutionEntry.Delay(10);
-                } else {
-                    response = this.action(...currentParameters);
-                }
-            } else {
-                if (this.async) {
-                    response = await this.action();
-                    await ExecutionEntry.Delay(10);
-                } else {
-                    response = this.action();
-                }
-            }
-            console.log(response);
-        } catch (e) {
-            console.log(e);
+        const targetObject = ExecutionEntry.GetTargetObject(this.appStore, this.target);
+        if (targetObject == null) {
+            console.log(`Missing target object: ${this.target}`);
+            return;
         }
+        const currentParameters = this.parameters.map(this.mapMacro);
+        const actionFunction = targetObject[this.action];
+        if (!actionFunction || typeof (actionFunction) !== "function") {
+            console.log(`Missing action function: ${this.action}`);
+            console.log(actionFunction);
+            return;
+        }
+        actionFunction.bind(targetObject);
+        let response;
+        if (this.async) {
+            response = await actionFunction(...currentParameters);
+            await ExecutionEntry.Delay(10);
+        } else {
+            response = actionFunction(...currentParameters);
+        }
+        return response;
+    }
+
+    private static GetTargetObject(baseObject: any, targetString: string) {
+        if (!targetString) {
+            return baseObject;
+        }
+
+        let target = baseObject;
+        const targetNameArray = targetString.split(".");
+        for (const targetEntry of targetNameArray) {
+            const arrayRegex = /(\w+)(?:\[(\d+)\])?/gm;
+            const matches = arrayRegex.exec(targetEntry);
+            // Check if there's an array index in this parameter
+            if (matches && matches.length === 3 && matches[2] !== undefined) {
+                target = target[matches[1]];
+                if (target == null) {
+                    return null;
+                }
+                target = target[matches[2]];
+            } else {
+                target = target[targetEntry];
+            }
+            if (target == null) {
+                return null;
+            }
+        }
+        return target;
     }
 
     private mapMacro = (parameter: any) => {
-        if (typeof parameter === "object" && parameter.macroName) {
-            const macroNameArray = parameter.macroName.split(".");
-            console.log(macroNameArray);
-            let obj = this.appStore;
-            // Traverse objects based on parameter
-            for (const entry of macroNameArray) {
-                // Array matching
-                const arrayOpeningIndex = entry.indexOf("[");
-                const arrayClosingIndex = entry.lastIndexOf("]");
-                if (arrayOpeningIndex > 0 && arrayClosingIndex > arrayOpeningIndex) {
-                    const arrayName = entry.substring(0, arrayOpeningIndex);
-                    const arrayElement = entry.substring(arrayOpeningIndex + 1, arrayClosingIndex);
-                    obj = obj[arrayName];
-                    if (obj == null) {
-                        return null;
-                    }
-                    obj = obj[arrayElement];
-                } else {
-                    obj = obj[entry];
-                }
-                console.log(obj);
-                if (obj == null) {
-                    return null;
-                }
-            }
-            return obj;
+        if (typeof parameter === "object" && parameter.macroVariable) {
+            const targetString = parameter.macroTarget ? `${parameter.macroTarget}.${parameter.macroVariable}` : parameter.macroVariable;
+            return ExecutionEntry.GetTargetObject(this.appStore, targetString);
         }
         return parameter;
     };
@@ -207,7 +240,11 @@ export class DebugExecutionDialogComponent extends React.Component<{ appStore: A
         this.isExecuting = true;
 
         for (const entry of this.executionEntries) {
-            await entry.execute();
+            try {
+                await entry.execute();
+            } catch (err) {
+                console.log(err);
+            }
         }
 
         this.isExecuting = false;
