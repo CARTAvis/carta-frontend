f23906276ecae1509a1fdb3dbbcd9556bb43c9ce
duidae <duidae@gmail.com>
2020-03-17 16:50:27 +0800

    Fix stoke part

--- src/components/StokesAnalysis/StokesAnalysisComponent.tsx
+++ src/components/StokesAnalysis/StokesAnalysisComponent.tsx
@@ -153,11 +153,6 @@ export class StokesAnalysisComponent extends React.Component<WidgetProps> {
         return frame.isCoordChannel ? channel : frame.channelValues[channel];
     }
 
-    private getChannelUnit = (): string => {
-        const frame = this.props.appStore.activeFrame;
-        return frame && !frame.isCoordChannel ? frame.spectralUnit : "Channel";
-    };
-
     onChannelChanged = (x: number) => {
         const frame = this.props.appStore.activeFrame;
         if (this.props.appStore.animatorStore.animationState === AnimationState.PLAYING) {
@@ -703,7 +698,7 @@ export class StokesAnalysisComponent extends React.Component<WidgetProps> {
     ) => {
         let cursorInfo = null;
         const isMouseEntered = this.widgetStore.isMouseMoveIntoLinePlots || this.widgetStore.isMouseMoveIntoScatterPlots;
-        const xUnit =  this.getChannelUnit();
+        const xUnit =  this.props.appStore.activeFrame ? this.props.appStore.activeFrame.spectralUnitStr : "Channel";
         if (isMouseEntered) {
             let profilerData = {q: NaN, u: NaN, pi: NaN, pa: NaN, channel: NaN};
             if (this.widgetStore.isMouseMoveIntoLinePlots) {
@@ -741,20 +736,23 @@ export class StokesAnalysisComponent extends React.Component<WidgetProps> {
             isNaN(this.cursorInfo.quValue.x) || isNaN(this.cursorInfo.quValue.y)) {
             return profilerInfo;
         }
-        const xLabel = this.cursorInfo.xUnit === "Channel" ?
-                    "Channel " + toFixed(this.cursorInfo.channel) :
-                    formattedNotation(this.cursorInfo.channel) + " " + this.cursorInfo.xUnit;
-        const fractionalPol = this.widgetStore.fractionalPolVisible;
-        const qLabel = fractionalPol ? ", Q/I: " : ", Q: ";
-        const uLabel = fractionalPol ? ", U/I: " : ", U: ";
-        const piLabel = fractionalPol ? ", PI/I: " : ", PI: ";
-        const cursorString = "(" + xLabel
-            + qLabel + toExponential(this.cursorInfo.quValue.x, 2)
-            + uLabel + toExponential(this.cursorInfo.quValue.y, 2)
-            + piLabel + toExponential(this.cursorInfo.pi, 2)
-            + ", PA: " + toFixed(this.cursorInfo.pa, 2)
-            + ")";
-        profilerInfo.push(`${this.cursorInfo.isMouseEntered ? "Cursor:" : "Data:"} ${cursorString}`);
+        const frame = this.props.appStore.activeFrame;
+        if (frame && this.plotData) {
+            const xLabel = this.cursorInfo.xUnit === "Channel" ?
+                        "Channel " + toFixed(this.cursorInfo.channel) :
+                        formattedNotation(this.cursorInfo.channel) + " " + this.cursorInfo.xUnit;
+            const fractionalPol = this.widgetStore.fractionalPolVisible;
+            const qLabel = fractionalPol ? ", Q/I: " : ", Q: ";
+            const uLabel = fractionalPol ? ", U/I: " : ", U: ";
+            const piLabel = fractionalPol ? ", PI/I: " : ", PI: ";
+            const cursorString = "(" + xLabel
+                + qLabel + toExponential(this.cursorInfo.quValue.x, 2)
+                + uLabel + toExponential(this.cursorInfo.quValue.y, 2)
+                + piLabel + toExponential(this.cursorInfo.pi, 2)
+                + ", PA: " + toFixed(this.cursorInfo.pa, 2)
+                + ")";
+            profilerInfo.push(`${this.cursorInfo.isMouseEntered ? "Cursor:" : "Data:"} ${cursorString}`);
+        }
         return profilerInfo;
     };
     
@@ -881,7 +879,7 @@ export class StokesAnalysisComponent extends React.Component<WidgetProps> {
             const cursorX = {
                 profiler: this.widgetStore.linePlotcursorX,
                 image: this.currentChannelValue,
-                unit: this.getChannelUnit()
+                unit: frame.spectralUnitStr
             };
             const currentPlotData = this.plotData;
             let channel = {channelCurrent: 0, channelHovered: 0};
@@ -991,7 +989,7 @@ export class StokesAnalysisComponent extends React.Component<WidgetProps> {
                 let scatterCursorInfor = {
                     profiler: { x: this.widgetStore.scatterPlotCursorX, y: this.widgetStore.scatterPlotCursorY},
                     image: this.matchXYindex(cursorX.image, currentPlotData.quValues.dataset),
-                    unit: this.getChannelUnit()
+                    unit: frame.spectralUnitStr
                 };
                 quScatterPlotProps.cursorXY = scatterCursorInfor;
                 this.cursorInfo = this.getCursorInfo(
@@ -1020,10 +1018,9 @@ export class StokesAnalysisComponent extends React.Component<WidgetProps> {
                 quScatterPlotProps.yLabel = "Stokes U (" + frame.unit + ")";
             }
 
-            if (!frame.isCoordChannel) {
-                const spectralCoordinate = frame.isSpectralCoordinateConvertible ? frame.spectralCoordinate : `${frame.spectralInfo.channelType.code} (${frame.spectralInfo.channelType.unit})`;
+            if (frame.spectralAxis && !frame.isCoordChannel) {
                 const spectralSystem = frame.isSpectralSystemConvertible ? frame.spectralSystem : `${frame.spectralInfo.specsys}`;
-                paLinePlotProps.xLabel = piLinePlotProps.xLabel = quLinePlotProps.xLabel = `${spectralSystem && spectralSystem !== "" ? spectralSystem + ", " : ""}${spectralCoordinate}`;
+                paLinePlotProps.xLabel = piLinePlotProps.xLabel = quLinePlotProps.xLabel = `${spectralSystem && spectralSystem !== "" ? spectralSystem + ", " : ""}${frame.spectralCoordinate}`;
             }
 
             paLinePlotProps.markers = [];
