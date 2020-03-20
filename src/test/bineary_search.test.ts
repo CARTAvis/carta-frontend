9ab8b7b48e7e222b864b802117d7e101ad3144ca
duidae <duidae@gmail.com>
2020-03-20 13:30:20 +0800

    Refine binary search first & add test

---
+++ src/test/bineary_search.test.ts
@@ -0,0 +1,22 @@
+import {Point2D} from "../models";
+import {binarySearchByX} from "../utilities";
+
+const incremental: Point2D[] = [{x: -1, y: 0}, {x: 134, y: 0}, {x: 232, y: 0}, {x: 234, y: 0}, {x: 587, y: 0}, {x: 623, y: 0}, {x: 793, y: 0}, {x: 963, y: 0}, {x: 1074, y: 0}];
+const decremental: Point2D[] = [{x: 20, y: 0}, {x: 17, y: 0}, {x: 9, y: 0}, {x: 7, y: 0}, {x: 6, y: 0}, {x: 4, y: 0}, {x: 3, y: 0}, {x: 2, y: 0}, {x: 1, y: 0}, {x: -15, y: 0}];
+
+test("test binarySearchByX with sorted array in incremental/decremental order", () => {
+    expect(binarySearchByX(null, null)).toEqual([]);
+    expect(binarySearchByX(null, 10)).toEqual([]);
+    expect(binarySearchByX(incremental, null)).toEqual([]);
+    expect(binarySearchByX([], 10)).toEqual([]);
+
+    expect(binarySearchByX(incremental, -99)).toEqual([]);
+    expect(binarySearchByX(incremental, 0)).toEqual([]);
+    expect(binarySearchByX(incremental, 453)).toEqual([]);
+    expect(binarySearchByX(incremental, 2048)).toEqual([]);
+
+    expect(binarySearchByX(decremental, 999)).toEqual([]);
+    expect(binarySearchByX(decremental, 5)).toEqual([]);
+    expect(binarySearchByX(decremental, 0)).toEqual([]);
+    expect(binarySearchByX(decremental, -33)).toEqual([]);
+});
--- src/utilities/array.ts
+++ src/utilities/array.ts
@@ -1,36 +1,35 @@
 import {Point2D} from "../models";
 
-// TODO: upgrade binary search to bi-directional for supporting both incremental & decremental data
-export function binarySearchByX(array: readonly Point2D[], x: number): {point: Point2D, index: number} {
-    if (array === undefined || array.length === 0 || x === undefined) {
+// Supports sorted array in incremental or decremental order 
+export function binarySearchByX(sortedArray: readonly Point2D[], x: number): {point: Point2D, index: number} {
+    if (!sortedArray || sortedArray.length === 0 || x === null || x === undefined) {
         return null;
     }
+    const incremental = sortedArray[0] <= sortedArray[sortedArray.length-1];
 
-    if (x < array[0].x) {
-        return {point: array[0], index: 0};
+    if (x < sortedArray[0].x) {
+        return {point: sortedArray[0], index: 0};
     }
-
-    if (x > array[array.length - 1].x) {
-        return {point: array[array.length - 1], index: array.length - 1};
+    if (x > sortedArray[sortedArray.length - 1].x) {
+        return {point: sortedArray[sortedArray.length - 1], index: sortedArray.length - 1};
     }
-
     // binary search for the nearest point by x
     let start = 0;
-    let end = array.length - 1;
+    let end = sortedArray.length - 1;
 
     while (start <= end) {
         let middle = Math.floor((start + end) / 2);
-        if (x < array[middle].x) {
+        if (x < sortedArray[middle].x) {
             end = middle - 1;
-        } else if (x > array[middle].x) {
+        } else if (x > sortedArray[middle].x) {
             start = middle + 1;
         } else {
-            return {point: array[middle], index: middle};
+            return {point: sortedArray[middle], index: middle};
         }
     }
 
-    const closer = ((array[start].x - x) < (x - array[end].x)) ? start : end;
-    return {point: array[closer], index: closer};
+    const closer = ((sortedArray[start].x - x) < (x - sortedArray[end].x)) ? start : end;
+    return {point: sortedArray[closer], index: closer};
 }
 
 export const distinct = (value: any, index: number, self: Array<any>) => {
