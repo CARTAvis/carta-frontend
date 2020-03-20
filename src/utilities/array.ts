6bf4af72369cb7907b3ad800a6b3202342fb750a
duidae <duidae@gmail.com>
2020-03-20 14:17:48 +0800

    Finish binary search

--- src/utilities/array.ts
+++ src/utilities/array.ts
@@ -5,30 +5,47 @@ export function binarySearchByX(sortedArray: readonly Point2D[], x: number): {po
     if (!sortedArray || sortedArray.length === 0 || x === null || x === undefined) {
         return null;
     }
+
     const incremental = sortedArray[0] <= sortedArray[sortedArray.length-1];
-
-    if (x < sortedArray[0].x) {
-        return {point: sortedArray[0], index: 0};
-    }
-    if (x > sortedArray[sortedArray.length - 1].x) {
-        return {point: sortedArray[sortedArray.length - 1], index: sortedArray.length - 1};
-    }
-    // binary search for the nearest point by x
-    let start = 0;
-    let end = sortedArray.length - 1;
-
-    while (start <= end) {
-        let middle = Math.floor((start + end) / 2);
-        if (x < sortedArray[middle].x) {
-            end = middle - 1;
-        } else if (x > sortedArray[middle].x) {
-            start = middle + 1;
-        } else {
-            return {point: sortedArray[middle], index: middle};
+    if (incremental) {
+        if (x <= sortedArray[0].x) {
+            return {point: sortedArray[0], index: 0};
+        }
+        if (x >= sortedArray[sortedArray.length - 1].x) {
+            return {point: sortedArray[sortedArray.length - 1], index: sortedArray.length - 1};
+        }
+    } else {
+        if (x >= sortedArray[0].x) {
+            return {point: sortedArray[0], index: 0};
+        }
+        if (x <= sortedArray[sortedArray.length - 1].x) {
+            return {point: sortedArray[sortedArray.length - 1], index: sortedArray.length - 1};
         }
     }
 
-    const closer = ((sortedArray[start].x - x) < (x - sortedArray[end].x)) ? start : end;
+    // binary search for the nearest point by x
+    let start = 0;
+    let end = sortedArray.length - 1;
+    while (start <= end) {
+        let middle = Math.floor((start + end) / 2);
+        if (x === sortedArray[middle].x) {
+            return {point: sortedArray[middle], index: middle};
+        }
+        if (incremental) {
+            if (x < sortedArray[middle].x) {
+                end = middle - 1;
+            } else {
+                start = middle + 1;
+            }
+        } else {
+            if (x > sortedArray[middle].x) {
+                end = middle - 1;
+            } else {
+                start = middle + 1;
+            }
+        }
+    }
+    const closer = Math.abs(sortedArray[start].x - x) < Math.abs(x - sortedArray[end].x) ? start : end;
     return {point: sortedArray[closer], index: closer};
 }
 
