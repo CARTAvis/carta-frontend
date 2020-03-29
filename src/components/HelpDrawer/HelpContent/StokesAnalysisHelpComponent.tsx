a6b8fe7bfc7885dc2016dcb54fc662875463e5f0
kswang1029 <kswang@asiaa.sinica.edu.tw>
2020-03-29 20:40:00 +0800

    update stokes

--- src/components/HelpDrawer/HelpContent/StokesAnalysisHelpComponent.tsx
+++ src/components/HelpDrawer/HelpContent/StokesAnalysisHelpComponent.tsx
@@ -17,10 +17,10 @@ export class StokesAnalysisHelpComponent extends React.Component<{ appStore: App
                 <p>All these plots are inter-linked so that when zooming profiles, data in the visible range will be highlighted in the scatter plot, and vice verse.</p>
                 <h3 id="regions">Regions</h3>
                 <p>The region dropdown defaults to &quot;Active&quot; region which means a selected region in the image viewer. Users can select a region by clicking one on the image viewer, or by clicking a region entry on the region list
-                    widget. Stokes profile plot of the selected region will be updated accordingly. If no region is selectd, &quot;Active&quot; region defaults to cursor.</p>
+                    widget. Stokes profile plot of the selected region will be updated accordingly. If no region is selected, &quot;Active&quot; region defaults to cursor.</p>
                 <h3 id="responsive-and-progressive-profile-update">Responsive and progressive profile update</h3>
                 <p>When region Stokes profiles are requested, depending on the performance of the server, users may see profiles are updated piece by piece in regular interval. This feature provides a visual progress update for better user
-                    experience. In addtion, if users move a region while profiles are being updating, the old calculations will be terminated immediately and calculations of the new region Stokes profiles will start and users will see
+                    experience. In addition, if users move a region while profiles are being updating, the old calculations will be terminated immediately and calculations of the new region Stokes profiles will start and users will see
                     partial profiles in seconds.</p>
                 <h3 id="spectral-conventions-and-reference-frame">Spectral conventions and reference frame</h3>
                 <p>With the Stokes analysis settings dialogue, users can change the spectral convention, including:</p>
@@ -54,7 +54,7 @@ export class StokesAnalysisHelpComponent extends React.Component<{ appStore: App
                 <p>Users may click on the Stokes profile plot to switch to a channel (as indicated by a red vertical line) and view the image in the image viewer. The red line is draggable and acts equivalently like the channel slider in
                     the animator widget.</p>
                 <h3 id="exports">Exports</h3>
-                <p>The Stokes profile plots can be exported as a png file or a text file in tsv format via the buttons at the bottm-right corner (shown when hovering over the plot).</p>
+                <p>The Stokes profile plots can be exported as a png file or a text file in tsv format via the buttons at the bottom-right corner (shown when hovering over the plot).</p>
                 <h3 id="plot-cosmetics">Plot cosmetics</h3>
                 <p>The appearance of the Stokes profile plot is customizable via the Stokes analysis settings dialogue (the cog icon). Supported options are:</p>
                 <ul>
