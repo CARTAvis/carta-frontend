6574e40331b0c46c346d7e660d85a7376fc1adac
kswang1029 <kswang@asiaa.sinica.edu.tw>
2020-03-29 20:30:01 +0800

    update region

--- src/components/HelpDrawer/HelpContent/RegionDialogHelpComponent.tsx
+++ src/components/HelpDrawer/HelpContent/RegionDialogHelpComponent.tsx
@@ -12,7 +12,7 @@ export class RegionDialogHelpComponent extends React.Component<{ appStore: AppSt
                 <ul>
                     <li>color</li>
                     <li>linewidth</li>
-                    <li>line style (solid or dashd)</li>
+                    <li>line style (solid or dashed)</li>
                 </ul>
                 <p>and region properties, such as</p>
                 <ul>
