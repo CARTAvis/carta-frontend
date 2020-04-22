7fc5be2748a6d4d05530616df515e9627428d87d
duidae <duidae@gmail.com>
2020-04-22 18:53:20 +0800

    Extent dialog

--- src/components/Dialogs/RegionDialog/RegionDialogComponent.tsx
+++ src/components/Dialogs/RegionDialog/RegionDialogComponent.tsx
@@ -107,7 +107,7 @@ export class RegionDialogComponent extends React.Component<{ appStore: AppStore 
         );
 
         return (
-            <DraggableDialogComponent dialogProps={dialogProps} appStore={appStore} helpType={HelpType.REGION_DIALOG} defaultWidth={600} defaultHeight={450} minHeight={300} minWidth={400} enableResizing={true}>
+            <DraggableDialogComponent dialogProps={dialogProps} appStore={appStore} helpType={HelpType.REGION_DIALOG} defaultWidth={650} defaultHeight={450} minHeight={300} minWidth={400} enableResizing={true}>
                 <div className={Classes.DIALOG_BODY}>
                     {bodyContent}
                 </div>
