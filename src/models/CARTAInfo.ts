7c463041bd68f4ff3c948b1d2906a3051c34133f
ajm-asiaa <ajm@asiaa.sinica.edu.tw>
2020-05-05 13:26:25 +0800

    Updating the version readout to 1.3.1

--- src/models/CARTAInfo.ts
+++ src/models/CARTAInfo.ts
@@ -1,6 +1,6 @@
 export const CARTA_INFO = {
     acronym: "CARTA",
-    version: "1.3.0",
-    date: "20200330",
+    version: "1.3.1",
+    date: "20200505",
     fullName: "Cube Analysis and Rendering Tool for Astronomy"
 };
