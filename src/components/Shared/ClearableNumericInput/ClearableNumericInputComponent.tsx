520a90135358f53c4fc1907cf751f44636bae42b
Panchy <qpang@ualberta.ca>
2020-06-24 05:28:50 -0600

    Qi/issue 880 , 882 , 908, 909, 878, 938, 916 (#931)
    * issue 882
    
    * catalog table view multi selection
    
    * add tooltip for filer
    
    * update according PR 931
    
    * add sorting for catalog table view
    
    * issue 938
    
    * add clear sort
    
    * centralize source on image overlay when user select a single source; fixed data table header issue; fixed table issus with show only selected source
    
    * centralize source on image overlay for multiple selection
    
    * disable source selection in image view and table view if show slected source enabled in scatter view
    
    * update max row with key down
    
    * fixed hard crash in issue 931
    
    * update catalog table info bar
    
    * merge dev, disable maxRow while data update
    
    * fixed scatter update bug when catalog szie over 100K
    
    Co-authored-by: qi pang <qipang@qis-iMac.hitronhub.home>

--- src/components/Shared/ClearableNumericInput/ClearableNumericInputComponent.tsx
+++ src/components/Shared/ClearableNumericInput/ClearableNumericInputComponent.tsx
@@ -1,39 +1,48 @@
 import * as React from "react";
-import {Button, FormGroup, NumericInput, Tooltip} from "@blueprintjs/core";
+import {Button, FormGroup, NumericInput, Tooltip, INumericInputProps} from "@blueprintjs/core";
 import {observer} from "mobx-react";
 import {observable} from "mobx";
 import {toExponential} from "utilities";
 
 const KEYCODE_ENTER = 13;
 
-export interface ClearableNumericInputProps {
+export interface ClearableNumericInputProps extends INumericInputProps {
     label: string; 
     value: number; 
     onValueChanged: (val: number) => void;
     onValueCleared: () => void;
     displayExponential?: boolean;
+    updateValueOnKeyDown?: boolean;
 }
 
 @observer
 export class ClearableNumericInputComponent extends React.Component<ClearableNumericInputProps> {
 
     @observable private isFocused: boolean = false;
+    // trigger keydown update for value
+    @observable isKeyDown: boolean = false;
 
-    handleChange = (ev) => {
-        
+    handleChange = (ev) => { 
         if (ev.type === "keydown" && ev.keyCode !== KEYCODE_ENTER) {
             return;
         }
-
         const val = parseFloat(ev.currentTarget.value);
         if (isFinite(val) && val !== this.props.value) {
             this.props.onValueChanged(val);
+        }
+        if (this.props.updateValueOnKeyDown) {
+            this.props.onValueChanged(val);
+            this.isKeyDown = true;   
         }
     };
 
     handleOnFocus = () => {
         if (this.props.displayExponential) {
             this.isFocused = true;
+        }
+        if (this.props.updateValueOnKeyDown) {
+            this.isFocused = true;
+            this.isKeyDown = false;   
         }
     };
 
@@ -42,20 +51,36 @@ export class ClearableNumericInputComponent extends React.Component<ClearableNum
         if (this.props.displayExponential) {
             this.isFocused = false;
         }
+        if (this.props.updateValueOnKeyDown) {
+            this.isKeyDown = false;   
+            this.isFocused = false;
+        }
+    }
+
+    handleOnValueChange = () => {
+        if (this.props.updateValueOnKeyDown) {
+            this.isKeyDown = false;   
+        } 
     }
 
     render () {
+        let value = this.props.displayExponential && !this.isFocused ? toExponential(this.props.value, 3) : this.props.value;
+        if (this.props.updateValueOnKeyDown) {
+            value = this.isFocused && !this.isKeyDown ? this.props.value.toString() : this.props.value;
+        }
         return (
-        <FormGroup label={this.props.label} inline={true}>
+        <FormGroup className={this.props.className} label={this.props.label} inline={true} disabled={this.props.disabled}>
             <NumericInput
-                value={this.props.displayExponential && !this.isFocused ? toExponential(this.props.value, 3) : this.props.value}
+                value={value}
                 onFocus={this.handleOnFocus}
                 onBlur={this.handleOnBlur}
                 onKeyDown={this.handleChange}
+                onValueChange={this.handleOnValueChange}
                 buttonPosition="none"
+                disabled={this.props.disabled}
                 rightElement={
                     <Tooltip content="Reset value to default">
-                        <Button icon="refresh" minimal={true} onClick={this.props.onValueCleared}/>
+                        <Button icon="refresh" minimal={true} onClick={this.props.onValueCleared} disabled={this.props.disabled}/>
                     </Tooltip>
                 }
             />
