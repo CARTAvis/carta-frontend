eea1a06fd9fd59bfd104f56a046b570837768ed2
duidae <duidae@gmail.com>
2020-06-30 20:13:37 +0800

    Wire up receiving data from backend

--- src/stores/widgets/SpectralLineOverlayWidgetStore.ts
+++ src/stores/widgets/SpectralLineOverlayWidgetStore.ts
@@ -5,7 +5,7 @@ import {CARTA} from "carta-protobuf";
 import {AppStore} from "stores";
 import {BackendService} from "services";
 import {RegionWidgetStore, RegionsType} from "./RegionWidgetStore";
-import {ProcessedColumnData} from "models";
+import {ProcessedColumnData, ProtobufProcessing} from "models";
 import {ControlHeader} from "stores/widgets";
 import {wavelengthToFrequency, SPEED_OF_LIGHT} from "utilities";
 
@@ -25,7 +25,7 @@ export enum SpectralLineHeaders {
     Species = "Species",
     ChemicalName = "Chemical Name",
     ShiftedFrequency = "Shifted Frequency",
-    RestFrequency = "Rest Frequency",
+    RestFrequency = "Freq-MHz(rest frame,redshifted)",
     FreqErr = "Freq Err(rest frame,redshifted)",
     MeasFreqMHz = "Meas Freq-MHz(rest frame,redshifted)",
     MeasFreqErr = "Meas Freq Err(rest frame,redshifted)",
@@ -68,6 +68,7 @@ export interface SpectralLine {
 
 const SPECIES_COLUMN_INDEX = 0;
 const SHIFTIED_FREQUENCY_COLUMN_INDEX = 2;
+const REST_FREQUENCY_COLUMN_INDEX = 3;
 const RESOLVED_QN_COLUMN_INDEX = 7;
 
 export class SpectralLineOverlayWidgetStore extends RegionWidgetStore {
@@ -78,7 +79,7 @@ export class SpectralLineOverlayWidgetStore extends RegionWidgetStore {
     @observable queryRangeByCenter: NumberRange;
     @observable queryUnit: SpectralLineQueryUnit;
     @observable isQuerying: boolean;
-    @observable columnHeaders: Array<CARTA.CatalogHeader>;
+    @observable columnHeaders: Array<CARTA.ICatalogHeader>;
     @observable headerDisplay: Map<SpectralLineHeaders, boolean>;
     @observable redshiftType: RedshiftType;
     @observable redshiftInput: number;
@@ -169,26 +170,20 @@ export class SpectralLineOverlayWidgetStore extends RegionWidgetStore {
             const backendService = BackendService.Instance;
             backendService.requestSpectralLine(new CARTA.DoubleBounds({min: freqMHzFrom, max: freqMHzTo})).subscribe(ack => {
                 this.isQuerying = false;
-                if (ack.success && ack.dataSize) {
-                    // TODO: handle headers & data
+                if (ack.success && ack.dataSize > 0) {
+                    this.numDataRows = ack.dataSize;
+                    this.isLineSelectedArray = new Array<boolean>(this.numDataRows).fill(false);
+                    this.queryResult = ProtobufProcessing.ProcessCatalogData(ack.spectralLineData);
+                    this.originalFreqColumn = this.queryResult.get(REST_FREQUENCY_COLUMN_INDEX);
+                    this.columnHeaders = ack.headers.sort((a, b) => {
+                        return (a.columnIndex - b.columnIndex);
+                    });
                 }
             }, error => {
                 this.isQuerying = false;
                 console.error(error);
                 AppStore.Instance.alertStore.showAlert(error);
             });
-
-            /*
-            const data =
-            `Species	Chemical Name	Freq-MHz(rest frame,redshifted)	Freq Err(rest frame,redshifted)	Meas Freq-MHz(rest frame,redshifted)	Meas Freq Err(rest frame,redshifted)	Resolved QNs	Unresolved Quantum Numbers	CDMS/JPL Intensity	S<sub>ij</sub>&#956;<sup>2</sup> (D<sup>2</sup>)	S<sub>ij</sub>	Log<sub>10</sub> (A<sub>ij</sub>)	Lovas/AST Intensity	E_L (cm^-1)	E_L (K)	E_U (cm^-1)	E_U (K)	Linelist
-            H2CO	Formaldehyde	230530	0			6(5,1)-6(5,2)	 6 5 1       6 5 2    	-19.9244	109.59367	6.719	-29.67917		255.3456	367.38325	255.3456	367.38325	JPL
-            H2CO	Formaldehyde	230531	0			6(5,1)-6(5,2)	 6 5 1       6 5 2    	-19.9242	109.64415	6.722	-29.67897		255.3456	367.38325	255.3456	367.38325	CDMS
-            CH318OHvt=0,1&2	Methanol	230532	0			23(5,18)-23(5,19)A,vt=2	23 518 6    23 519 6  	-23.063	6.89188	0	-30.36989		1025.747	1475.81264	1025.747	1475.81264	CDMS
-            CH318OHvt=0,1&2	Methanol	230533	0			22(5,17)-22(5,18)A,vt=2	22 517 6    22 518 6  	-22.97	7.20636	0	-30.33162		990.4	1424.95648	990.4	1424.95648	CDMS
-            CH318OHvt=0,1&2	Methanol	230534	0			21(5,16)-21(5,17)A,vt=2	21 516 6    21 517 6  	-22.88	7.53846	0	-30.29231		956.585	1376.30452	956.585	1376.30452	CDMS\n`;
-            this.parsingQueryResponse(data);
-            this.isQuerying = false;
-            */
         }
     };
 
@@ -282,88 +277,6 @@ export class SpectralLineOverlayWidgetStore extends RegionWidgetStore {
             return value;
         } else {
             return null;
-        }
-    };
-
-    private initHeaderRelated() {
-        // column header
-        this.columnHeaders = [];
-        const headers = Object.values(SpectralLineHeaders);
-        for (let columnIndex = 0; columnIndex < headers.length; columnIndex++) {
-            const columnName = headers[columnIndex];
-            let dataType = columnName === (SpectralLineHeaders.ShiftedFrequency || SpectralLineHeaders.RestFrequency) ? CARTA.ColumnType.Double : CARTA.ColumnType.String;
-            this.columnHeaders.push(new CARTA.CatalogHeader({name: columnName, dataType: dataType, columnIndex: columnIndex}));
-        }
-
-        // control header
-        this.controlHeaders.clear();
-        if (this.columnHeaders.length) {
-            for (let columnIndex = 0; columnIndex < this.columnHeaders.length; columnIndex++) {
-                const header = this.columnHeaders[columnIndex];
-                let controlHeader: ControlHeader = {
-                    columnIndex: columnIndex,
-                    dataIndex: columnIndex,
-                    display: columnIndex < SpectralLineOverlayWidgetStore.initDisplayedColumnSize ? true : false,
-                    representAs: undefined,
-                    filter: undefined,
-                    columnWidth: null
-                };
-                this.controlHeaders.set(header.name, controlHeader);
-            }
-        }
-    }
-
-    private parsingQueryResponse = (response: string) => {
-        if (!response) {
-            return;
-        }
-        const lines = response.replace(/\n$/, "").split(/\r?\n/);
-        if (lines && lines.length > 1) {
-            const spectralLineInfo = [];
-            lines.forEach(line => {
-                spectralLineInfo.push(line.split(/\t/));
-            });
-
-            this.initHeaderRelated();
-
-            // find column data: spectralLineInfo[1] ~ spectralLineInfo[lines.length - 1]
-            const numDataRows = lines.length - 1;
-            if (this.columnHeaders.length > 0) {
-                for (let columnIndex = 0; columnIndex < this.columnHeaders.length; columnIndex++) {
-                    const columnName = this.columnHeaders[columnIndex].name;
-                    let dataType = CARTA.ColumnType.String;
-                    let data = [];
-                    if (columnName === SpectralLineHeaders.Species || columnName === SpectralLineHeaders.ChemicalName) {
-                        for (let row = 0; row < numDataRows; row++) {
-                            const valString = spectralLineInfo[row + 1][columnIndex];
-                            data.push(valString);
-                        }
-                    } else if (columnName === SpectralLineHeaders.ShiftedFrequency) {
-                        dataType = CARTA.ColumnType.Double;
-                        for (let row = 0; row < numDataRows; row++) {
-                            const valString = spectralLineInfo[row + 1][columnIndex];
-                            data.push(Number(valString) * this.redshiftFactor);
-                        }
-                    } else if (columnName === SpectralLineHeaders.RestFrequency) {
-                        dataType = CARTA.ColumnType.Double;
-                        for (let row = 0; row < numDataRows; row++) {
-                            const valString = spectralLineInfo[row + 1][columnIndex - 1];
-                            data.push(Number(valString));
-                        }
-                        this.originalFreqColumn = {dataType: dataType, data: data};
-                    } else {
-                        for (let row = 0; row < numDataRows; row++) {
-                            const valString = spectralLineInfo[row + 1][columnIndex - 1];
-                            data.push(valString);
-                        }
-                    }
-                    this.queryResult.set(columnIndex, {dataType: dataType, data: data});
-                }
-            }
-
-            // update numDataRows
-            this.numDataRows = numDataRows;
-            this.isLineSelectedArray = new Array<boolean>(numDataRows).fill(false);
         }
     };
 
