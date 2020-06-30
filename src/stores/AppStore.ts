046257f3517626b72db8f8e4d6d4b20d671716d4
Angus Comrie <accomrie@gmail.com>
2020-06-30 10:37:20 +0200

    adds contour properties to resume message (#966)
    * adds contour properties to resume message
    
    * use existing contour parameters message when resuming
    
    * specify fileId correctly for contour resume settings
    
    * send catalog information when resuming session

--- src/stores/AppStore.ts
+++ src/stores/AppStore.ts
@@ -543,11 +543,11 @@ export class AppStore {
             this.fileLoading = false;
             console.timeEnd(`CatalogLoad_${file}`);
             if (frame && ack.success && ack.dataSize) {
-                let catalogInfo: CatalogInfo = {fileId: fileId, fileInfo: ack.fileInfo, dataSize: ack.dataSize};
-                let catalogWidgetId = null;
+                let catalogInfo: CatalogInfo = {fileId, directory, fileInfo: ack.fileInfo, dataSize: ack.dataSize};
+                let catalogWidgetId;
                 const columnData = ProtobufProcessing.ProcessCatalogData(ack.previewData);
                 const catalogComponentSize = this.widgetsStore.catalogComponentSize();
-                if (catalogComponentSize === 0 ) {
+                if (catalogComponentSize === 0) {
                     const catalog = this.widgetsStore.createFloatingCatalogOverlayWidget(catalogInfo, ack.headers, columnData);
                     catalogWidgetId = catalog.widgetStoreId;
                     this.catalogProfiles.set(catalog.widgetComponentId, fileId);
@@ -599,7 +599,7 @@ export class AppStore {
                 const nextFileId = this.catalogs.values().next().value;
                 this.catalogProfiles.forEach((catalogFileId, componentId) => {
                     if (catalogFileId === fileId) {
-                        this.catalogProfiles.set(componentId, nextFileId);     
+                        this.catalogProfiles.set(componentId, nextFileId);
                     }
                 });
             } else {
@@ -1211,6 +1211,19 @@ export class AppStore {
                 };
             });
 
+            let contourSettings: CARTA.ISetContourParameters;
+            if (frame.contourConfig.enabled) {
+                contourSettings = {
+                    fileId: frame.frameInfo.fileId,
+                    levels: frame.contourConfig.levels,
+                    smoothingMode: frame.contourConfig.smoothingMode,
+                    smoothingFactor: frame.contourConfig.smoothingFactor,
+                    decimationFactor: this.preferenceStore.contourDecimation,
+                    compressionLevel: this.preferenceStore.contourCompressionLevel,
+                    contourChunkSize: this.preferenceStore.contourChunkSize
+                };
+            }
+
             return {
                 file: info.fileInfo.name,
                 directory: info.directory,
@@ -1219,13 +1232,30 @@ export class AppStore {
                 renderMode: info.renderMode,
                 channel: frame.requiredChannel,
                 stokes: frame.requiredStokes,
-                regions
+                regions,
+                contourSettings
             };
+        });
+
+        const catalogFiles: CARTA.IOpenCatalogFile[] = [];
+
+        this.widgetsStore.catalogOverlayWidgets.forEach((widgetsStore) => {
+            const catalogInfo = widgetsStore.catalogInfo;
+            const existingEntry = catalogFiles.find(entry => entry.fileId === catalogInfo.fileId);
+            // Skip duplicates
+            if (existingEntry) {
+                return;
+            }
+            catalogFiles.push({
+                fileId: catalogInfo.fileId,
+                name: catalogInfo.fileInfo.name,
+                directory: catalogInfo.directory
+            });
         });
 
         this.resumingSession = true;
 
-        this.backendService.resumeSession({images}).subscribe(this.onSessionResumed, err => {
+        this.backendService.resumeSession({images, catalogFiles}).subscribe(this.onSessionResumed, err => {
             console.error(err);
             this.alertStore.showAlert("Error resuming session");
         });
@@ -1615,5 +1645,5 @@ export class AppStore {
                 this.catalogProfiles.set(componentIds[0], catalogFileId);
             }
         }
-    }
+    };
 }
