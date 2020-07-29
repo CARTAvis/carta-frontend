0f08b390d77240549a4dfd84ce9aa864056c935a
veggiesaurus <accomrie@gmail.com>
2020-07-29 10:59:48 +0200

    clamp input values correctly when out of bounds values are entered

--- src/components/Shared/SafeNumericInput/SafeNumericInput.tsx
+++ src/components/Shared/SafeNumericInput/SafeNumericInput.tsx
@@ -1,10 +1,17 @@
 import * as React from "react";
 import {NumericInput} from "@blueprintjs/core";
+import {clamp} from "utilities";
 
 export class SafeNumericInput extends React.PureComponent<any> {
     safeHandleValueChanged = (valueAsNumber: number, valueAsString: string) => {
-        if (this.props.onValueChange && isFinite(valueAsNumber) && (!this.props.min || this.props.min <= valueAsNumber) && (!this.props.max || this.props.max >= valueAsNumber)) {
-            this.props.onValueChange(valueAsNumber, valueAsString);
+        if (isFinite(valueAsNumber)) {
+            const clampedValue = clamp(valueAsNumber, this.props.min, this.props.max);
+            if (clampedValue !== valueAsNumber) {
+                valueAsString = valueAsNumber.toString();
+            }
+            if (this.props.onValueChange) {
+                this.props.onValueChange(clampedValue, valueAsString);
+            }
         }
     };
 
