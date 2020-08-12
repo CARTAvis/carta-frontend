f73c7472360ed6740182600ce1f8da0d4b762063
veggiesaurus <accomrie@gmail.com>
2020-08-12 17:24:19 +0200

    use reference frame when displaying region information

--- src/components/Dialogs/RegionDialog/RegionDialogComponent.tsx
+++ src/components/Dialogs/RegionDialog/RegionDialogComponent.tsx
@@ -51,7 +51,7 @@ export class RegionDialogComponent extends React.Component {
             bodyContent = RegionDialogComponent.InvalidRegionNode;
         } else {
             region = appStore.activeFrame.regionSet.selectedRegion;
-            const frame = appStore.activeFrame;
+            const frame = appStore.activeFrame.spatialReference ?? appStore.activeFrame;
 
             dialogProps.title = `Editing ${region.nameString}`;
             switch (region.regionType) {
