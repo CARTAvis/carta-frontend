0d0ff7e41cbbc6234d6842c068f7f636c6b63a57
許守傑 Shou-Chieh Hsu (Jason) <duidae@gmail.com>
2020-08-13 18:52:53 +0800

    Fix warning (#1113)
    * Fix warning
    
    * Fix warnings
    
    Co-authored-by: veggiesaurus <accomrie@gmail.com>

--- src/components/HelpDrawer/HelpContent/SpatialProfilerSettingsSmoothingHelpComponent.tsx
+++ src/components/HelpDrawer/HelpContent/SpatialProfilerSettingsSmoothingHelpComponent.tsx
@@ -6,18 +6,17 @@ export class SpatialProfilerSettingsSmoothingHelpComponent extends React.Compone
             <div>
                 <h3>Smoothing</h3>
                 <p>Smoothing may be applied to profiles to enhance signal-to-noise 
-                    ratio. CARTA provides the following smoothing methods:
-                    <ul>
-                        <li><b>Boxcar</b>: convolution with a boxcar function</li>
-                        <li><b>Gaussian</b>: convolution with a Gaussian function</li>
-                        <li><b>Hanning</b>: convolution with a Hanning function</li>
-                        <li><b>Binning</b>: averaging channels with a given width</li>
-                        <li><b>Savitzky-Golay</b>: fitting successive sub-sets of adjacent 
-                            data points with a low-degree polynomial by the method 
-                            of linear least squares</li>
-                        <li><b>Decimation</b>: min-max decimation with a given width</li>    
-                    </ul>
-                </p>
+                    ratio. CARTA provides the following smoothing methods:</p>
+                <ul>
+                    <li><b>Boxcar</b>: convolution with a boxcar function</li>
+                    <li><b>Gaussian</b>: convolution with a Gaussian function</li>
+                    <li><b>Hanning</b>: convolution with a Hanning function</li>
+                    <li><b>Binning</b>: averaging channels with a given width</li>
+                    <li><b>Savitzky-Golay</b>: fitting successive sub-sets of adjacent 
+                        data points with a low-degree polynomial by the method 
+                        of linear least squares</li>
+                    <li><b>Decimation</b>: min-max decimation with a given width</li>    
+                </ul>
                 <p>Optionally, the original profile can be overplotted with the 
                     smoothed profile. The appearance of the smoothed profile, 
                     including color, style, width, and size, can be customized.</p>
--- src/components/HelpDrawer/HelpContent/StokesAnalysisSettingsScatterPlotStylingHelpComponent.tsx
+++ src/components/HelpDrawer/HelpContent/StokesAnalysisSettingsScatterPlotStylingHelpComponent.tsx
@@ -5,14 +5,13 @@ export class StokesAnalysisSettingsScatterPlotStylingHelpComponent extends React
         return (
             <div>
                 <h3>Scatter Plot Styling</h3>
-                <p>The appearance of the scatter plot is customizable via the spectral profile settings dialogue (the cog icon). Supported options are:
-                    <ul>
-                        <li>Colormap</li>
-                        <li>Symbol size</li>
-                        <li>Symbol transparency</li>
-                        <li>Q-to-U scale ratio as unity</li>
-                    </ul>
-                </p>
+                <p>The appearance of the scatter plot is customizable via the spectral profile settings dialogue (the cog icon). Supported options are:</p>
+                <ul>
+                    <li>Colormap</li>
+                    <li>Symbol size</li>
+                    <li>Symbol transparency</li>
+                    <li>Q-to-U scale ratio as unity</li>
+                </ul>
             </div>
         );
     }
