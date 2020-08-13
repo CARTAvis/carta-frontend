d51f21340844fa593aae1e886c35e8fb87e6a518
duidae <duidae@gmail.com>
2020-08-13 17:13:22 +0800

    Fix warnings

--- src/components/HelpDrawer/HelpContent/StokesAnalysisSettingsLinePlotStylingHelpComponent.tsx
+++ src/components/HelpDrawer/HelpContent/StokesAnalysisSettingsLinePlotStylingHelpComponent.tsx
@@ -5,14 +5,13 @@ export class StokesAnalysisSettingsLinePlotStylingHelpComponent extends React.Co
         return (
             <div>
                 <h3>Line Plot Styling</h3>
-                <p>The appearance of the spectral profile plot is customizable via the spectral profile settings dialogue (the cog icon). Supported options are:
-                    <ul>
-                        <li>color of the plot</li>
-                        <li>plot styles including steps (default), lines, and dots</li>
-                        <li>line width for steps or lines</li>
-                        <li>point size for dots</li>
-                    </ul>
-                </p>
+                <p>The appearance of the spectral profile plot is customizable via the spectral profile settings dialogue (the cog icon). Supported options are:</p>
+                <ul>
+                    <li>color of the plot</li>
+                    <li>plot styles including steps (default), lines, and dots</li>
+                    <li>line width for steps or lines</li>
+                    <li>point size for dots</li>
+                </ul>
             </div>
         );
     }
