e3b5a816207b92b515f104ba925ef534b27a9472
veggiesaurus <accomrie@gmail.com>
2020-08-13 11:00:50 +0200

    updated protobuf commit and version number

--- src/models/CARTAInfo.ts
+++ src/models/CARTAInfo.ts
@@ -1,6 +1,6 @@
 export const CARTA_INFO = {
     acronym: "CARTA",
-    version: "1.4.0-alpha.5",
-    date: "20200811",
+    version: "1.4.0-alpha.6",
+    date: "20200813",
     fullName: "Cube Analysis and Rendering Tool for Astronomy"
 };
