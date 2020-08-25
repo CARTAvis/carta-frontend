0475d35d05b18bd261a616cddcfae50e503803b1
kswang1029 <kswang@asiaa.sinica.edu.tw>
2020-08-25 13:40:22 +0800

    update region dialogue help

--- src/components/HelpDrawer/HelpContent/RegionDialogHelpComponent.tsx
+++ src/components/HelpDrawer/HelpContent/RegionDialogHelpComponent.tsx
@@ -8,7 +8,8 @@ export class RegionDialogHelpComponent extends React.Component {
         return (
             <div>
 
-                <p>The region configuration dialogue allows users to change the appearance of a region, such as</p>
+                <p>The region configuration dialogue allows users to change the appearance of a 
+                    region, such as</p>
                 <ul>
                     <li>color</li>
                     <li>linewidth</li>
@@ -17,11 +18,20 @@ export class RegionDialogHelpComponent extends React.Component {
                 <p>and region properties, such as</p>
                 <ul>
                     <li>region name</li>
-                    <li>region location and shape properties</li>
+                    <li>region location and shape properties in image or world coordinate</li>
                 </ul>
+                <p>Region properties can be defined in world coordinate. If the coordinate reference 
+                    system is FK4, FK5, or ICRS, the coordinate format is sexagesimal. If the 
+                    coordinate system is Galactic or Ecliptic, the coordinate format is decimal degree. 
+                    Region size can be defined in arcsecond with <code>&quot;</code>, in arcminute 
+                    with <code>&apos;</code>, or in degree with <code>deg</code>.</p>
                 <br/>
+                <h4 id="note">NOTE</h4>
+                <p>The displayed image coordinates refer to the spatial reference 
+                    image as indicated in the title of the dialogue.</p>
                 <h4 id="tip">TIP</h4>
-                <p><code>Double-Click</code> on a region in the image viewer or on an entry in the region list widget will bring up this region configuration dialogue.</p>
+                <p><code>Double-Click</code> on a region in the image viewer or on an entry in 
+                the region list widget will bring up this region configuration dialogue.</p>
 
             </div>
         );
