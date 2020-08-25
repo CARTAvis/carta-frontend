b9d41ced84af594b015ae5bde97e9e33ec51d926
kswang1029 <kswang@asiaa.sinica.edu.tw>
2020-08-25 11:38:33 +0800

    update image list help

--- src/components/HelpDrawer/HelpContent/LayerListHelpComponent.tsx
+++ src/components/HelpDrawer/HelpContent/LayerListHelpComponent.tsx
@@ -8,12 +8,20 @@ export class LayerListHelpComponent extends React.Component {
         return (
             <div>
                 <p><ImageComponent light={headLayerButton} dark={headLayerButton_d} width="90%"/></p>
-                <p>The image list widget displays loaded images as a list, which includes image name, rendering layers (R as raster and C as contours) and layer visibility state, WCS matching state, channel index, and Stokes index. The
-                    channel index and Stokes index are synchronized with the animator.</p>
-                <p>Users may click &quot;R&quot; and/or &quot;C&quot; to hide/show a layer. For example, if users want to generate an image with contours only, users can hide raster layers by clicking &quot;R&quot;.</p>
-                <p>Per image, users can click the &quot;XY&quot; button to enable/disable spatial matching and click &quot;Z&quot; button to enable/disable spectral matching.</p>
+                <p>The image list widget displays loaded images as a list, which includes image name, 
+                    rendering layers (R as raster and C as contours) and layer visibility state, 
+                    WCS matching state, channel index, and Stokes index. The channel index and Stokes 
+                    index are synchronized with the animator.</p>
+                <p>Users may click &quot;R&quot; and/or &quot;C&quot; to hide/show a layer. For example, 
+                    if users want to generate an image with contours only, users can hide raster layers 
+                    by clicking &quot;R&quot;.</p>
+                <p>Per image, users can click the &quot;XY&quot; button to enable/disable spatial 
+                    matching and click &quot;Z&quot; button to enable/disable spectral matching.</p>
                 <p>To change reference image, <code>right-click</code> on a row to bring up the menu.</p>
-                <p>The list order reflects the order of the frame slider in the animator. The order may be modified by selecting an entry by <code>clicking</code> then <code>click-and-dragging</code> to form a desired order.</p>
+                <p>To close an image (or images), <code>right-click</code> on a row to bring up the menu.</p>
+                <p>The list order reflects the order of the image slider in the animator. The order 
+                    may be modified by selecting an entry by <code>clicking</code> 
+                    then <code>click-and-dragging</code> to form a desired order.</p>
             </div>
         );
     }
