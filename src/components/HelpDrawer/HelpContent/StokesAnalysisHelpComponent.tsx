4f3cc390a3a059a404af56da12a3560f7a7256b4
kswang1029 <kswang@asiaa.sinica.edu.tw>
2020-08-27 13:08:13 +0800

    update stokes help

--- src/components/HelpDrawer/HelpContent/StokesAnalysisHelpComponent.tsx
+++ src/components/HelpDrawer/HelpContent/StokesAnalysisHelpComponent.tsx
@@ -8,23 +8,36 @@ export class StokesAnalysisHelpComponent extends React.Component {
         return (
             <div>
                 <p><ImageComponent light={headStokesButton} dark={headStokesButton_d} width="90%"/></p>
-                <p>The Stokes analysis widget is specifically made for efficient visualization of a Stokes cube. The widget includes plots, such as:</p>
+                <p>The Stokes analysis widget is specifically made for efficient visualization of a 
+                    Stokes cube. The widget includes plots, such as:</p>
                 <ul>
-                    <li>Region spectral profiles for Stokes Q and Stokes U, as absolute or fractional values</li>
+                    <li>Region spectral profiles for Stokes Q and Stokes U, as absolute or 
+                        fractional values</li>
                     <li>Polarized intensity spectral profile, as absolute or fractional values</li>
                     <li>Linearly polarized angle spectral profile</li>
                     <li>Stokes Q vs Stokes U scatter plot</li>
                 </ul>
-                <p>All these plots are inter-linked so that when zooming profiles, data in the visible range will be highlighted in the scatter plot, and vice verse.</p>
+                <p>All these plots are inter-linked so that when zooming profiles, data in the 
+                    visible range will be highlighted in the scatter plot, and vice versa.</p>
+                <h3 id="images">Images</h3>
+                <p>The image dropdown defaults to &quot;Active&quot; image which means the current 
+                    image in the image viewer.</p>
                 <h3 id="regions">Regions</h3>
-                <p>The region dropdown defaults to &quot;Active&quot; region which means a selected region in the image viewer. Users can select a region by clicking one on the image viewer, or by clicking a region entry on the region list
-                    widget. Stokes profile plot of the selected region will be updated accordingly. If no region is selected, &quot;Active&quot; region defaults to cursor.</p>
+                <p>The region dropdown defaults to &quot;Active&quot; region which means a selected 
+                    region in the image viewer. Users can select a region by clicking one on the 
+                    image viewer, or by clicking a region entry on the region list widget. Stokes 
+                    profile plot of the selected region will be updated accordingly. If no region is 
+                    selected, &quot;Active&quot; region defaults to cursor.</p>
                 <h3 id="responsive-and-progressive-profile-update">Responsive and progressive profile update</h3>
-                <p>When region Stokes profiles are requested, depending on the performance of the server, users may see profiles are updated piece by piece in regular interval. This feature provides a visual progress update for better user
-                    experience. In addition, if users move a region while profiles are being updating, the old calculations will be terminated immediately and calculations of the new region Stokes profiles will start and users will see
-                    partial profiles in seconds.</p>
+                <p>When region Stokes profiles are requested, depending on the performance of the 
+                    server, users may see profiles are updated piece by piece in regular interval. This 
+                    feature provides a visual progress update for better user experience. In addition, if 
+                    users move a region while profiles are being updating, the old calculations will be 
+                    terminated immediately and calculations of the new region Stokes profiles will start 
+                    and users will see partial profiles in seconds.</p>
                 <h3 id="spectral-conventions-and-reference-frame">Spectral conventions and reference frame</h3>
-                <p>With the Stokes analysis settings dialogue, users can change the spectral convention, including:</p>
+                <p>With the &quot;Conversion&quot; tab of the Stokes analysis settings dialogue, users 
+                    can change the spectral convention, including:</p>
                 <ul>
                     <li>Radio velocity (km/s, m/s)</li>
                     <li>Optical velocity (km/s, m/s)</li>
@@ -40,31 +53,41 @@ export class StokesAnalysisHelpComponent extends React.Component {
                     <li>BARY: barycentric, the rest-frame of the solar-system barycenter</li>
                     <li>TOPO: topocentric, the observer's rest-frame on Earth</li>
                 </ul>
-                <p>Note that depending on the integrity of image headers, some conversions may not be possible.</p>
+                <p>Note that depending on the integrity of image headers, some conversions may not be 
+                    possible.</p>
+                <h3 id="profile-smoothing">Profile smoothing</h3>
+                <p>The displayed profiles can be smoothed via the &quot;Smoothing&quot; tab of the 
+                    Stokes analysis settings dialogue (the cog icon). A shortcut button 
+                    of the &quot;Smoothing&quot; tab can be found at the top-right corner of the widget.</p>    
                 <h3 id="interactivity-zoom-pan-changing-channel">Interactivity: zoom, pan, changing channel</h3>
                 <p>The x and y ranges of the Stokes profile plot can be modified by</p>
                 <ul>
-                    <li><code>scrolling wheel</code> (up to zoom in and down to zoom out with respect to the cursor position)</li>
+                    <li><code>scrolling wheel</code> (up to zoom in and down to zoom out with 
+                        respect to the cursor position)</li>
                     <li><code>click-and-drag</code> horizontally to zoom in x</li>
                     <li><code>click-and-drag</code> vertically to zoom in y</li>
                     <li><code>click-and-drag</code> diagonally to zoom in both x and y</li>
                     <li><code>double-click</code> to reset x and y ranges</li>
                     <li><code>shift + click-and-drag</code> to pan in x</li>
                 </ul>
-                <p>In addition, the x and y ranges can be explicitly set in the Stokes analysis settings dialogue.</p>
-                <p>Users may click on the Stokes profile plot to switch to a channel (as indicated by a red vertical line) and view the image in the image viewer. The red line is draggable and acts equivalently like the channel slider in
-                    the animator widget.</p>
+                <p>Users may click on the Stokes profile plot to switch to a channel (as indicated by a 
+                    red vertical line) and view the image in the image viewer. The red line is 
+                    draggable and acts equivalently like the channel slider in the animator widget.</p>
                 <h3 id="exports">Exports</h3>
-                <p>The Stokes profile plots can be exported as a png file or a text file in tsv format via the buttons at the bottom-right corner (shown when hovering over the plot).</p>
+                <p>The Stokes profile plots can be exported as a png file or a text file in tsv format 
+                    via the buttons at the bottom-right corner (shown when hovering over the plot).</p>
                 <h3 id="plot-cosmetics">Plot cosmetics</h3>
-                <p>The appearance of the Stokes profile plot is customizable via the Stokes analysis settings dialogue (the cog icon). Supported options are:</p>
+                <p>The appearance of the Stokes profile plot is customizable via 
+                    the &quot;Line Plot Styling&quot; tab of the Stokes analysis settings 
+                    dialogue (the cog icon). Supported options are:</p>
                 <ul>
                     <li>colors of Stokes Q and Stokes U profiles</li>
                     <li>plot styles including steps (default), lines, and dots</li>
                     <li>line width for steps or lines</li>
                     <li>point size for dots</li>
                 </ul>
-                <p>In addition, the appearance of the scatter plot can be customized too, including:</p>
+                <p>In addition, the appearance of the scatter plot can be customized with 
+                    the &quot;Scatter Plot Styling&quot; tab too, including:</p>
                 <ul>
                     <li>Colormap</li>
                     <li>Symbol size</li>
