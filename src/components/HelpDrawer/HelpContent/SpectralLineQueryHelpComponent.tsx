ef6718e9caba35549833dae1069fab201ae034a8
kswang1029 <kswang@asiaa.sinica.edu.tw>
2020-09-08 19:41:38 +0800

    update line query help

--- src/components/HelpDrawer/HelpContent/SpectralLineQueryHelpComponent.tsx
+++ src/components/HelpDrawer/HelpContent/SpectralLineQueryHelpComponent.tsx
@@ -16,7 +16,7 @@ export class SpectralLineQueryHelpComponent extends React.Component {
                 </ul>                
                 <h4>IMPORTANT NOTE</h4>
                 <p>Currently, the Splatalogue query service is under active development. Unexpected query results might happen. When users believe there is something wrong, please contact the <a href="mailto:carta_helpdesk@asiaa.sinica.edu.tw">helpdesk</a> or file an issue on <a href="https://github.com/CARTAvis/carta/issues">Github</a> (recommended).</p>
-                
+                <br />
                 <p>Once a query is successfully made, the line catalogue will be displayed in the tables. The upper table shows the column information in the catalogue with options to show or hide a specific column. The actually line catalogue is displayed in the lower table.</p>
                 <p>The &quot;Shifted Frequency&quot; column is computed based on the user input of a velocity or a redshift. This &quot;Shifted Frequency&quot; is adopted for line ID overlay on a spectral profiler widget.</p>
                 <p>The line catalogue table accepts sub-filters such as partial string match or value range. This would allow users to select potential target lines quickly. Users can use the checkbox to select a set of lines to be overplotted on a spectral profiler widget. The maximum number of line ID overlay is 1000.</p>
