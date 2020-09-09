e13f354687a70ca2601c73adaca2da001912dab6
kswang1029 <kswang@asiaa.sinica.edu.tw>
2020-09-09 15:01:15 +0800

    update line query help

--- src/components/HelpDrawer/HelpContent/SpectralLineQueryHelpComponent.tsx
+++ src/components/HelpDrawer/HelpContent/SpectralLineQueryHelpComponent.tsx
@@ -18,10 +18,10 @@ export class SpectralLineQueryHelpComponent extends React.Component {
                 <h4>IMPORTANT NOTE</h4>
                 <p>Currently, the Splatalogue query service is under active development. Unexpected query results might happen. When users believe there is something wrong, please contact the <a href="mailto:carta_helpdesk@asiaa.sinica.edu.tw">helpdesk</a> or file an issue on <a href="https://github.com/CARTAvis/carta/issues">Github</a> (recommended).</p>
                 <br />
-                <p>Once a query is successfully made, the line catalogue will be displayed in the tables. The upper table shows the column information in the catalogue with options to show or hide a specific column. The actually line catalogue is displayed in the lower table.</p>
+                <p>Once a query is successfully made, the line catalogue will be displayed in the tables. The upper table shows the column information in the catalogue with options to show or hide a specific column. The actual line catalogue is displayed in the lower table.</p>
                 <p>The &quot;Shifted Frequency&quot; column is computed based on the user input of a velocity or a redshift. This &quot;Shifted Frequency&quot; is adopted for line ID overlay on a spectral profiler widget.</p>
-                <p>The line catalogue table accepts sub-filters such as partial string match or value range. This would allow users to select potential target lines quickly. Users can use the checkbox to select a set of lines to be overplotted on a spectral profiler widget. The maximum number of line ID overlay is 1000.</p>
-                <p>The sorting function will be available in a future release.</p>
+                <p>Users can use the checkbox to select a set of lines to be overplotted on a spectral profiler widget. The maximum number of line ID overlay is 1000.</p>
+                <p>The sorting and filtering functions will be available in a future release.</p>
                 <p>The text labels of the line ID overlay are shown dynamically based on the zoom level of a profile. Different line ID overlays (with different velocity shifts) can be created on different spectral profilers widgets via the &quot;Spectral Profiler&quot; dropdown. By clicking the &quot;Clear&quot; button, the line ID overlay on the selected &quot;Spectral Profiler&quot; will be removed.</p>                    
             </div>
         );
