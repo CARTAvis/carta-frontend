3fadfe4f34a579f6b4ecffaed6af50dc16f2c360
kswang1029 <kswang@asiaa.sinica.edu.tw>
2020-09-09 15:07:39 +0800

    update line query help

--- src/components/HelpDrawer/HelpContent/SpectralLineQueryHelpComponent.tsx
+++ src/components/HelpDrawer/HelpContent/SpectralLineQueryHelpComponent.tsx
@@ -8,7 +8,7 @@ export class SpectralLineQueryHelpComponent extends React.Component {
         return (
             <div>
                 <p><ImageComponent light={headLineQueryButton} dark={headLineQueryButton_d} width="90%"/></p>
-                <p>CARTA supports spectral line ID overlay on a spectral profiler widget with a query to the Splatalogue service (https://splatalogue.online). The query is made by defining a spectral range in frequency or wavelength and optionally a lower limit of CDMS/JPL line intensity (log). The spectral range can be defined as from-to or a center with a width.</p> 
+                <p>CARTA supports an initial implementation of spectral line ID overlay on a spectral profiler widget with a query to the Splatalogue service (https://splatalogue.online). The query is made by defining a spectral range in frequency or wavelength and optionally a lower limit of CDMS/JPL line intensity (log). The spectral range can be defined as from-to or a center with a width.</p> 
                 <h4>QUERY LIMITATION</h4>    
                 <ul>
                     <li>The allowed maximum query range, equivalent in frequency, is 20 GHz.</li>
