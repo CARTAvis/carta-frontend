6d73840f1721f62512aa2247cf17359a56bd2eec
duidae <duidae@gmail.com>
2020-09-28 19:28:21 +0800

    Refine expression

--- src/components/Shared/ProfilerInfo/ProfilerInfoComponent.tsx
+++ src/components/Shared/ProfilerInfo/ProfilerInfoComponent.tsx
@@ -7,11 +7,9 @@ interface ProfilerInfoComponentProps {
 
 export const ProfilerInfoComponent: React.FC<ProfilerInfoComponentProps> = (props) => {
     let infoString = "";
-    if (props.info?.length > 0) {
-        props.info.forEach((info, index) => {
-            infoString = index === 0 ? info : (infoString + ", " + info);
-        });
-    }
+    props.info?.forEach((info, index) => {
+        infoString = index === 0 ? info : (infoString + ", " + info);
+    });
 
     return (
         <div className="profiler-info">
