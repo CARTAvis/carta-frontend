1477b854b374bc109c4d64b7a66ed87e49afeaa4
許守傑 Shou-Chieh Hsu (Jason) <duidae@gmail.com>
2020-10-06 16:37:50 +0800

    Jason/issue 435 (#1221)
    * Check whether cursor is inside image or not
    
    * Correct with rounding

--- src/stores/FrameStore.ts
+++ src/stores/FrameStore.ts
@@ -1073,10 +1073,16 @@ export class FrameStore {
             }
         }
 
+        const imageX = Math.round(cursorPosImageSpace.x);
+        const imageY = Math.round(cursorPosImageSpace.y);
+        const isInsideImage = imageX >= 0 && imageX < this.frameInfo.fileInfoExtended.width &&
+                                imageY >= 0 && imageY < this.frameInfo.fileInfoExtended.height;
+
         return {
             posImageSpace: cursorPosImageSpace,
+            isInsideImage: isInsideImage,
             posWCS: cursorPosWCS,
-            infoWCS: cursorPosFormatted,
+            infoWCS: cursorPosFormatted
         };
     }
 
