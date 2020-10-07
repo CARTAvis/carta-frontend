9a49a70c47034bc3dc6b8f111683faa9a3d66007
veggiesaurus <accomrie@gmail.com>
2020-10-07 13:42:31 +0200

    info update

--- src/models/CARTAInfo.ts
+++ src/models/CARTAInfo.ts
@@ -1,6 +1,6 @@
 export const CARTA_INFO = {
     acronym: "CARTA",
-    version: "1.4.0-alpha.6",
-    date: "20200813",
+    version: "2.0.0-dev.1",
+    date: "20201007",
     fullName: "Cube Analysis and Rendering Tool for Astronomy"
 };
