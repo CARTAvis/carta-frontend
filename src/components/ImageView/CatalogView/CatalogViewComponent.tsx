fe57167fff2e66453ea0e4d00f19dd1e468d1235
Angus Comrie <accomrie@gmail.com>
2020-10-28 09:34:24 +0200

    solves issue with plotly types, adds package-lock to repo (#1247)
    * solves issue with plotly types, adds package-lock to repo
    
    * removed typo newline

--- src/components/ImageView/CatalogView/CatalogViewComponent.tsx
+++ src/components/ImageView/CatalogView/CatalogViewComponent.tsx
@@ -28,7 +28,7 @@ export class CatalogViewComponent extends React.Component<CatalogViewComponentPr
         let coordsData = new Map<number, Plotly.Data>();
         catalogStore.catalogData.forEach((catalog, key) => {
             if (!catalog.showSelectedData) {
-                let unSelecteData: Plotly.Data = {};
+                let unSelecteData: Partial<Plotly.PlotData> = {};
                 unSelecteData.type = "scattergl";
                 unSelecteData.mode = "markers";
                 unSelecteData.hoverinfo = "none";
@@ -52,7 +52,7 @@ export class CatalogViewComponent extends React.Component<CatalogViewComponentPr
             const selectedPoints = profileStore.selectedPointIndices;
             const selectedPointSize = selectedPoints.length;
             const fileId = profileStore.catalogFileId;
-            let selecteData: Plotly.Data = {};
+            let selecteData: Partial<Plotly.PlotData> = {};
             if (selectedPointSize > 0) {
                 selecteData.type = "scattergl";
                 selecteData.mode = "markers";
