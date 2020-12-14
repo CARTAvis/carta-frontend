2f9cf829827ab878ac26957fd15977fad3dcc4db
duidae <duidae@gmail.com>
2020-12-14 20:00:40 +0800

    Refine file name

--- src/stores/FrameStore.ts
+++ src/stores/FrameStore.ts
@@ -100,7 +100,7 @@ export class FrameStore {
     @computed get filename(): string {
         // hdu extension name is in field 3 of fileInfoExtended computed entries
         return this.frameInfo?.hdu !== "" && this.frameInfo?.fileInfoExtended?.computedEntries?.length >= 3 ?
-            `${this.frameInfo.fileInfo.name}_HDU_${this.frameInfo.hdu}_${this.frameInfo.fileInfoExtended.computedEntries[2]?.value}` :
+            `${this.frameInfo.fileInfo.name}.HDU_${this.frameInfo.hdu}_${this.frameInfo.fileInfoExtended.computedEntries[2]?.value}` :
             this.frameInfo.fileInfo.name;
     }
 
