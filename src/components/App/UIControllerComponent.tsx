4a931a6a7a13742e2d75ac2a2adad49636285f1b
veggiesaurus <accomrie@gmail.com>
2021-03-03 14:05:24 +0200

    adjusted ordering

--- src/components/App/UIControllerComponent.tsx
+++ src/components/App/UIControllerComponent.tsx
@@ -18,18 +18,18 @@ export class UIControllerComponent extends React.Component {
     render() {
         return (
             <React.Fragment>
-                <SplashScreenComponent/>
                 <RootMenuComponent/>
-                <AboutDialogComponent/>
                 <RegionDialogComponent/>
-                <SaveLayoutDialogComponent/>
                 <ContourDialogComponent/>
                 <FileInfoDialogComponent/>
-                <PreferenceDialogComponent/>
                 <FileBrowserDialogComponent/>
+                <PreferenceDialogComponent/>
+                <SaveLayoutDialogComponent/>
                 <DebugExecutionDialogComponent/>
+                <AboutDialogComponent/>
                 <ExternalPageDialogComponent/>
                 <HelpDrawerComponent/>
+                <SplashScreenComponent/>
             </React.Fragment>
         );
     }
