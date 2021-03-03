5976f4ec6456ebedb577460e6256348da579c7c3
thchang <tienhaochang703@gmail.com>
2021-03-03 20:38:47 +0800

    fixed transparent background color in the exported image of imageviewer

--- src/stores/AppStore.ts
+++ src/stores/AppStore.ts
@@ -1726,7 +1726,7 @@ export class AppStore {
 
     exportImage = (): boolean => {
         if (this.activeFrame) {
-            const backgroundColor = this.preferenceStore.transparentExportedImage ? null : (this.darkTheme ? Colors.DARK_GRAY3 : Colors.LIGHT_GRAY5);
+            const backgroundColor = this.preferenceStore.transparentExportedImage ? "rgba(255, 255, 255, 0)" : (this.darkTheme ? Colors.DARK_GRAY3 : Colors.LIGHT_GRAY5);
             const composedCanvas = getImageCanvas(this.overlayStore.padding, backgroundColor);
             if (composedCanvas) {
                 composedCanvas.toBlob((blob) => {
