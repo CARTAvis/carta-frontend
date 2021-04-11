d7aee55fdee4e8540cb0d9e5c1aa8940cc36e816
duidae <duidae@gmail.com>
2021-04-11 16:02:49 +0800

    Add color to legend

--- src/components/SpectralProfiler/SpectralProfilerInfoComponent/SpectralProfilerInfoComponent.tsx
+++ src/components/SpectralProfiler/SpectralProfilerInfoComponent/SpectralProfilerInfoComponent.tsx
@@ -10,7 +10,21 @@ export const SpectralProfilerInfoComponent: React.FC<{profileInfo: ProfileInfo[]
     return (
         <div className="spectral-profiler-info">
             {props.profileInfo?.map(info => {
-                return <pre>{info?.color}{info.infoString}</pre>;
+                const colorSquare = info?.color ?
+                    <div
+                        style={{
+                            height: '12px',
+                            width: '12px',
+                            margin: '0 5px 0 0',
+                            background: info.color
+                        }}
+                    /> : null;
+                return (
+                    <div className="profile-legend">
+                        {colorSquare}
+                        <pre>{info.infoString}</pre>
+                    </div>
+                );
             })}
         </div>
     );
