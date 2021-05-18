cfb80923988b6c15ee8ea055d624d3698cd67b7e
yuhsuan <r07222045@ntu.edu.tw>
2021-05-18 09:50:57 +0800

    adjust unit

--- wasm_src/ast_wrapper/ast_wrapper.cc
+++ wasm_src/ast_wrapper/ast_wrapper.cc
@@ -202,7 +202,7 @@ void plotDistText(AstFrameSet* wcsinfo, AstPlot* plot, double* start, double* fi
 
     string distString;
     const char* unit = astGetC(wcsinfo, "Unit(1)");
-    if (strcmp(unit, "degrees") == 0 || strcmp(unit, "hh:mm:ss.ss") == 0)
+    if (strstr(unit, "degree") != nullptr || strstr(unit, "hh:mm:s") != nullptr)
     {
         if (dist < M_PI / 180.0 / 60.0)
         {
