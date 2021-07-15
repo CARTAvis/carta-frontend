9f9fa629bb5aa89d6d8ad40b2d0ccc0aff27d7cc
ajm-asiaa <ajm@asiaa.sinica.edu.tw>
2021-07-15 16:55:52 +0800

    removing Docker interactive mode as Jenkins has no TTY

--- build_wasm_libs_docker.sh
+++ build_wasm_libs_docker.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 echo "Building WebAssembly libraries inside Docker container"
-docker run -it --rm -u $(id -u):$(id -g) -v `pwd`:/src emscripten/emsdk:2.0.14 /src/wasm_libs/build_libs.sh
+if [ -t 0 ] && [ -t 1 ]; then
+  docker run -it --rm -u $(id -u):$(id -g) -v `pwd`:/src emscripten/emsdk:2.0.14 /src/wasm_libs/build_libs.sh
+else
+  # Non interactive version needed by Jenkins as no TTY is available 
+  docker run --rm -u $(id -u):$(id -g) -v `pwd`:/src emscripten/emsdk:2.0.14 /src/wasm_libs/build_libs.sh
+fi
