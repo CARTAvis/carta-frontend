319d12a79a8370030bf82c8cbda6c9a6fbbffbfc
ajm-asiaa <ajm@asiaa.sinica.edu.tw>
2021-07-19 14:00:07 +0800

    removing it flag from the build docker scripts

--- build_wasm_libs_docker.sh
+++ build_wasm_libs_docker.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 echo "Building WebAssembly libraries inside Docker container"
-if [ -t 0 ] && [ -t 1 ]; then
-  docker run -it --rm -u $(id -u):$(id -g) -v `pwd`:/src emscripten/emsdk:2.0.14 /src/wasm_libs/build_libs.sh
-else
-  # Non interactive version needed by Jenkins as no TTY is available 
-  docker run --rm -u $(id -u):$(id -g) -v `pwd`:/src emscripten/emsdk:2.0.14 /src/wasm_libs/build_libs.sh
-fi
+docker run --rm -u $(id -u):$(id -g) -v `pwd`:/src emscripten/emsdk:2.0.14 /src/wasm_libs/build_libs.sh
--- build_wasm_wrappers_docker.sh
+++ build_wasm_wrappers_docker.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 echo "Building WebAssembly wrappers inside Docker container"
-if [ -t 0 ] && [ -t 1 ]; then
-  docker run -it --rm -u $(id -u):$(id -g) -v `pwd`:/src emscripten/emsdk:2.0.14 /src/wasm_src/build_wrappers.sh
-else
-  # Non interactive version needed by Jenkins as no TTY is available 
-  docker run --rm -u $(id -u):$(id -g) -v `pwd`:/src emscripten/emsdk:2.0.14 /src/wasm_src/build_wrappers.sh
-fi
+docker run --rm -u $(id -u):$(id -g) -v `pwd`:/src emscripten/emsdk:2.0.14 /src/wasm_src/build_wrappers.sh
