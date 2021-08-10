662f0dc4e9379c757bd0dda271a2042f078aa48f
Jorge Lopez <jorgext@gmail.com>
2021-08-10 04:24:38 -0400

    set starting directory in AppStore

--- src/stores/AppStore.ts
+++ src/stores/AppStore.ts
@@ -1179,6 +1179,7 @@ export class AppStore {
             try {
                 await this.connectToServer();
                 await this.preferenceStore.fetchPreferences();
+                await this.fileBrowserStore.setStartingDirectory();
                 await this.layoutStore.fetchLayouts();
                 await this.snippetStore.fetchSnippets();
 
