1629223952ad78765efef8c1b69b35b5f12fd6cf
Angus Comrie <accomrie@gmail.com>
2021-08-10 09:23:08 +0200

    Angus/1571 raster image not updated (#1615)
    * force clear of GPU cache for inactive images when changing channels
    
    * renamed function

--- src/services/TileService.ts
+++ src/services/TileService.ts
@@ -243,8 +243,9 @@ export class TileService {
         }
     }
 
-    updateInactiveFileChannel(fileId: number, channel: number, stokes: number) {
+    updateHiddenFileChannels(fileId: number, channel: number, stokes: number) {
         this.clearCompressedCache(fileId);
+        this.clearGPUCache(fileId);
         this.channelMap.set(fileId, {channel, stokes});
         this.backendService.setChannels(fileId, channel, stokes, {});
     }
