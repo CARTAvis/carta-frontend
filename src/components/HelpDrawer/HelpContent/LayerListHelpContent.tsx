c5ca9096ede2afd6f2b1c8f43edca2d0574da1cd
duidae <duidae@gmail.com>
2021-09-24 11:20:21 +0800

    Restore to separate files

---
+++ src/components/HelpDrawer/HelpContent/LayerListHelpContent.tsx
@@ -0,0 +1,31 @@
+import {ImageComponent} from "../ImageComponent";
+import headLayerButton from "static/help/head_layer_button.png";
+import headLayerButton_d from "static/help/head_layer_button_d.png";
+
+export const LAYER_LIST_HELP_CONTENT = (
+    <div>
+        <p>
+            <ImageComponent light={headLayerButton} dark={headLayerButton_d} width="90%" />
+        </p>
+        <p>
+            The image list widget displays loaded images as a list, which includes the image name, rendering layers (R for raster and C for contours), layer visibility state, WCS matching state, color range matching state, channel index,
+            and Stokes index. The channel index and Stokes index are synchronized with the animator.
+        </p>
+        <p>
+            You may click <code>R</code> and/or <code>C</code> to hide/show a layer. For example, if you want to generate an image with contours only, you can hide raster layers by clicking <code>R</code>.
+        </p>
+        <p>
+            Per image, you can click the <code>XY</code> button to enable/disable spatial matching and click the <code>Z</code> button to enable/disable spectral matching. To match color range to the reference image, click the{" "}
+            <code>R</code> button.
+        </p>
+        <p>
+            To change reference image, <code>right-click</code> on a row to bring up the menu.
+        </p>
+        <p>
+            To close an image (or images), <code>right-click</code> on a row to bring up the menu.
+        </p>
+        <p>
+            The list order reflects the order of the image slider in the animator. To change the order, <code>click</code> to select an entry and <code>drag-and-drop</code> it to the desired position.
+        </p>
+    </div>
+);
---
+++ src/components/HelpDrawer/HelpContent/StokesHypercubeDialogHelpContent.tsx
@@ -0,0 +1,9 @@
+export const STOKES_HYPERCUBE_DIALOG_HELP_CONTENT = (
+    <div>
+        <h3>Form a Stokes hypercube</h3>
+        <p>
+            This dialog allows user to confirm the auto-identification of the Stokes parameters from the image list and make corrections when necessary. The auto-identification obtains information from the image headers. If Stokes
+            information is not available in the headers, CARTA will make a guess from the file names. Otherwise, you need to assign the Stokes parameters manually.
+        </p>
+    </div>
+);
