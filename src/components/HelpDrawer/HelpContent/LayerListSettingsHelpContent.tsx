be13905c1c54ae4a5aab6b081ff276977764db8a
duidae <duidae@gmail.com>
2021-10-12 10:15:23 +0800

    Format

--- src/components/HelpDrawer/HelpContent/LayerListSettingsHelpContent.tsx
+++ src/components/HelpDrawer/HelpContent/LayerListSettingsHelpContent.tsx
@@ -1,3 +1,1 @@
-export const LAYER_LIST_SETTINGS_HELP_CONTENT = (
-    <div>Image list setting help content</div>
-);
+export const LAYER_LIST_SETTINGS_HELP_CONTENT = <div>Image list setting help content</div>;
