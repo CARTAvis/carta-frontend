8797feefc656cb3f4062102c1f71ed5dcb2aa079
yuhsuan <r07222045@ntu.edu.tw>
2021-10-28 15:29:30 +0800

    minor change

--- src/components/ImageView/Toolbar/ToolbarComponent.tsx
+++ src/components/ImageView/Toolbar/ToolbarComponent.tsx
@@ -320,7 +320,7 @@ export class ToolbarComponent extends React.Component<ToolbarComponentProps> {
                             position={tooltipPosition}
                             content={
                                 <span>
-                                    {`Export image`}
+                                    Export image
                                     {this.exportImageTooltip()}
                                 </span>
                             }
