fdb8664af9101f9ce05bd443169332229208042e
yuhsuan <r07222045@ntu.edu.tw>
2021-11-09 11:32:32 +0800

    add style to filename

--- src/components/LayerList/LayerListSettingsPanelComponent/LayerListSettingsPanelComponent.tsx
+++ src/components/LayerList/LayerListSettingsPanelComponent/LayerListSettingsPanelComponent.tsx
@@ -13,6 +13,8 @@ export enum LayerListSettingsTabs {
     REST_FREQ
 }
 
+const FILENAME_END_LEN = 15
+
 @observer
 export class LayerListSettingsPanelComponent extends React.Component<WidgetProps> {
     @observable selectedTabId: TabId = LayerListSettingsTabs.MATCHING;
@@ -25,7 +27,7 @@ export class LayerListSettingsPanelComponent extends React.Component<WidgetProps
         return {
             id: "layer-list-settings",
             type: "floating-settings",
-            minWidth: 280,
+            minWidth: 360,
             minHeight: 225,
             defaultWidth: 400,
             defaultHeight: 375,
@@ -61,31 +63,43 @@ export class LayerListSettingsPanelComponent extends React.Component<WidgetProps
 
         const restFreqPanel = (
             <div className="panel-container">
-                {appStore.frames.map((frame, index) => (
-                    <React.Fragment key={index}>
-                        <FormGroup inline={true} label="Source" className="name-text" disabled={frame.isRestFreqEditable}>
-                            <Text className={classNames({"disabled": frame.isRestFreqEditable}, {"dark": appStore.darkTheme})} ellipsize={true}>
-                                {frame.filename}
-                            </Text>
-                        </FormGroup>
-                        <div className="freq-input">
-                            <ClearableNumericInputComponent
-                                label="Rest frequency"
-                                value={0}
-                                disabled={frame.isRestFreqEditable}
-                                placeholder="rest frequency"
-                                selectAllOnFocus={true}
-                                onValueChanged={() => {}}
-                                onValueCleared={() => {}}
-                                resetDisabled={false}
-                                tooltipContent={"default"}
-                                tooltipPlacement={"bottom"}
-                            />
-                            <HTMLSelect disabled={frame.isRestFreqEditable} options={Object.values(FrequencyUnit)} value={"Hz"} onChange={ev => console.log(ev.currentTarget.value as FrequencyUnit)} />
-                        </div>
-                        {index !== appStore.frames.length - 1 ? <MenuDivider/> : null}
-                    </React.Fragment>
-                ))}
+                {appStore.frames.map((frame, index) => {
+                    const isActive = index === appStore.activeFrameIndex;
+                    const style = classNames({"disabled": !frame.isRestFreqEditable}, {"dark": appStore.darkTheme}, {"active": isActive});
+                    const filenameEndLen = FILENAME_END_LEN - (isActive ? 9 : 0);
+                    return (
+                        <React.Fragment key={index}>
+                            <FormGroup inline={true} label="Source" className="name-text" disabled={!frame.isRestFreqEditable}>
+                                <Text className={style} ellipsize={true}>
+                                    {`${index}: `}{frame.filename.slice(0, -1 - filenameEndLen)}
+                                </Text>
+                                <Text className={style}>
+                                    {frame.filename.slice(-1 - filenameEndLen, -1)}
+                                </Text>
+                                {isActive ?
+                                    <Text className={style}>
+                                        {"\u00a0(Active)"}
+                                    </Text>
+                                : null}
+                            </FormGroup>
+                            <div className="freq-input">
+                                <ClearableNumericInputComponent
+                                    label="Rest frequency"
+                                    value={0}
+                                    disabled={!frame.isRestFreqEditable}
+                                    placeholder="rest frequency"
+                                    selectAllOnFocus={true}
+                                    onValueChanged={() => {}}
+                                    onValueCleared={() => {}}
+                                    resetDisabled={false}
+                                    tooltipContent={"default"}
+                                    tooltipPlacement={"bottom"}
+                                />
+                                <HTMLSelect disabled={!frame.isRestFreqEditable} options={Object.values(FrequencyUnit)} value={"Hz"} onChange={ev => console.log(ev.currentTarget.value as FrequencyUnit)} />
+                            </div>
+                            {index !== appStore.frames.length - 1 ? <MenuDivider/> : null}
+                        </React.Fragment>
+                )})}
             </div>
         );
 
