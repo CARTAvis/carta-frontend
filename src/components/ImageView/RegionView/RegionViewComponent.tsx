c8bd84ef5b7b5c5072622187d3ebcaecb8c36474
duidae <duidae@gmail.com>
2021-11-23 15:04:43 +0800

    Refine a little bit

--- src/components/ImageView/RegionView/RegionViewComponent.tsx
+++ src/components/ImageView/RegionView/RegionViewComponent.tsx
@@ -472,8 +472,8 @@ export class RegionViewComponent extends React.Component<RegionViewComponentProp
         if (stage && refCenterMovement && isFinite(refCenterMovement.x) && isFinite(refCenterMovement.y) && isFinite(refFrameZoom)) {
             stage.scale({x: refFrameZoom, y: refFrameZoom});
             const origin = scale2D({x: this.props.width / 2, y: this.props.height / 2}, 1 - refFrameZoom);
-            const centerMovementCanvas = scale2D({x: refCenterMovement.x / devicePixelRatio, y: -refCenterMovement.y / devicePixelRatio}, refFrameZoom);
-            const newOrigin = add2D(origin, centerMovementCanvas);
+            const centerMovementCanvas = scale2D({x: refCenterMovement.x, y: -refCenterMovement.y}, refFrameZoom);
+            const newOrigin = add2D(origin, scale2D(centerMovementCanvas, 1 / devicePixelRatio));
             // Correct the origin if region view is ever resized
             const correctedOrigin = subtract2D(newOrigin, scale2D(this.stageResizeOffset, refFrameZoom));
             stage.position(correctedOrigin);
