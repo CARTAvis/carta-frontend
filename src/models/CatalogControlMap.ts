f83d932769bb38d7290127873f85cd9a8af1d40d
yuhsuan <r07222045@ntu.edu.tw>
2022-05-27 10:50:41 +0800

    refactor controlMap and catalogControlMap

--- src/models/CatalogControlMap.ts
+++ src/models/CatalogControlMap.ts
@@ -11,9 +11,9 @@ export class CatalogControlMap {
     readonly height: number;
     minPoint: Point2D;
     maxPoint: Point2D;
-    private gl2: WebGL2RenderingContext;
     private texture: WebGLTexture;
     private grid: Float32Array;
+    private gl: WebGL2RenderingContext;
     private boundaryUpdated: boolean;
 
     constructor(src: FrameStore, dst: FrameStore, astTransform: AST.FrameSet, width: number, height: number) {
@@ -21,20 +21,13 @@ export class CatalogControlMap {
         this.destination = dst;
         this.width = width;
         this.height = height;
-
         this.minPoint = {x: Number.MAX_VALUE, y: Number.MAX_VALUE};
         this.maxPoint = {x: -Number.MAX_VALUE, y: -Number.MAX_VALUE};
         this.boundaryUpdated = false;
     }
 
-    hasTextureForContext = (gl2: WebGL2RenderingContext) => {
-        return gl2 === this.gl2 && this.texture && gl2.isTexture(this.texture);
-    };
-
     updateCatalogBoundary = () => {
         const catalogStore = CatalogStore.Instance;
-        const paddingX = Math.ceil(this.source.frameInfo.fileInfoExtended.width / this.width);
-        const paddingY = Math.ceil(this.destination.frameInfo.fileInfoExtended.height / this.height);
         const srcMinMax = catalogStore.getFrameMinMaxPoints(this.source.frameInfo.fileId);
         const dstMinMax = catalogStore.getFrameMinMaxPoints(this.destination.frameInfo.fileId);
         const minX = srcMinMax.minX < dstMinMax.minX ? srcMinMax.minX : dstMinMax.minX;
@@ -42,41 +35,63 @@ export class CatalogControlMap {
         const maxX = srcMinMax.maxX > dstMinMax.maxX ? srcMinMax.maxX : dstMinMax.maxX;
         const maxY = srcMinMax.maxY > dstMinMax.maxY ? srcMinMax.maxY : dstMinMax.maxY;
         if (this.minPoint.x > minX || this.minPoint.y > minY || this.maxPoint.x < maxX || this.maxPoint.y < maxY) {
-            this.minPoint = {x: minX - paddingX, y: minY - paddingY};
-            this.maxPoint = {x: maxX + paddingX, y: maxY + paddingY};
+            this.setMinMaxPoint(minX, minY, maxX, maxY);
             this.boundaryUpdated = true;
+            this.setGrid();
+        }
+    };
 
+    setMinMaxPoint = (minX, minY, maxX, maxY) => {
+        const paddingX = Math.ceil(this.source.frameInfo.fileInfoExtended.width / this.width);
+        const paddingY = Math.ceil(this.source.frameInfo.fileInfoExtended.height / this.height);
+        this.minPoint = {x: minX - paddingX, y: minY - paddingY};
+        this.maxPoint = {x: maxX + paddingX, y: maxY + paddingY};
+    };
+
+    setGrid = (astTransform?: AST.FrameSet) => {
+        let cleanUpTransform: boolean = false;
+
+        if (!astTransform || astTransform < 0) {
             const copySrc = AST.copy(this.source.wcsInfo);
             const copyDest = AST.copy(this.destination.wcsInfo);
             AST.invert(copySrc);
             AST.invert(copyDest);
-            const astTransform = AST.convert(copySrc, copyDest, "");
+            astTransform = AST.convert(copySrc, copyDest, "");
             AST.deleteObject(copySrc);
             AST.deleteObject(copyDest);
+            cleanUpTransform = true;
+        }
 
-            this.minPoint = {x: minX - paddingX, y: minY - paddingY};
-            this.maxPoint = {x: maxX + paddingX, y: maxY + paddingY};
-            this.grid = AST.getTransformGrid(astTransform, this.minPoint.x, this.maxPoint.x, this.width, this.minPoint.y, this.maxPoint.y, this.height, true);
+        this.grid = AST.getTransformGrid(astTransform, this.minPoint.x, this.maxPoint.x, this.width, this.minPoint.y, this.maxPoint.y, this.height, true);
 
+        if (cleanUpTransform) {
             AST.deleteObject(astTransform);
         }
     };
 
-    getTextureX = (gl2: WebGL2RenderingContext) => {
-        if (gl2 !== this.gl2 || !this.texture || this.boundaryUpdated) {
+    getTextureX = (gl: WebGL2RenderingContext) => {
+        if (gl !== this.gl || !this.texture || this.boundaryUpdated) {
             this.boundaryUpdated = false;
             // Context has changed, texture needs to be regenerated
-            this.gl2 = gl2;
-            this.texture = this.gl2.createTexture();
-            this.gl2.activeTexture(GL2.TEXTURE1);
-            this.gl2.bindTexture(GL2.TEXTURE_2D, this.texture);
-            this.gl2.texParameteri(GL2.TEXTURE_2D, GL2.TEXTURE_MIN_FILTER, GL2.NEAREST);
-            this.gl2.texParameteri(GL2.TEXTURE_2D, GL2.TEXTURE_MAG_FILTER, GL2.NEAREST);
-            this.gl2.texParameteri(GL2.TEXTURE_2D, GL2.TEXTURE_WRAP_S, GL2.CLAMP_TO_EDGE);
-            this.gl2.texParameteri(GL2.TEXTURE_2D, GL2.TEXTURE_WRAP_T, GL2.CLAMP_TO_EDGE);
-            this.gl2.texImage2D(GL2.TEXTURE_2D, 0, GL2.RG32F, this.width, this.height, 0, GL2.RG, GL2.FLOAT, this.grid);
+            this.createTexture(gl);
         }
         return this.texture;
+    };
+
+    createTexture = (gl: WebGL2RenderingContext) => {
+        this.gl = gl;
+        this.texture = this.gl.createTexture();
+        this.gl.activeTexture(GL2.TEXTURE1);
+        this.gl.bindTexture(GL2.TEXTURE_2D, this.texture);
+        this.gl.texParameteri(GL2.TEXTURE_2D, GL2.TEXTURE_MIN_FILTER, GL2.NEAREST);
+        this.gl.texParameteri(GL2.TEXTURE_2D, GL2.TEXTURE_MAG_FILTER, GL2.NEAREST);
+        this.gl.texParameteri(GL2.TEXTURE_2D, GL2.TEXTURE_WRAP_S, GL2.CLAMP_TO_EDGE);
+        this.gl.texParameteri(GL2.TEXTURE_2D, GL2.TEXTURE_WRAP_T, GL2.CLAMP_TO_EDGE);
+        this.gl.texImage2D(GL2.TEXTURE_2D, 0, GL2.RG32F, this.width, this.height, 0, GL2.RG, GL2.FLOAT, this.grid);
+    };
+
+    hasTextureForContext = (gl: WebGL2RenderingContext) => {
+        return gl === this.gl && this.texture && gl.isTexture(this.texture);
     };
 
     static IsWidthValid(width: number) {
--- src/models/ControlMap.ts
+++ src/models/ControlMap.ts
@@ -8,9 +8,9 @@ export class ControlMap {
     readonly destination: FrameStore;
     readonly width: number;
     readonly height: number;
-    readonly minPoint: Point2D;
-    readonly maxPoint: Point2D;
-    private readonly grid: Float32Array;
+    minPoint: Point2D;
+    maxPoint: Point2D;
+    private grid: Float32Array;
     private texture: WebGLTexture;
     private gl: WebGL2RenderingContext;
 
@@ -19,12 +19,24 @@ export class ControlMap {
         this.destination = dst;
         this.width = width;
         this.height = height;
+        this.setMinMaxPoint(0, 0, this.source.frameInfo.fileInfoExtended.width, this.source.frameInfo.fileInfoExtended.height);
+        this.setGrid(astTransform);
+    }
+
+    setMinMaxPoint = (minX, minY, maxX, maxY) => {
+        const paddingX = Math.ceil(this.source.frameInfo.fileInfoExtended.width / this.width);
+        const paddingY = Math.ceil(this.source.frameInfo.fileInfoExtended.height / this.height);
+        this.minPoint = {x: minX - paddingX, y: minY - paddingY};
+        this.maxPoint = {x: maxX + paddingX, y: maxY + paddingY};
+    };
+
+    setGrid = (astTransform?: AST.FrameSet) => {
 
         let cleanUpTransform: boolean = false;
 
-        if (astTransform < 0) {
-            const copySrc = AST.copy(src.wcsInfo);
-            const copyDest = AST.copy(dst.wcsInfo);
+        if (!astTransform || astTransform < 0) {
+            const copySrc = AST.copy(this.source.wcsInfo);
+            const copyDest = AST.copy(this.destination.wcsInfo);
             AST.invert(copySrc);
             AST.invert(copyDest);
             astTransform = AST.convert(copySrc, copyDest, "");
@@ -33,32 +45,31 @@ export class ControlMap {
             cleanUpTransform = true;
         }
 
-        const paddingX = Math.ceil(src.frameInfo.fileInfoExtended.width / width);
-        const paddingY = Math.ceil(src.frameInfo.fileInfoExtended.height / height);
-        this.minPoint = {x: -paddingX, y: -paddingY};
-        this.maxPoint = {x: paddingX + src.frameInfo.fileInfoExtended.width, y: paddingY + src.frameInfo.fileInfoExtended.height};
-        this.grid = AST.getTransformGrid(astTransform, this.minPoint.x, this.maxPoint.x, width, this.minPoint.y, this.maxPoint.y, height, true);
+        this.grid = AST.getTransformGrid(astTransform, this.minPoint.x, this.maxPoint.x, this.width, this.minPoint.y, this.maxPoint.y, this.height, true);
 
         if (cleanUpTransform) {
             AST.deleteObject(astTransform);
         }
-    }
+    };
 
     getTextureX = (gl: WebGL2RenderingContext) => {
         if (gl !== this.gl || !this.texture) {
             // Context has changed, texture needs to be regenerated
-            this.gl = gl;
-            this.texture = this.gl.createTexture();
-            this.gl.activeTexture(GL2.TEXTURE1);
-            this.gl.bindTexture(GL2.TEXTURE_2D, this.texture);
-            this.gl.texParameteri(GL2.TEXTURE_2D, GL2.TEXTURE_MIN_FILTER, GL2.NEAREST);
-            this.gl.texParameteri(GL2.TEXTURE_2D, GL2.TEXTURE_MAG_FILTER, GL2.NEAREST);
-            this.gl.texParameteri(GL2.TEXTURE_2D, GL2.TEXTURE_WRAP_S, GL2.CLAMP_TO_EDGE);
-            this.gl.texParameteri(GL2.TEXTURE_2D, GL2.TEXTURE_WRAP_T, GL2.CLAMP_TO_EDGE);
-            this.gl.texImage2D(GL2.TEXTURE_2D, 0, GL2.RG32F, this.width, this.height, 0, GL2.RG, GL2.FLOAT, this.grid);
+            this.createTexture(gl);
         }
-
         return this.texture;
+    };
+
+    createTexture = (gl: WebGL2RenderingContext) => {
+        this.gl = gl;
+        this.texture = this.gl.createTexture();
+        this.gl.activeTexture(GL2.TEXTURE1);
+        this.gl.bindTexture(GL2.TEXTURE_2D, this.texture);
+        this.gl.texParameteri(GL2.TEXTURE_2D, GL2.TEXTURE_MIN_FILTER, GL2.NEAREST);
+        this.gl.texParameteri(GL2.TEXTURE_2D, GL2.TEXTURE_MAG_FILTER, GL2.NEAREST);
+        this.gl.texParameteri(GL2.TEXTURE_2D, GL2.TEXTURE_WRAP_S, GL2.CLAMP_TO_EDGE);
+        this.gl.texParameteri(GL2.TEXTURE_2D, GL2.TEXTURE_WRAP_T, GL2.CLAMP_TO_EDGE);
+        this.gl.texImage2D(GL2.TEXTURE_2D, 0, GL2.RG32F, this.width, this.height, 0, GL2.RG, GL2.FLOAT, this.grid);
     };
 
     hasTextureForContext = (gl: WebGL2RenderingContext) => {
