02ca20fe176c154640e26dd8099d72720647c5dd
Angus Comrie <accomrie@gmail.com>
2022-06-14 10:36:12 +0200

    Angus/splatalog service (#1876)
    * migrated splatalogue ping/pong to SplatalogueService
    
    * migrated splatalogue ping/pong to SplatalogueService
    
    * wrangle data into exact same format as previous implementation
    
    * fixed column ordering in response processing
    
    * use colon as separator instead of tab
    
    * removed old spectral line stuff from backend
    
    * updated changelog
    
    * updated protobuf commit and switched from node-sass to sass
    
    * proper use of flow instead of async for asynchronous actions
    
    * update protobuf
    
    Co-authored-by: yuhsuan <r07222045@ntu.edu.tw>

--- src/services/index.ts
+++ src/services/index.ts
@@ -8,3 +8,4 @@ export * from "./ScriptingService";
 export * from "./CatalogWebGLService";
 export * from "./CatalogApiService";
 export * from "./TelemetryService";
+export * from "./SplatalogueService";
