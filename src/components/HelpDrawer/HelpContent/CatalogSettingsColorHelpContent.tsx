6a4b59f7b23a1ea69d6319eb62b15df3be523266
Kuo-Song Wang <kswang@asiaa.sinica.edu.tw>
2022-07-19 14:21:28 +0800

    update catalog color settings

--- src/components/HelpDrawer/HelpContent/CatalogSettingsColorHelpContent.tsx
+++ src/components/HelpDrawer/HelpContent/CatalogSettingsColorHelpContent.tsx
@@ -6,8 +6,8 @@ export const CATALOG_SETTINGS_COLOR_HELP_CONTENT = (
     <div>
         <h3>Marker color</h3>
         <p>
-            This dialog provides options to set the color or colormap to source markers of an image overlay. If the <code>Column</code> dropdown menu is None, all markers are rendered with a single color as set with the <code>Color</code>{" "}
-            dropdown menu. You may assign a numeric data column for color mapping. Different scaling, colormap, or clip bounds can be applied.
+            This dialog provides options to set a color or a colormap to source markers of an image overlay. If the <code>Column</code> dropdown menu is None, all markers are rendered with a single color as set with the <code>Color</code>{" "}
+            dropdown menu. You may assign a numeric data column with the <code>Column</code> dropdown menu for color mapping. Different scaling functions, colormaps, or clip bounds can be applied. When a source or a set of sources are selected, the marker color is changed to the color as specified with the <code>Overlay Highlight</code> dropdown menu.
         </p>
         <p>
             <ImageComponent light={demoImageOverlayColorMapping} dark={demoImageOverlayColorMapping_d} width="100%" />
