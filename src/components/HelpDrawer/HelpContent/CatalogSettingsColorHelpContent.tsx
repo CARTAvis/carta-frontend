3c159e2358ec49aeecd9d2c6907b9d872dea2129
Kuo-Song Wang <kswang@asiaa.sinica.edu.tw>
2022-07-19 14:48:31 +0800

    update catalog size, color, orientation settings

--- src/components/HelpDrawer/HelpContent/CatalogSettingsColorHelpContent.tsx
+++ src/components/HelpDrawer/HelpContent/CatalogSettingsColorHelpContent.tsx
@@ -6,7 +6,7 @@ export const CATALOG_SETTINGS_COLOR_HELP_CONTENT = (
     <div>
         <h3>Marker color</h3>
         <p>
-            This dialog provides options to set a color or a colormap to source markers of an image overlay. If the <code>Column</code> dropdown menu is None, all markers are rendered with a single color as set with the <code>Color</code>{" "}
+            This dialog provides options to set a color or a colormap to source markers of an image overlay. The shape of the marker can be set with the <code>Shape</code> dropdown menu. If the <code>Column</code> dropdown menu is None, all markers are rendered with a single color as set with the <code>Color</code>{" "}
             dropdown menu. You may assign a numeric data column with the <code>Column</code> dropdown menu for color mapping. Different scaling functions, colormaps, or clip bounds can be applied. When a source or a set of sources are selected, the marker color is changed to the color as specified with the <code>Overlay Highlight</code> dropdown menu.
         </p>
         <p>
--- src/components/HelpDrawer/HelpContent/CatalogSettingsOrientationHelpContent.tsx
+++ src/components/HelpDrawer/HelpContent/CatalogSettingsOrientationHelpContent.tsx
@@ -6,7 +6,7 @@ export const CATALOG_SETTINGS_ORIENTATION_HELP_CONTENT = (
     <div>
         <h3>Marker orientation</h3>
         <p>
-            This dialog provides options to set the orientation of source markers of an image overlay. If the <code>Column</code> dropdown menu is None, all markers are rendered without applying an additional rotation. You may
+            This dialog provides options to set the orientation of source markers of an image overlay. The shape of the marker can be set with the <code>Shape</code> dropdown menu. If the <code>Column</code> dropdown menu is None, all markers are rendered without applying an additional rotation. You may
             assign a numeric data column for orientation mapping with the <code>Column</code> dropdown menu. Different scaling functions, orientation ranges, or clip bounds can be applied.
         </p>
         <p>
