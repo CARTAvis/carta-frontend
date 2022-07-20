2cc7ffe84feba53cafe844f9ac2c8ed66645c682
Kuo-Song Wang <kswang@asiaa.sinica.edu.tw>
2022-07-20 10:25:32 +0800

    update file header dialog

--- src/components/HelpDrawer/HelpContent/FileInfoHelpContent.tsx
+++ src/components/HelpDrawer/HelpContent/FileInfoHelpContent.tsx
@@ -1,13 +1,15 @@
 import {ImageComponent} from "../ImageComponent";
-import headFileinfoButton from "static/help/head_fileinfo_button.png";
-import headFileinfoButton_d from "static/help/head_fileinfo_button_d.png";
+import dialogButtonFileHeader from "static/help/dialogButton_fileHeader.png";
+import dialogButtonFileHeader_d from "static/help/dialogButton_fileHeader_d.png";
 
 export const FILE_INFO_HELP_CONTENT = (
     <div>
         <p>
-            <ImageComponent light={headFileinfoButton} dark={headFileinfoButton_d} width="90%" />
+            <ImageComponent light={dialogButtonFileHeader} dark={dialogButtonFileHeader_d} width="39%" />
         </p>
-        <p>File header dialog provides full image header and a summary of the properties of the image in the current image view. To switch to other images, use the image slider in the animator widget, or use the image list widget.</p>
-        <p>Search function is available in the header tab. The search button appears when you hover over the header context. Matches are case-insensitive, and partial words are matched.</p>
+        <p>File header dialog provides full image header (<code>Header</code> tab) and a summary of the properties of the active image (<code>File Information</code> tab). To view the file headers of other images, use the image slider in the animator widget, or use the image list widget.</p>
+        <p>You can search the header or export the header as a text file with the tools at the bottom-right corner of the <code>Header</code> tab.</p>
+        <h4>NOTE</h4>
+        The image coordinate in FITS definition is 1-based, meaning that the image coordinate at the center of the bottom-left corner pixel is (1, 1). In CARTA, image coordinate is 0-based so that the image coordinate at the center of the bottom-left corner pixel is (0, 0). An index shift is applied internally. This applies to spectral axis too.
     </div>
 );
