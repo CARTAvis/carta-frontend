5373cb0b08e371950af6923f1953e8a36091d514
Kuo-Song Wang <kswang@asiaa.sinica.edu.tw>
2022-07-24 08:53:03 +0800

    update save layout

--- src/components/HelpDrawer/HelpContent/SaveLayoutHelpContent.tsx
+++ src/components/HelpDrawer/HelpContent/SaveLayoutHelpContent.tsx
@@ -3,10 +3,14 @@ export const SAVE_LAYOUT_HELP_CONTENT = (
         <p>With this dialog, the current layout can be saved and re-used in the future. The widgets in a layout can be either docked or undocked.</p>
         <h3 id="layout-management">Layout management</h3>
         <p>
-            Load a preset layout or a custom layout: <strong>View</strong> -&gt; <strong>Layouts</strong> -&gt; <strong>Existing layouts</strong>
+            Save a custom layout: <strong>View</strong> -&gt; <strong>Layouts</strong> -&gt; <strong>Existing layouts</strong>.
+        </p>
+        
+        <p>
+            Load a preset layout or a custom layout: <strong>View</strong> -&gt; <strong>Layouts</strong> -&gt; <strong>Save Layout</strong>.
         </p>
         <p>
-            Set a layout as the default layout: <strong>File</strong> -&gt; <strong>Preferences</strong> -&gt; <strong>Global</strong> tab -&gt; <strong>Initial layout</strong>.
+            Set a layout as the default layout: use the <code>Initial layout</code> dropdown menu in the <code>Global</code> tab of the preferences dialog (<strong>File</strong> -&gt; <strong>Preferences</strong>).
         </p>
         <p>
             Delete a custom layout: <strong>View</strong> -&gt; <strong>Layouts</strong> -&gt; <strong>Delete layout</strong>.
