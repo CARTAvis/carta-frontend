bef815b7b593403b9b91bddd51f77d7c47a771af
crocka <raulomar@ualberta.ca>
2022-08-24 15:08:28 -0400

    Improved CSS style of the distance measuring dialog.

--- src/components/Dialogs/DistanceMeasuringDialog/DistanceMeasuringDialog.tsx
+++ src/components/Dialogs/DistanceMeasuringDialog/DistanceMeasuringDialog.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import {observable, action, makeObservable} from "mobx";
 import {observer} from "mobx-react";
 import {ColorResult} from "react-color";
-import {FormGroup, IDialogProps, NonIdealState} from "@blueprintjs/core";
+import {FormGroup, IDialogProps, NonIdealState, H5} from "@blueprintjs/core";
 import {AppStore, DialogStore} from "stores";
 import {RegionCoordinate} from "stores/Frame";
 import {SafeNumericInput, ColorPickerComponent, CoordinateComponent} from "components/Shared";
@@ -10,6 +10,7 @@ import {DraggableDialogComponent} from "components/Dialogs";
 import {WCSPoint2D, Point2D} from "models";
 import {getPixelValueFromWCS, getFormattedWCSPoint, isWCSStringFormatValid, SWATCH_COLORS} from "utilities";
 import {ImageViewLayer} from "components";
+import "./DistanceMeasuringDialog.scss";
 
 @observer
 export class DistanceMeasuringDialog extends React.Component {
@@ -57,7 +58,6 @@ export class DistanceMeasuringDialog extends React.Component {
         // eslint-disable-next-line @typescript-eslint/no-unused-vars
         const system = appStore.overlayStore.global.explicitSystem;
         const frame = appStore.activeFrame;
-        // const isSpatialReferenceOn = frame?.spatialReference;
         const distanceMeasuringStore = frame?.distanceMeasuring;
         const wcsInfo = frame?.validWcs ? frame.wcsInfoForTransformation : 0;
         const WCSStart = getFormattedWCSPoint(wcsInfo, distanceMeasuringStore?.start);
@@ -128,94 +128,147 @@ export class DistanceMeasuringDialog extends React.Component {
 
         const startInput = this.WCSMode ? (
             <>
-                <FormGroup helperText={this.error ? "Invalid Input" : ""} label={"X-Coordinate"} inline={true} style={style}>
-                    <SafeNumericInput selectAllOnFocus intent={this.error ? "danger" : "none"} allowNumericCharactersOnly={false} buttonPosition="none" value={WCSStart.x} onBlur={event => handleValueChange(event, true, false, false)} />
-                </FormGroup>
-                <FormGroup helperText={this.error ? "Invalid Input" : ""} label={"Y-Coordinate"} inline={true} style={style}>
-                    <SafeNumericInput selectAllOnFocus intent={this.error ? "danger" : "none"} allowNumericCharactersOnly={false} buttonPosition="none" value={WCSStart.y} onBlur={event => handleValueChange(event, false, false, false)} />
-                </FormGroup>
+                <td>
+                    <FormGroup helperText={this.error ? "Invalid Input" : ""} inline={true} style={style}>
+                        <SafeNumericInput
+                            selectAllOnFocus
+                            intent={this.error ? "danger" : "none"}
+                            allowNumericCharactersOnly={false}
+                            buttonPosition="none"
+                            value={WCSStart?.x}
+                            onBlur={event => handleValueChange(event, true, false, false)}
+                        />
+                    </FormGroup>
+                </td>
+                <td>
+                    <FormGroup helperText={this.error ? "Invalid Input" : ""} inline={true} style={style}>
+                        <SafeNumericInput
+                            selectAllOnFocus
+                            intent={this.error ? "danger" : "none"}
+                            allowNumericCharactersOnly={false}
+                            buttonPosition="none"
+                            value={WCSStart?.y}
+                            onBlur={event => handleValueChange(event, false, false, false)}
+                        />
+                    </FormGroup>
+                </td>
             </>
         ) : (
             <>
-                <FormGroup label={"x-Pixel"} inline={true} style={style}>
-                    <SafeNumericInput selectAllOnFocus buttonPosition="none" value={distanceMeasuringStore?.start.x} onBlur={event => handleValueChange(event, true, false, true)} />
-                </FormGroup>
-                <FormGroup label={"y-Pixel"} inline={true} style={style}>
-                    <SafeNumericInput selectAllOnFocus buttonPosition="none" value={distanceMeasuringStore?.start.y} onBlur={event => handleValueChange(event, false, false, true)} />
-                </FormGroup>
+                <td>
+                    <FormGroup inline={true} style={style}>
+                        <SafeNumericInput selectAllOnFocus buttonPosition="none" value={distanceMeasuringStore?.start.x} onBlur={event => handleValueChange(event, true, false, true)} />
+                    </FormGroup>
+                </td>
+                <td>
+                    <FormGroup inline={true} style={style}>
+                        <SafeNumericInput selectAllOnFocus buttonPosition="none" value={distanceMeasuringStore?.start.y} onBlur={event => handleValueChange(event, false, false, true)} />
+                    </FormGroup>
+                </td>
             </>
         );
 
         const finishInput = this.WCSMode ? (
             <>
-                <FormGroup helperText={this.error ? "Invalid Input" : ""} label={"X-Coordinate"} inline={true} style={style}>
-                    <SafeNumericInput selectAllOnFocus intent={this.error ? "danger" : "none"} allowNumericCharactersOnly={false} buttonPosition="none" value={WCSFinish.x} onBlur={event => handleValueChange(event, true, true, false)} />
-                </FormGroup>
-                <FormGroup helperText={this.error ? "Invalid Input" : ""} label={"Y-Coordinate"} inline={true} style={style}>
-                    <SafeNumericInput selectAllOnFocus intent={this.error ? "danger" : "none"} allowNumericCharactersOnly={false} buttonPosition="none" value={WCSFinish.y} onBlur={event => handleValueChange(event, false, true, false)} />
-                </FormGroup>
+                <td>
+                    <FormGroup helperText={this.error ? "Invalid Input" : ""} inline={true} style={style}>
+                        <SafeNumericInput
+                            selectAllOnFocus
+                            intent={this.error ? "danger" : "none"}
+                            allowNumericCharactersOnly={false}
+                            buttonPosition="none"
+                            value={WCSFinish?.x}
+                            onBlur={event => handleValueChange(event, true, true, false)}
+                        />
+                    </FormGroup>
+                </td>
+                <td>
+                    <FormGroup helperText={this.error ? "Invalid Input" : ""} inline={true} style={style}>
+                        <SafeNumericInput
+                            selectAllOnFocus
+                            intent={this.error ? "danger" : "none"}
+                            allowNumericCharactersOnly={false}
+                            buttonPosition="none"
+                            value={WCSFinish?.y}
+                            onBlur={event => handleValueChange(event, false, true, false)}
+                        />
+                    </FormGroup>
+                </td>
             </>
         ) : (
             <>
-                <FormGroup label={"x-Pixel"} inline={true} style={style}>
-                    <SafeNumericInput selectAllOnFocus buttonPosition="none" value={distanceMeasuringStore?.finish.x} onBlur={event => handleValueChange(event, true, true, true)} />
-                </FormGroup>
-                <FormGroup label={"y-Pixel"} inline={true} style={style}>
-                    <SafeNumericInput selectAllOnFocus buttonPosition="none" value={distanceMeasuringStore?.finish.y} onBlur={event => handleValueChange(event, false, true, true)} />
-                </FormGroup>
+                <td>
+                    <FormGroup inline={true} style={style}>
+                        <SafeNumericInput selectAllOnFocus buttonPosition="none" value={distanceMeasuringStore?.finish.x} onBlur={event => handleValueChange(event, true, true, true)} />
+                    </FormGroup>
+                </td>
+                <td>
+                    <FormGroup inline={true} style={style}>
+                        <SafeNumericInput selectAllOnFocus buttonPosition="none" value={distanceMeasuringStore?.finish.y} onBlur={event => handleValueChange(event, false, true, true)} />
+                    </FormGroup>
+                </td>
             </>
         );
 
         return (
-            <DraggableDialogComponent dialogProps={dialogProps} defaultWidth={775} defaultHeight={475} minHeight={300} minWidth={700} enableResizing={true}>
+            <DraggableDialogComponent dialogProps={dialogProps} defaultWidth={775} defaultHeight={475} minHeight={350} minWidth={775} enableResizing={true}>
                 <div className="distance-measuring-settings">
                     {appStore.activeLayer === ImageViewLayer.DistanceMeasuring ? (
-                        <table>
-                            <tbody>
-                                <tr>
-                                    <td>
-                                        <FormGroup label="Color" inline={true}>
-                                            <ColorPickerComponent
-                                                color={distanceMeasuringStore?.color}
-                                                presetColors={SWATCH_COLORS}
-                                                setColor={(color: ColorResult) => distanceMeasuringStore?.setColor(color.hex)}
-                                                disableAlpha={true}
-                                                darkTheme={appStore.darkTheme}
-                                            />
-                                        </FormGroup>
-                                    </td>
-                                    <td>
-                                        <FormGroup inline={true} label="Line Width" labelInfo="(px)">
-                                            <SafeNumericInput placeholder="Line Width" min={0.5} max={20} value={distanceMeasuringStore?.lineWidth} stepSize={0.5} onValueChange={value => distanceMeasuringStore?.setLineWidth(value)} />
-                                        </FormGroup>
-                                    </td>
-                                    <td>
-                                        <FormGroup inline={true} label="Font Size" labelInfo="(px)">
-                                            <SafeNumericInput placeholder="Font Size" min={0.5} max={50} value={distanceMeasuringStore?.fontSize} stepSize={1} onValueChange={value => distanceMeasuringStore?.setFontSize(value)} />
-                                        </FormGroup>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td colSpan={2}>
-                                        <CoordinateComponent onChange={(ev: React.FormEvent<HTMLInputElement>) => handleChangeWCSMode(ev)} selectedValue={this.WCSMode ? RegionCoordinate.World : RegionCoordinate.Image} />
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td>Start</td>
-                                    <td>{startInput}</td>
-                                    <td>
-                                        <span className="info-string">{this.WCSMode ? `Image: ${Point2D.ToString(distanceMeasuringStore?.transformedStart, "px", 3)}` : `WCS: ${WCSPoint2D.ToString(WCSStart)}`}</span>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td>Finish</td>
-                                    <td>{finishInput}</td>
-                                    <td>
-                                        <span className="info-string">{this.WCSMode ? `Image: ${Point2D.ToString(distanceMeasuringStore?.transformedFinish, "px", 3)}` : `WCS: ${WCSPoint2D.ToString(WCSFinish)}`}</span>
-                                    </td>
-                                </tr>
-                            </tbody>
-                        </table>
+                        <>
+                            <table className="distance-measuring-settings-table">
+                                <tbody>
+                                    <tr className="distance-measuring-settings-table-title">
+                                        <H5>Line Style</H5>
+                                    </tr>
+                                    <tr className="distance-measuring-settings-table-line-style">
+                                        <td>
+                                            <FormGroup label="Color" inline={true}>
+                                                <ColorPickerComponent
+                                                    color={distanceMeasuringStore?.color}
+                                                    presetColors={SWATCH_COLORS}
+                                                    setColor={(color: ColorResult) => distanceMeasuringStore?.setColor(color.hex)}
+                                                    disableAlpha={true}
+                                                    darkTheme={appStore.darkTheme}
+                                                />
+                                            </FormGroup>
+                                        </td>
+                                        <td colSpan={2} className="distance-measuring-settings-table-line-style-numeric-input">
+                                            <FormGroup inline={true} label="Line Width" labelInfo="(px)">
+                                                <SafeNumericInput placeholder="Line Width" min={0.5} max={20} value={distanceMeasuringStore?.lineWidth} stepSize={0.5} onValueChange={value => distanceMeasuringStore?.setLineWidth(value)} />
+                                            </FormGroup>
+                                        </td>
+                                        <td width="300px" className="distance-measuring-settings-table-line-style-numeric-input">
+                                            <FormGroup inline={true} label="Font Size" labelInfo="(px)">
+                                                <SafeNumericInput placeholder="Font Size" min={0.5} max={50} value={distanceMeasuringStore?.fontSize} stepSize={1} onValueChange={value => distanceMeasuringStore?.setFontSize(value)} />
+                                            </FormGroup>
+                                        </td>
+                                    </tr>
+                                    <tr className="distance-measuring-settings-table-title">
+                                        <H5>Properties</H5>
+                                    </tr>
+                                    <tr className="distance-measuring-settings-table-coordinate">
+                                        <td>Coordinate</td>
+                                        <td colSpan={2}>
+                                            <CoordinateComponent onChange={(ev: React.FormEvent<HTMLInputElement>) => handleChangeWCSMode(ev)} selectedValue={this.WCSMode ? RegionCoordinate.World : RegionCoordinate.Image} />
+                                        </td>
+                                    </tr>
+                                    <tr className="distance-measuring-settings-table-input">
+                                        <td>Start</td>
+                                        {startInput}
+                                        <td colSpan={3}>
+                                            <span className="info-string">{this.WCSMode ? `Image: ${Point2D.ToString(distanceMeasuringStore?.start, "px", 3)}` : `WCS: ${WCSPoint2D.ToString(WCSStart)}`}</span>
+                                        </td>
+                                    </tr>
+                                    <tr className="distance-measuring-settings-table-input">
+                                        <td>Finish</td>
+                                        {finishInput}
+                                        <td colSpan={3}>
+                                            <span className="info-string">{this.WCSMode ? `Image: ${Point2D.ToString(distanceMeasuringStore?.finish, "px", 3)}` : `WCS: ${WCSPoint2D.ToString(WCSFinish)}`}</span>
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </>
                     ) : (
                         MissingFrame
                     )}
