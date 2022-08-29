992956de031a6f59fce673f092b7784b6dc8c67f
thchang <tienhaochang703@gmail.com>
2022-08-29 16:04:12 +0800

    fixed minor bug

--- src/components/SpectralProfiler/SpectralProfilerComponent.tsx
+++ src/components/SpectralProfiler/SpectralProfilerComponent.tsx
@@ -180,7 +180,7 @@ export class SpectralProfilerComponent extends React.Component<WidgetProps> {
             const cursorXValue = isCursorInsideLinePlots ? this.widgetStore.cursorX : this.currentChannelValue;
             const cursorXUnit = frame.spectralUnitStr;
 
-            if (this.plotData.numProfiles === 1) {
+            if (this.plotData.numProfiles === 1 && (this.widgetStore.smoothingStore.type === SmoothingType.NONE || !this.widgetStore.smoothingStore.isOverlayOn)) {
                 // Single profile, Mean/RMS is available
                 const data = this.plotData.data[0];
                 const cursorInfoString = this.genCursoInfoString(data, cursorXValue, cursorXUnit, label);
