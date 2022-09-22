05b2baba2c22f8d057b06d0e1a5feb754dca5549
Cheng-Chin Chiang <chengchin.chiang@gmail.com>
2022-09-23 00:31:54 +0800

    Get the spectral axis number when setting its unit conversion

--- src/components/ImageView/Overlay/OverlayComponent.tsx
+++ src/components/ImageView/Overlay/OverlayComponent.tsx
@@ -161,14 +161,15 @@ export class OverlayComponent extends React.Component<OverlayComponentProps> {
         /* eslint-enable no-unused-vars, @typescript-eslint/no-unused-vars */
 
         // Trigger switching AST overlay axis for PV image
-        if ((frame.isPVImage && frame.spectralAxis?.valid) || (frame.isSwappedImage && frame.spectralAxis?.dimension === 2)) {
+        if ((frame.isPVImage && frame.spectralAxis?.valid) || (frame.isSwappedImage && frame.spectralAxis?.dimension < 3)) {
+            const spectralAxis = frame.spectralAxis?.dimension;
             AST.set(
                 frame.wcsInfo,
-                `${frame.spectralType ? `System(2)=${frame.spectralType},` : ""}` +
-                    `${frame.spectralUnit ? `Unit(2)=${frame.spectralUnit},` : ""}` +
+                `${frame.spectralType ? `System(${spectralAxis})=${frame.spectralType},` : ""}` +
+                    `${frame.spectralUnit ? `Unit(${spectralAxis})=${frame.spectralUnit},` : ""}` +
                     `${frame.spectralSystem ? `StdOfRest=${frame.spectralSystem},` : ""}` +
                     `${frame.restFreqStore.restFreqInHz ? `RestFreq=${frame.restFreqStore.restFreqInHz} Hz,` : ""}` +
-                    `${frame.spectralType && frame.spectralSystem ? `Label(2)=[${frame.spectralSystem}] ${SPECTRAL_TYPE_STRING.get(frame.spectralType)},` : ""}`
+                    `${frame.spectralType && frame.spectralSystem ? `Label(${spectralAxis})=[${frame.spectralSystem}] ${SPECTRAL_TYPE_STRING.get(frame.spectralType)},` : ""}`
             );
         }
 
