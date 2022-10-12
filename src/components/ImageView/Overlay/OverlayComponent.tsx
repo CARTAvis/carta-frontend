394ed0981f57c0a99874965934423f236fb8de08
Carli <94193689+crocka@users.noreply.github.com>
2022-10-11 21:56:27 -0400

    carli/1201_angular_distance_measuring_tool (#1971)
    * In progress: added a dialog to input the starting and finishing pixel
    
    * In progress, adding wcs input
    
    * Implemented pixel and WCS coords input for distance measurement
    
    * Implemented dialog for distance measurement with pixel and wcs coordinates
    
    * Minor changes
    
    * Implemented pixel and wcs input with improved UI
    
    * "Update deleted files"
    
    * Ran reformat
    
    * Minor update
    
    * Refactored the CoordinateComponent to the components/shared folder
    
    * Implemented line width, font size, color settings and added button on the top menu toolbar.
    
    * Ran reformat
    
    * Implemented multiple frame distance measuring functionality
    
    * Fixed bugs when spatial matching is enabled
    
    * Implemented better pixel and wcs input when spatial matching is enabled
    
    * Fixed problem where style changes when spatial matching is enabled
    
    * Improved CSS style of the distance measuring dialog.
    
    * In progress: overlayComponent stylestring correction
    
    * Style final touch
    
    * Implemented WCS input error handling
    
    * Delete comments
    
    * Run reformat
    
    * Minor refractoring
    
    * Minor changes
    
    * In progress: added a dialog to input the starting and finishing pixel
    
    * In progress, adding wcs input
    
    * Implemented pixel and WCS coords input for distance measurement
    
    * Implemented dialog for distance measurement with pixel and wcs coordinates
    
    * Minor changes
    
    * Implemented pixel and wcs input with improved UI
    
    * "Update deleted files"
    
    * Ran reformat
    
    * Minor update
    
    * Refactored the CoordinateComponent to the components/shared folder
    
    * Implemented line width, font size, color settings and added button on the top menu toolbar.
    
    * Ran reformat
    
    * Implemented multiple frame distance measuring functionality
    
    * Fixed bugs when spatial matching is enabled
    
    * Implemented better pixel and wcs input when spatial matching is enabled
    
    * Fixed problem where style changes when spatial matching is enabled
    
    * Improved CSS style of the distance measuring dialog.
    
    * In progress: overlayComponent stylestring correction
    
    * Style final touch
    
    * Implemented WCS input error handling
    
    * Delete comments
    
    * Run reformat
    
    * Minor refractoring
    
    * Minor changes
    
    * Implemented changes according to comments
    
    * Minor change
    
    * Implemented disabled WCS selection when no valid WCS available
    
    * Implemented improvements based on comments
    
    * Minor changes
    
    * Changed dialog size
    
    * Implemented changes according to comments.
    
    * Change null to NaN
    
    * Minor changes
    
    * Changes according to comments
    
    * Minor changes according to comments
    
    * Minor change according to comments
    
    * Updated Changelog
    
    Co-authored-by: crocka <raulomar@ualberta.ca>

--- src/components/ImageView/Overlay/OverlayComponent.tsx
+++ src/components/ImageView/Overlay/OverlayComponent.tsx
@@ -94,7 +94,8 @@ export class OverlayComponent extends React.Component<OverlayComponentProps> {
                 );
             };
 
-            let currentStyleString = settings.styleString;
+            let currentStyleString = settings.styleString(frame);
+
             // Override the AST tolerance during motion
             if (frame.moving) {
                 const tolVal = Math.max((settings.global.tolerance * 2) / 100.0, 0.1);
@@ -129,8 +130,6 @@ export class OverlayComponent extends React.Component<OverlayComponentProps> {
     };
 
     render() {
-        const styleString = this.props.overlaySettings.styleString;
-
         const frame = this.props.frame;
         const refFrame = frame.spatialReference ?? frame;
         // changing the frame view, padding or width/height triggers a re-render
@@ -153,9 +152,15 @@ export class OverlayComponent extends React.Component<OverlayComponentProps> {
         const numbersColor = this.props.overlaySettings.numbers.color;
         const labelsColor = this.props.overlaySettings.labels.color;
         const darktheme = AppStore.Instance.darkTheme;
+        const distanceMeasuring = frame.distanceMeasuring;
         const distanceMeasuringShowCurve = frame.distanceMeasuring.showCurve;
-        const distanceMeasuringStart = frame.distanceMeasuring.transformedStart;
-        const distanceMeasuringFinish = frame.distanceMeasuring.transformedFinish;
+        const distanceMeasuringStart = frame.distanceMeasuring.start;
+        const distanceMeasuringFinish = frame.distanceMeasuring.finish;
+        const distanceMeasuringTransformedStart = frame.distanceMeasuring.transformedStart;
+        const distanceMeasuringTransformedFinish = frame.distanceMeasuring.transformedFinish;
+        const distanceMeasuringColor = frame.distanceMeasuring.color;
+        const distanceMeasuringFontSize = frame.distanceMeasuring.fontSize;
+        const distanceMeasuringLineWidth = frame.distanceMeasuring.lineWidth;
         const title = frame.titleCustomText;
         const ratio = AppStore.Instance.imageRatio;
         /* eslint-enable no-unused-vars, @typescript-eslint/no-unused-vars */
@@ -173,6 +178,6 @@ export class OverlayComponent extends React.Component<OverlayComponentProps> {
         }
 
         const className = classNames("overlay-canvas", {docked: this.props.docked});
-        return <canvas className={className} style={{width: w, height: h}} id="overlay-canvas" key={styleString} ref={this.getRef} />;
+        return <canvas className={className} style={{width: w, height: h}} id="overlay-canvas" ref={this.getRef} />;
     }
 }
