d9cf9729b78f3c54b22d6653177dea797bd38d9d
yuhsuan <r07222045@ntu.edu.tw>
2022-10-20 16:25:34 +0800

    adjust enum import path

--- src/components/Shared/ScalingSelectComponent/ScalingSelectComponent.tsx
+++ src/components/Shared/ScalingSelectComponent/ScalingSelectComponent.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import {MenuItem, IPopoverProps, Button} from "@blueprintjs/core";
 import {Select} from "@blueprintjs/select";
-import {FrameScaling, RenderConfigStore} from "stores/Frame";
+import {FrameScaling, RenderConfigStore} from "stores/Frame/RenderConfigStore";
 import "./ScalingSelectComponent.scss";
 
 // Equation PNG images
