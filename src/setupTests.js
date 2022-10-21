d74bdff7046f9e1bfd1fff96444c4199f1173385
yuhsuan <r07222045@ntu.edu.tw>
2022-10-21 15:44:43 +0800

    mock window.matchMedia

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,2 +1,16 @@
-window.URL.createObjectURL = () => { };
+window.URL.createObjectURL = () => {};
 global.WebGL2RenderingContext = null;
+
+Object.defineProperty(window, 'matchMedia', {
+    writable: true,
+    value: query => ({
+        matches: false,
+        media: query,
+        onchange: null,
+        addListener: () => {}, // deprecated
+        removeListener: () => {}, // deprecated
+        addEventListener: () => {},
+        removeEventListener: () => {},
+        dispatchEvent: () => {},
+    }),
+});
