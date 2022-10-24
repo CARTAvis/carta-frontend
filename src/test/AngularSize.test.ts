d871b3d0a2c4f0966fa79b632faf1e105bd209e0
yuhsuan <r07222045@ntu.edu.tw>
2022-10-24 16:58:20 +0800

    test other cases for convertFromArcsec

--- src/test/AngularSize.test.ts
+++ src/test/AngularSize.test.ts
@@ -19,11 +19,47 @@ describe("test convertFromArcsec", () => {
         convertValueFromArcsecMock = jest.spyOn(AngularSize, "convertValueFromArcsec");
     });
 
-    test("creates AngularSize object with required unit", () => {
-        convertValueFromArcsecMock.mockImplementationOnce(() => 1);
-        const size = AngularSize.convertFromArcsec(1);
+    test("returns values with required unit", () => {
+        convertValueFromArcsecMock
+            .mockImplementationOnce(() => 1)
+            .mockImplementationOnce(() => 2)
+            .mockImplementationOnce(() => 2);
+        
+        let size = AngularSize.convertFromArcsec(1);
         expect(convertValueFromArcsecMock).toBeCalledWith(1, AngularSizeUnit.ARCSEC);
         expect(size?.value).toEqual(1);
+        expect(size?.unit).toEqual(AngularSizeUnit.ARCSEC);
+
+        size = AngularSize.convertFromArcsec(120);
+        expect(convertValueFromArcsecMock).toBeCalledWith(120, AngularSizeUnit.ARCMIN);
+        expect(size?.value).toEqual(2);
+        expect(size?.unit).toEqual(AngularSizeUnit.ARCMIN);
+
+        size = AngularSize.convertFromArcsec(7200);
+        expect(convertValueFromArcsecMock).toBeCalledWith(7200, AngularSizeUnit.DEG);
+        expect(size?.value).toEqual(2);
+        expect(size?.unit).toEqual(AngularSizeUnit.DEG);
+    });
+
+    test("converts to milliarsec when needed", () => {
+        convertValueFromArcsecMock
+            .mockImplementationOnce(() => 1)
+            .mockImplementationOnce(() => 0.001);
+        
+        let size = AngularSize.convertFromArcsec(0.001, true);
+        expect(convertValueFromArcsecMock).toBeCalledWith(0.001, AngularSizeUnit.MILLIARCSEC);
+        expect(size?.value).toEqual(1);
+        expect(size?.unit).toEqual(AngularSizeUnit.MILLIARCSEC);
+
+        size = AngularSize.convertFromArcsec(0.001, false);
+        expect(convertValueFromArcsecMock).toBeCalledWith(0.001, AngularSizeUnit.ARCSEC);
+        expect(size?.value).toEqual(0.001);
+        expect(size?.unit).toEqual(AngularSizeUnit.ARCSEC);
+    });
+
+    test("returns undefined if input arcsec is not finite", () => {
+        const size = AngularSize.convertFromArcsec(NaN);
+        expect(size?.value).toBeUndefined();
         expect(size?.unit).toEqual(AngularSizeUnit.ARCSEC);
     });
 });
