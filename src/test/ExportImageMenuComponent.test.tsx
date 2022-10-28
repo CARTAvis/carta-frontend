e08c8400524409644fff1e513705a90a8c58d58f
yuhsuan <r07222045@ntu.edu.tw>
2022-10-28 14:04:59 +0800

    mock dependencies

--- src/test/ExportImageMenuComponent.test.tsx
+++ src/test/ExportImageMenuComponent.test.tsx
@@ -1,8 +1,22 @@
 import React from "react";
 import {render, screen} from "@testing-library/react";
 import {ExportImageMenuComponent} from "../components/Shared";
+import {AppStore} from "../stores/AppStore";
 
 describe("test ExportImageMenuComponent", () => {
+    
+    let modifierStringMock: jest.SpyInstance;
+    let exportImageMock: jest.SpyInstance;
+    
+    beforeEach(() => {
+        modifierStringMock = jest.spyOn(AppStore.prototype, "modifierString", "get").mockImplementation(() => "ctrl + ");
+        exportImageMock = jest.spyOn(AppStore.Instance, "exportImage");
+    });
+    
+    afterEach(() => {
+        jest.restoreAllMocks();
+    })
+    
     test("renders", () => {
         render(<ExportImageMenuComponent />);
         screen.debug();
