b29d9bce5a9030134bf470228f3096b4e985bdb8
I-Chenn <icchen@asiaa.sinica.edu.tw>
2022-11-01 16:35:59 +0800

    Update image view mode when catalog selection button is disabled

--- src/components/CatalogOverlay/CatalogOverlayComponent.tsx
+++ src/components/CatalogOverlay/CatalogOverlayComponent.tsx
@@ -16,6 +16,8 @@ import {CatalogWidgetStore, CatalogPlotWidgetStoreProps, CatalogPlotType, Catalo
 import {toFixed, ProcessedColumnData} from "utilities";
 import {AbstractCatalogProfileStore, CatalogOverlay, CatalogSystemType} from "models";
 import "./CatalogOverlayComponent.scss";
+import {ImageViewLayer} from "components/ImageView/ImageViewComponent";
+import {RegionMode} from "stores/Frame";
 
 enum HeaderTableColumnName {
     Name = "Name",
@@ -453,6 +455,12 @@ export class CatalogOverlayComponent extends React.Component<WidgetProps> {
         const profileStore = this.profileStore;
         const catalogWidgetStore = this.widgetStore;
         const appStore = AppStore.Instance;
+        const catalogStore = CatalogStore.Instance;
+        const frame = appStore.getFrame(catalogStore.getFrameIdByCatalogId(this.catalogFileId));
+
+        appStore.updateActiveLayer(ImageViewLayer.RegionMoving);
+        frame.regionSet.setMode(RegionMode.MOVING);
+
         if (profileStore && catalogWidgetStore) {
             profileStore.resetCatalogFilterRequest();
             this.resetSelectedPointIndices();
