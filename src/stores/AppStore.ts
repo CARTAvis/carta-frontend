15122f1f42cbc8dcc05d01fa42a4eeee3fcf9b30
Kuo-Song Wang <kswang@asiaa.sinica.edu.tw>
2022-11-23 16:10:48 +0800

    increase batch load size to 1000

--- src/stores/AppStore.ts
+++ src/stores/AppStore.ts
@@ -60,7 +60,7 @@ interface ChannelUpdate {
     stokes: number;
 }
 
-const IMPORT_REGION_BATCH_SIZE = 100;
+const IMPORT_REGION_BATCH_SIZE = 1000;
 const EXPORT_IMAGE_DELAY = 500;
 
 export class AppStore {
