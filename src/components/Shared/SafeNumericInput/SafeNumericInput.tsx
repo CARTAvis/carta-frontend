8113f1401d21595942f8ea342b6572e8ba93db4f
I-Chenn <icchen@asiaa.sinica.edu.tw>
2023-02-15 23:21:23 +0800

    Define onKeyDown and on onBlur in SafeNumericInput

--- src/components/Shared/SafeNumericInput/SafeNumericInput.tsx
+++ src/components/Shared/SafeNumericInput/SafeNumericInput.tsx
@@ -1,14 +1,49 @@
 import * as React from "react";
 import {INumericInputProps, NumericInput} from "@blueprintjs/core";
 
+const KEYCODE_ENTER = 13;
+
 export interface SafeNumericInputProps extends INumericInputProps {
     intOnly?: boolean;
+    onValueChanged?: (val: number) => void;
     onBlur?(ev: React.FocusEvent<HTMLInputElement>): void;
     onKeyDown?(ev: React.KeyboardEvent<HTMLInputElement>): void;
 }
 
 export class SafeNumericInput extends React.Component<SafeNumericInputProps> {
     private static minorStepSize = 0.001;
+
+    handleChange = ev => {
+        if (ev.type === "keydown" && ev.keyCode !== KEYCODE_ENTER) {
+            return;
+        }
+        const val = parseFloat(this.validation(ev.currentTarget.value));
+        this.props.onValueChanged(val);
+    };
+
+    handleOnBlur = (ev: React.FocusEvent<HTMLInputElement>) => {
+        this.handleChange(ev);
+    };
+
+    private validation = (value: string): string => {
+        const valueAsNumber = parseFloat(value);
+        let valueAsString = value;
+        if (this.props.intOnly) {
+            const roundValue = Math.round(valueAsNumber);
+            if (isFinite(roundValue)) {
+                valueAsString = roundValue.toString();
+            }
+        }
+
+        if (this.props.min && valueAsNumber < this.props.min) {
+            valueAsString = this.props.min.toString();
+        }
+
+        if (this.props.max && valueAsNumber > this.props.max) {
+            valueAsString = this.props.max.toString();
+        }
+        return valueAsString;
+    };
 
     safeHandleValueChanged = (valueAsNumber: number, valueAsString: string, inputElement: HTMLInputElement) => {
         if (this.props.intOnly) {
@@ -27,6 +62,16 @@ export class SafeNumericInput extends React.Component<SafeNumericInputProps> {
     render() {
         const {intOnly, ...otherProps} = this.props;
 
-        return <NumericInput {...otherProps} asyncControl={true} minorStepSize={this.props.minorStepSize ? this.props.minorStepSize : intOnly ? 1 : SafeNumericInput.minorStepSize} onValueChange={this.safeHandleValueChanged} />;
+        // return <NumericInput {...otherProps} asyncControl={true} minorStepSize={this.props.minorStepSize ? this.props.minorStepSize : intOnly ? 1 : SafeNumericInput.minorStepSize} onValueChange={this.safeHandleValueChanged} onBlur={this.handleOnBlur} />;
+        return (
+            <NumericInput
+                {...otherProps}
+                asyncControl={true}
+                minorStepSize={this.props.minorStepSize ? this.props.minorStepSize : intOnly ? 1 : SafeNumericInput.minorStepSize}
+                onValueChange={this.safeHandleValueChanged}
+                onKeyDown={this.handleChange}
+                onBlur={this.handleOnBlur}
+            />
+        );
     }
 }
