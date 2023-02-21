99c2b2d6745b7b3081640fab06bd8997f994623c
I-Chenn <icchen@asiaa.sinica.edu.tw>
2023-02-21 20:23:37 +0800

    Syntax tweak

--- src/components/Dialogs/ContourDialog/ContourDialogComponent.tsx
+++ src/components/Dialogs/ContourDialog/ContourDialogComponent.tsx
@@ -223,7 +223,7 @@ export class ContourDialogComponent extends React.Component {
 
     private handleGraphClicked = (x: number) => {
         this.levels.push(x);
-        this.levels = this.levels.sort();
+        this.levels.sort();
     };
 
     private handleGraphRightClicked = (x: number) => {
