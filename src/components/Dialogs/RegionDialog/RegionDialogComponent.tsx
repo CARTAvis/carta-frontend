76923540aa8efc2e4bd6f21add36cff6f114565e
yuhsuan <r07222045@ntu.edu.tw>
2023-03-27 15:31:15 +0800

    add tabs

--- src/components/Dialogs/RegionDialog/RegionDialogComponent.tsx
+++ src/components/Dialogs/RegionDialog/RegionDialogComponent.tsx
@@ -1,7 +1,8 @@
 import * as React from "react";
-import {AnchorButton, Classes, FormGroup, HTMLSelect, IDialogProps, Intent, NonIdealState, Switch} from "@blueprintjs/core";
+import {AnchorButton, Classes, FormGroup, HTMLSelect, IDialogProps, Intent, NonIdealState, Switch, Tab, Tabs} from "@blueprintjs/core";
 import {Tooltip2} from "@blueprintjs/popover2";
 import {CARTA} from "carta-protobuf";
+import { action, makeObservable, observable } from "mobx";
 import {observer} from "mobx-react";
 
 import {DraggableDialogComponent} from "components/Dialogs";
@@ -20,8 +21,24 @@ import {RectangularRegionForm} from "./RectangularRegionForm/RectangularRegionFo
 
 import "./RegionDialogComponent.scss";
 
+enum RegionDialogTabs {
+    Configuration,
+    Styling
+}
+
 @observer
 export class RegionDialogComponent extends React.Component {
+    @observable selectedTab: RegionDialogTabs = RegionDialogTabs.Configuration;
+
+    @action private setSelectedTab = (tab: RegionDialogTabs) => {
+        this.selectedTab = tab;
+    };
+
+    constructor(props) {
+        super(props);
+        makeObservable(this);
+    }
+    
     private static readonly MissingRegionNode = (<NonIdealState icon={"folder-open"} title={"No region selected"} description={"Select a region using the list or image view"} />);
     private static readonly InvalidRegionNode = (<NonIdealState icon={"error"} title={"Region not supported"} description={"The selected region does not have any editable properties"} />);
 
@@ -77,6 +94,7 @@ export class RegionDialogComponent extends React.Component {
         };
 
         let bodyContent;
+        let configurationPanel;
         let region: RegionStore;
         let editableRegion = false;
         if (!appStore.activeFrame || !appStore.activeFrame.regionSet.selectedRegion) {
@@ -90,7 +108,7 @@ export class RegionDialogComponent extends React.Component {
             switch (region.regionType) {
                 case CARTA.RegionType.POINT:
                 case CARTA.RegionType.ANNPOINT:
-                    bodyContent = (
+                    configurationPanel = (
                         <React.Fragment>
                             <AppearanceForm region={region} darkTheme={appStore.darkTheme}>
                                 {region.regionType === CARTA.RegionType.ANNPOINT && (
@@ -119,7 +137,7 @@ export class RegionDialogComponent extends React.Component {
                 case CARTA.RegionType.RECTANGLE:
                 case CARTA.RegionType.ANNRECTANGLE:
                 case CARTA.RegionType.ANNTEXT:
-                    bodyContent = (
+                    configurationPanel = (
                         <React.Fragment>
                             <AppearanceForm region={region} darkTheme={appStore.darkTheme} />
                             <RectangularRegionForm region={region} frame={frame} wcsInfo={frame.validWcs ? frame.wcsInfoForTransformation : 0} />
@@ -129,7 +147,7 @@ export class RegionDialogComponent extends React.Component {
                     break;
                 case CARTA.RegionType.ELLIPSE:
                 case CARTA.RegionType.ANNELLIPSE:
-                    bodyContent = (
+                    configurationPanel = (
                         <React.Fragment>
                             <AppearanceForm region={region} darkTheme={appStore.darkTheme} />
                             <EllipticalRegionForm region={region} frame={frame} wcsInfo={frame.validWcs ? frame.wcsInfoForTransformation : 0} />
@@ -141,7 +159,7 @@ export class RegionDialogComponent extends React.Component {
                 case CARTA.RegionType.POLYLINE:
                 case CARTA.RegionType.ANNPOLYGON:
                 case CARTA.RegionType.ANNPOLYLINE:
-                    bodyContent = (
+                    configurationPanel = (
                         <React.Fragment>
                             <AppearanceForm region={region} darkTheme={appStore.darkTheme} />
                             <PolygonRegionForm region={region} wcsInfo={frame.validWcs ? frame.wcsInfoForTransformation : 0} />
@@ -152,7 +170,7 @@ export class RegionDialogComponent extends React.Component {
                 case CARTA.RegionType.LINE:
                 case CARTA.RegionType.ANNLINE:
                 case CARTA.RegionType.ANNVECTOR:
-                    bodyContent = (
+                    configurationPanel = (
                         <React.Fragment>
                             <AppearanceForm region={region} darkTheme={appStore.darkTheme}>
                                 {region.regionType === CARTA.RegionType.ANNVECTOR && (
@@ -187,7 +205,7 @@ export class RegionDialogComponent extends React.Component {
                     break;
                 case CARTA.RegionType.ANNCOMPASS:
                 case CARTA.RegionType.ANNRULER:
-                    bodyContent = (
+                    configurationPanel = (
                         <React.Fragment>
                             <AppearanceForm region={region} darkTheme={appStore.darkTheme}>
                                 {region.regionType === CARTA.RegionType.ANNCOMPASS && (
@@ -273,6 +291,14 @@ export class RegionDialogComponent extends React.Component {
                     break;
                 default:
                     bodyContent = RegionDialogComponent.InvalidRegionNode;
+            }
+            if (editableRegion) {
+                bodyContent = (
+                    <Tabs id="regionDialogTabs" selectedTabId={this.selectedTab} onChange={this.setSelectedTab}>
+                        <Tab id={RegionDialogTabs.Configuration} title="Configuration" panel={configurationPanel} />
+                        <Tab id={RegionDialogTabs.Styling} title="Styling" panel={<div>styling placeholder</div>} />
+                    </Tabs>
+                );
             }
         }
 
