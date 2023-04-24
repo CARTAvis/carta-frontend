c2ac3af8e8334c132b76ff2089c63f331c7375ae
yuhsuan <43841102+YuHsuan-Hwang@users.noreply.github.com>
2023-04-24 16:01:47 +0800

    Yuhsuan/2114 splatalogue backend query (#2126)
    * implement new query
    
    * fix limit bug
    
    * set headers
    
    * store line data
    
    * adjust line data
    
    * set shift freq
    
    * remove html tag
    
    * remove old headers
    
    * remove alive check; fix err alert
    
    * minor change; reformat
    
    * rename func; fix query param; add tests
    
    * reformat
    
    * ignore the last data
    
    * avoid negative data length; fix test
    
    * filter data instead of remove last line
    
    * improve parsing
    
    * minor change
    
    * fix eslint
    
    * remove html tags in resolved QN column
    
    * remove html tags in chemical name column
    
    * compare and swap freq min and max
    
    * fix filtering data; add tests
    
    * reformat
    
    * convert html entities
    
    * update changelog
    
    * fix changelog typo

--- src/services/SplatalogueService.ts
+++ src/services/SplatalogueService.ts
@@ -1,6 +1,8 @@
 import axios, {AxiosInstance} from "axios";
 import {CARTA} from "carta-protobuf";
 
+import {SpectralLineHeaders} from "stores/Widgets";
+
 export interface SpectralLineResponse {
     headers: CARTA.ICatalogHeader[];
     spectralLineData: {[key: string]: CARTA.IColumnData};
@@ -8,50 +10,51 @@ export interface SpectralLineResponse {
 }
 
 export class SplatalogueService {
-    private static BaseUrl = "https://splatalogue.online";
+    private static BaseUrl = "https://almahd-staging.cv.nrao.edu/splata-slap/advanceded/false/";
     private readonly axiosInstance: AxiosInstance;
 
-    private static SplatalogueHeaders: string[] = [
-        "Species",
-        "Chemical Name",
-        "Freq-MHz(rest frame,redshifted)",
-        "Freq Err(rest frame,redshifted)",
-        "Meas Freq-MHz(rest frame,redshifted)",
-        "Meas Freq Err(rest frame,redshifted)",
-        "Resolved QNs",
-        "Unresolved Quantum Numbers",
-        "CDMS/JPL Intensity",
-        "S<sub>ij</sub>&#956;<sup>2</sup> (D<sup>2</sup>)",
-        "S<sub>ij</sub>",
-        "Log<sub>10</sub> (A<sub>ij</sub>)",
-        "Lovas/AST Intensity",
-        "E_L (cm^-1)",
-        "E_L (K)",
-        "E_U (cm^-1)",
-        "E_U (K)",
-        "Linelist"
-    ];
+    private static SplatalogueHeaderTypeMap = new Map<SpectralLineHeaders, CARTA.ColumnType>([
+        [SpectralLineHeaders.Species, CARTA.ColumnType.String],
+        [SpectralLineHeaders.ChemicalName, CARTA.ColumnType.String],
+        [SpectralLineHeaders.ShiftedFrequency, CARTA.ColumnType.Double],
+        [SpectralLineHeaders.RestFrequency, CARTA.ColumnType.Double],
+        [SpectralLineHeaders.RestFrequencyErr, CARTA.ColumnType.Double],
+        [SpectralLineHeaders.MeasuredFrequency, CARTA.ColumnType.Double],
+        [SpectralLineHeaders.MeasuredFrequencyErr, CARTA.ColumnType.Double],
+        [SpectralLineHeaders.ResolvedQN, CARTA.ColumnType.String],
+        [SpectralLineHeaders.UnresolvedQN, CARTA.ColumnType.String],
+        [SpectralLineHeaders.IntensityCDMS, CARTA.ColumnType.Double],
+        [SpectralLineHeaders.IntensitySijm2, CARTA.ColumnType.Double],
+        [SpectralLineHeaders.IntensitySij, CARTA.ColumnType.Double],
+        [SpectralLineHeaders.IntensityAij, CARTA.ColumnType.Double],
+        [SpectralLineHeaders.IntensityLovas, CARTA.ColumnType.Double],
+        [SpectralLineHeaders.EnergyLowerCM, CARTA.ColumnType.Double],
+        [SpectralLineHeaders.EnergyLowerK, CARTA.ColumnType.Double],
+        [SpectralLineHeaders.EnergyUpperCM, CARTA.ColumnType.Double],
+        [SpectralLineHeaders.EnergyUpperK, CARTA.ColumnType.Double],
+        [SpectralLineHeaders.LineList, CARTA.ColumnType.String]
+    ]);
 
-    private static SplatalogueHeaderTypeMap = new Map<string, CARTA.ColumnType>([
-        ["Species", CARTA.ColumnType.String],
-        ["Chemical Name", CARTA.ColumnType.String],
-        ["Shifted Frequency", CARTA.ColumnType.Double],
-        ["Freq-MHz(rest frame,redshifted)", CARTA.ColumnType.Double],
-        ["Freq Err(rest frame,redshifted)", CARTA.ColumnType.Double],
-        ["Meas Freq-MHz(rest frame,redshifted)", CARTA.ColumnType.Double],
-        ["Meas Freq Err(rest frame,redshifted)", CARTA.ColumnType.Double],
-        ["Resolved QNs", CARTA.ColumnType.String],
-        ["Unresolved Quantum Numbers", CARTA.ColumnType.String],
-        ["CDMS/JPL Intensity", CARTA.ColumnType.Double],
-        ["S<sub>ij</sub>&#956;<sup>2</sup> (D<sup>2</sup>)", CARTA.ColumnType.Double],
-        ["S<sub>ij</sub>", CARTA.ColumnType.Double],
-        ["Log<sub>10</sub> (A<sub>ij</sub>)", CARTA.ColumnType.Double],
-        ["Lovas/AST Intensity", CARTA.ColumnType.Double],
-        ["E_L (cm^-1)", CARTA.ColumnType.Double],
-        ["E_L (K)", CARTA.ColumnType.Double],
-        ["E_U (cm^-1)", CARTA.ColumnType.Double],
-        ["E_U (K)", CARTA.ColumnType.Double],
-        ["Linelist", CARTA.ColumnType.String]
+    private static SplatalogueHeaderStringMap = new Map<SpectralLineHeaders, string>([
+        [SpectralLineHeaders.Species, "name"],
+        [SpectralLineHeaders.ChemicalName, "chemical_name"],
+        [SpectralLineHeaders.ShiftedFrequency, "orderedFreq"],
+        [SpectralLineHeaders.RestFrequency, "orderedFreq"],
+        [SpectralLineHeaders.RestFrequencyErr, "orderedFreq"],
+        [SpectralLineHeaders.MeasuredFrequency, "measFreq"],
+        [SpectralLineHeaders.MeasuredFrequencyErr, "measFreq"],
+        [SpectralLineHeaders.ResolvedQN, "resolved_QNs"],
+        [SpectralLineHeaders.UnresolvedQN, "unres_quantum_numbers"],
+        [SpectralLineHeaders.IntensityCDMS, "intintensity"],
+        [SpectralLineHeaders.IntensitySijm2, "sijmu2"],
+        [SpectralLineHeaders.IntensitySij, "sij"],
+        [SpectralLineHeaders.IntensityAij, "aij"],
+        [SpectralLineHeaders.IntensityLovas, "LovasASTIntensity"],
+        [SpectralLineHeaders.EnergyLowerCM, "lower_state_energy"],
+        [SpectralLineHeaders.EnergyLowerK, "lower_state_energy_K"],
+        [SpectralLineHeaders.EnergyUpperCM, "upper_state_energy"],
+        [SpectralLineHeaders.EnergyUpperK, "upper_state_energy_K"],
+        [SpectralLineHeaders.LineList, "linelist"]
     ]);
 
     private static staticInstance: SplatalogueService;
@@ -67,139 +70,141 @@ export class SplatalogueService {
         this.axiosInstance = axios.create({baseURL: SplatalogueService.BaseUrl});
     }
 
-    async aliveCheck(): Promise<boolean> {
-        try {
-            const {status} = await this.axiosInstance.head("");
-            return status === 200;
-        } catch (err) {
-            return false;
-        }
-    }
+    query = async (freqMin: number, freqMax: number, intensityLimit?: number): Promise<SpectralLineResponse> => {
+        const params = SplatalogueService.GetParams(freqMin, freqMax, intensityLimit);
+        const response = await this.axiosInstance.post("", {body: JSON.stringify(params)});
+        return SplatalogueService.ConvertTable(response?.data);
+    };
 
-    async query(freqMin: number, freqMax: number, intensityLimit?: number): Promise<SpectralLineResponse> {
-        const url = SplatalogueService.ConstructUrl(freqMin, freqMax, intensityLimit);
-        const response = await this.axiosInstance.get(url);
-        return SplatalogueService.ConvertTable(response.data);
-    }
-
-    private static ConvertTable(tableString: string) {
-        if (!tableString || typeof tableString !== "string") {
+    private static ConvertTable = (data: object[]) => {
+        if (!data) {
             throw new Error("invalid data received from Splatalogue");
         }
 
-        const lines = tableString.split("\n");
-        if (!lines.length) {
-            throw new Error("invalid data received from Splatalogue");
-        }
+        // remove data with invalid species id and invalid species name
+        // this will remove data which only stores the query string
+        data = data.filter(line => line && (line["species_id"] !== null || line["name"]));
 
-        const headerLine = lines[0];
-        const headerEntries = headerLine.split(":");
-        // First row is header, last row might be empty
-        let numDataRows = lines.length - 1;
-        if (lines[lines.length - 1] === "") {
-            numDataRows--;
-        }
-
-        const numColumns = headerEntries.length;
-
-        if (numColumns !== SplatalogueService.SplatalogueHeaders.length) {
-            throw new Error("Unexpected header data received from Splatalogue");
-        }
-
+        const numDataRows = data.length;
         const responseData: SpectralLineResponse = {
             headers: new Array<CARTA.ICatalogHeader>(),
             spectralLineData: {},
             dataSize: numDataRows
         };
 
-        for (let i = 0; i < numColumns; i++) {
-            // ensure headers match expected headers
-            if (headerEntries[i] !== SplatalogueService.SplatalogueHeaders[i]) {
-                throw new Error("Unexpected header data received from Splatalogue");
-            } else {
-                const header: CARTA.ICatalogHeader = {
-                    dataType: SplatalogueService.SplatalogueHeaderTypeMap.get(headerEntries[i]),
-                    name: headerEntries[i],
-                    columnIndex: i
-                };
-                responseData.headers.push(header);
-                responseData.spectralLineData[i] = {
-                    dataType: CARTA.ColumnType.String,
-                    stringData: new Array<string>(numDataRows)
-                };
-            }
-        }
+        let columnIndex = 0;
+        SplatalogueService.SplatalogueHeaderTypeMap.forEach((value, key) => {
+            const header: CARTA.ICatalogHeader = {
+                dataType: value,
+                name: key,
+                columnIndex: columnIndex
+            };
+            responseData.headers.push(header);
+            responseData.spectralLineData[columnIndex] = {
+                dataType: CARTA.ColumnType.String,
+                stringData: new Array<string>(numDataRows)
+            };
+            columnIndex++;
+        });
 
-        for (let i = 1; i <= numDataRows; i++) {
-            const dataEntries = lines[i].split(":");
-            if (dataEntries.length !== numColumns) {
-                console.warn(`Skipping line with ${dataEntries.length} columns`, dataEntries, lines[i]);
-                continue;
-            }
+        for (let i = 0; i < numDataRows; i++) {
+            const line = data[i];
 
-            for (let j = 0; j < numColumns; j++) {
-                const entry = dataEntries[j];
+            let j = 0;
+            SplatalogueService.SplatalogueHeaderStringMap.forEach((value, key) => {
+                let entry = line[value]?.toString() ?? "";
                 const column = responseData.spectralLineData[j];
-                column.stringData[i - 1] = entry;
-            }
-        }
 
-        // Copy rest freq row and shift all the others
-        const restFreqColumn = 2;
-        const shiftedHeaders: CARTA.ICatalogHeader[] = [];
-        const shiftedData = {};
-        let counter = 0;
-        for (let i = 0; i < responseData.headers.length; i++) {
-            if (i === restFreqColumn) {
-                shiftedHeaders.push({
-                    dataType: CARTA.ColumnType.Double,
-                    columnIndex: counter,
-                    name: "Shifted Frequency"
-                });
-                counter++;
-            }
-            const header = responseData.headers[i];
-            header.columnIndex = counter;
-            shiftedHeaders.push(header);
-            counter++;
-        }
-        for (let i = 0; i < responseData.headers.length; i++) {
-            if (i < restFreqColumn) {
-                shiftedData[i] = responseData.spectralLineData[i];
-            } else if (i === restFreqColumn) {
-                shiftedData[i] = responseData.spectralLineData[i];
-                shiftedData[i + 1] = {
-                    dataType: CARTA.ColumnType.String,
-                    stringData: responseData.spectralLineData[i].stringData.slice()
-                };
-            } else {
-                shiftedData[i + 1] = responseData.spectralLineData[i];
-            }
-        }
+                if (key === SpectralLineHeaders.Species || key === SpectralLineHeaders.ChemicalName || key === SpectralLineHeaders.ResolvedQN || key === SpectralLineHeaders.UnresolvedQN) {
+                    // convert html entities to string, and remove html tags
+                    const dom = new DOMParser().parseFromString(entry, "text/html");
+                    entry = dom.documentElement.textContent;
+                } else if (key === SpectralLineHeaders.RestFrequencyErr || key === SpectralLineHeaders.MeasuredFrequencyErr) {
+                    entry = entry?.match(/\((.*?)\)/)?.[1] ?? ""; // match the string between the first "(" and ")"
+                } else if (key === SpectralLineHeaders.ShiftedFrequency || key === SpectralLineHeaders.RestFrequency || key === SpectralLineHeaders.MeasuredFrequency) {
+                    entry = entry?.match(/^(^[\d.]+)/)?.[1] ?? ""; // match the string before the first char that is not a number or "."
+                }
 
-        responseData.headers = shiftedHeaders;
-        responseData.spectralLineData = shiftedData;
+                column.stringData[i] = entry;
+                j++;
+            });
+        }
 
         return responseData;
-    }
+    };
 
-    // This function is adapted quite strictly from the backend version, and should probably be
-    // cleaned up at a later stage to make it flexible and more readable.
-    private static ConstructUrl(freqMin: number, freqMax: number, intensityLimit?: number) {
-        const base = "/c_export.php?&sid%5B%5D=&data_version=v3.0&lill=on";
-        const intensityLimitString = isFinite(intensityLimit) ? `&lill_cdms_jpl=${intensityLimit === 0 ? 0.000001 : intensityLimit}` : "";
-        const lineListParameters =
-            "&displayJPL=displayJPL&displayCDMS=displayCDMS&displayLovas=displayLovas" +
-            "&displaySLAIM=displaySLAIM&displayToyaMA=displayToyaMA&displayOSU=displayOSU" +
-            "&displayRecomb=displayRecomb&displayLisa=displayLisa&displayRFI=displayRFI";
-        const lineStrengthParameters = "&ls1=ls1&ls2=ls2&ls3=ls3&ls4=ls4&ls5=ls5";
-        const energyLevelParameters = "&el1=el1&el2=el2&el3=el3&el4=el4";
-        const miscellaneousParameters = "&show_unres_qn=show_unres_qn&submit=Export&export_type=current&export_delimiter=colon&offset=0&limit=100000&range=on";
+    private static GetParams = (freqMin: number, freqMax: number, intensityLimit?: number): object => {
+        if (freqMin > freqMax) {
+            [freqMin, freqMax] = [freqMax, freqMin];
+        }
+        let freqFrom = new Array(20).fill("");
+        freqFrom[0] = freqMin.toString();
+        let freqTo = new Array(20).fill("");
+        freqTo[0] = freqMax.toString();
+        const unit = "MHz";
 
-        // workaround to fix splatalogue frequency range parameter bug
-        const freqMinString = freqMin === Math.floor(freqMin) ? freqMin.toFixed(1) : freqMin.toString();
-        const freqMaxString = freqMax === Math.floor(freqMax) ? freqMax.toFixed(1) : freqMax.toString();
-        const frequencyRangeString = `&frequency_units=MHz&from=${freqMinString}&to=${freqMaxString}`;
-        return base + intensityLimitString + lineListParameters + lineStrengthParameters + energyLevelParameters + miscellaneousParameters + frequencyRangeString;
-    }
+        const intensityLimitEnabled = isFinite(intensityLimit);
+        // use 0.000001 instead of 0 to avoid issues from the catalog itself
+        const limit = intensityLimitEnabled ? (intensityLimit === 0 ? 0.000001 : intensityLimit) : 0;
+
+        return {
+            searchSpecies: "",
+            speciesSelectBox: [""],
+            dataVersion: "v3.0",
+            userInputFrequenciesFrom: freqFrom,
+            userInputFrequenciesTo: freqTo,
+            userInputFrequenciesUnit: unit,
+            frequencyRedshift: 0,
+            energyFrom: 0,
+            energyTo: 0,
+            energyRangeType: "el_cm-1",
+            lineIntensity: intensityLimitEnabled ? "CDMS/JPL (log)" : "None",
+            lineIntensityLowerLimit: limit,
+            excludeAtmosSpecies: false,
+            excludePotentialInterstellarSpecies: false,
+            excludeProbableInterstellarSpecies: false,
+            excludeKnownASTSpecies: false,
+            showOnlyAstronomicallyObservedTransitions: false,
+            showOnlyNRAORecommendedFrequencies: false,
+            lineListDisplayJPL: true,
+            lineListDisplayCDMS: true,
+            lineListDisplayLovasNIST: !intensityLimitEnabled,
+            lineListDisplaySLAIM: !intensityLimitEnabled,
+            lineListDisplayToyaMA: !intensityLimitEnabled,
+            lineListDisplayOSU: !intensityLimitEnabled,
+            lineListDisplayRecombination: !intensityLimitEnabled,
+            lineListDisplayTopModel: !intensityLimitEnabled,
+            lineListDisplayRFI: !intensityLimitEnabled,
+            lineStrengthDisplayCDMSJPL: true,
+            lineStrengthDisplaySijMu2: true,
+            lineStrengthDisplaySij: true,
+            lineStrengthDisplayAij: true,
+            lineStrengthDisplayLovasAST: true,
+            energyLevelOne: true,
+            energyLevelTwo: false,
+            energyLevelThree: false,
+            energyLevelFour: false,
+            displayObservedTransitions: false,
+            displayG358MaserTransitions: false,
+            displayObservationReference: false,
+            displayObservationSource: false,
+            displayTelescopeLovasNIST: false,
+            frequencyErrorLimit: false,
+            displayHFSIntensity: false,
+            displayUnresolvedQuantumNumbers: true,
+            displayUpperStateDegeneracy: false,
+            displayMoleculeTag: false,
+            displayQuantumNumberCode: false,
+            displayLabRef: false,
+            displayOrderedFrequencyOnly: false,
+            displayNRAORecommendedFrequencies: false,
+            displayUniqueSpeciesTag: false,
+            displayUniqueLineIDNumber: false,
+            exportType: "current",
+            exportDelimiter: "tab",
+            exportLimit: "allRecords",
+            exportStart: 1,
+            exportStop: 250
+        };
+    };
 }
