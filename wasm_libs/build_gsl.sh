5d1546813db4b198408f6559f75d9e2107d49bbc
Anthony Moraghan <ajm@asiaa.sinica.edu.tw>
2023-04-25 14:46:45 +0800

    ajm/build libs update (#2143)
    * improve robustness of the lib download scripts
    
    * use local copies of the library source code
    
    * temporary debugging
    
    * temporary debugging - specify bash shell
    
    * temporary debugging 3
    
    * removing debugging lines
    
    * adding additional exit 1
    
    * adding two different fallback mirrors to download gsl
    
    * adding a few more specific mirrors for gsl download

--- wasm_libs/build_gsl.sh
+++ wasm_libs/build_gsl.sh
@@ -3,7 +3,30 @@ command -v emcc >/dev/null 2>&1 || { echo "Script requires emcc but it's not ins
 cd "${0%/*}"
 if ! [[ $(find gsl-2.6.tar.gz -type f 2>/dev/null && md5sum -c gsl.md5 &>/dev/null) ]]; then
     echo "Fetching GSL 2.6"
-    wget http://ftpmirror.gnu.org/gsl/gsl-2.6.tar.gz
+    retry_count=0
+    max_retries=2
+    while (( retry_count < max_retries )); do
+        wget http://ftpmirror.gnu.org/gsl/gsl-2.6.tar.gz && break
+        ((retry_count++))
+        echo "Download failed. Trying again."
+        sleep 10
+    done
+    if (( retry_count == max_retries )); then
+        echo "Failed to fetch GSL 2.6 from http://ftpmirror.gnu.org"
+        if ! wget https://mirror.ossplanet.net/gnu/gsl/gsl-2.6.tar.gz; then
+            echo "Failed to fetch GSL 2.6 from https://mirror.ossplanet.net"
+            if ! wget https://ftp.jaist.ac.jp/pub/GNU/gsl/gsl-2.6.tar.gz; then
+                echo "Failed to fetch GSL 2.6 from https://ftp.jaist.ac.jp" 
+                if ! wget https://mirrors.ocf.berkeley.edu/gnu/gsl/gsl-2.6.tar.gz; then
+                    echo "Failed to fetch GSL 2.6 from https://mirrors.ocf.berkeley.edu"
+                     if ! wget https://mirror.dogado.de/gnu/gsl/gsl-2.6.tar.gz; then
+                         echo "Failed to fetch GSL 2.6 from https://mirror.dogado.de"
+                         exit 1
+                     fi
+                fi
+            fi
+        fi
+    fi
 fi
 
 mkdir -p gsl; tar -xf gsl-2.6.tar.gz --directory ./gsl --strip-components=1
@@ -19,4 +42,5 @@ if [[ $(find -L ../built/lib/libgsl.a -type f -size +192000c 2>/dev/null) ]]; th
     echo "Found"
 else
     echo "Not found!"
+    exit 1
 fi
