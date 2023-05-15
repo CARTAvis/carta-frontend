3ea5331ad3aef393398797ce90e257c8901735cb
crocka <raulomar@ualberta.ca>
2023-05-15 01:06:47 -0400

    Changing hard coded pixel value

--- src/components/ImageView/ImageViewComponent.tsx
+++ src/components/ImageView/ImageViewComponent.tsx
@@ -227,7 +227,7 @@ export class ImageViewComponent extends React.Component<WidgetProps> {
             <ReactResizeDetector handleWidth handleHeight onResize={this.onResize} refreshMode={"throttle"} refreshRate={33}>
                 <div
                     className="image-view-div"
-                    style={{gridTemplateColumns: `repeat(${appStore.numImageColumns}, ${appStore.overlayStore.viewWidth}px)`, gridTemplateRows: `repeat(${appStore.numImageRows}, ${appStore.overlayStore.viewHeight}px)`}}
+                    style={{gridTemplateColumns: `repeat(${appStore.numImageColumns}, auto)`, gridTemplateRows: `repeat(${appStore.numImageRows}, 1fr)`}}
                 >
                     {divContents}
                 </div>
