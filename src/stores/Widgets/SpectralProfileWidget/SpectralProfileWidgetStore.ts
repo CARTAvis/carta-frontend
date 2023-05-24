70a42cea3b323fcc852297d30f1cda040909b056
yuhsuan <43841102+YuHsuan-Hwang@users.noreply.github.com>
2023-05-24 10:07:48 +0800

    Yuhsuan/2033 beam related unit conversion (#2161)
    * extend beam property from one value to an array
    
    * refactor
    
    * extend freq from one value to an array
    
    * add unit tests
    
    * add unit test for intensityConfig
    
    * add unit test for intensityConfig
    
    * update changelog

--- src/stores/Widgets/SpectralProfileWidget/SpectralProfileWidgetStore.ts
+++ src/stores/Widgets/SpectralProfileWidget/SpectralProfileWidgetStore.ts
@@ -386,11 +386,12 @@ export class SpectralProfileWidgetStore extends RegionWidgetStore {
         const frame = this.effectiveFrame;
         if (frame) {
             let config: IntensityConfig = {nativeIntensityUnit: frame.headerUnit};
-            if (frame.beamProperties) {
-                config["bmaj"] = frame.beamProperties.majorAxis;
-                config["bmin"] = frame.beamProperties.minorAxis;
+            const beams = frame.beamAllChannels;
+            if (beams?.length) {
+                config["bmaj"] = beams.map(b => b?.majorAxis);
+                config["bmin"] = beams.map(b => b?.minorAxis);
                 if (frame.spectralAxis?.type?.code === "FREQ") {
-                    config["freqGHz"] = GetFreqInGHz(frame.spectralAxis.type.unit, frame.spectralAxis.value);
+                    config["freqGHz"] = frame.channelInfo?.values.map(x => GetFreqInGHz(frame.spectralAxis.type.unit, x));
                 }
             }
 
