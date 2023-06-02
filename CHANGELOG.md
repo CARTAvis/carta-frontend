1b43021981d2fb89f5ab02d467fa66010c6b10b6
Carli <94193689+crocka@users.noreply.github.com>
2023-06-01 23:49:57 -0400

    carli/1987_changes_to_tsv_files (#2175)
    * Corrected type
    
    * Replaced spaces in image file name with __
    
    * Removed trailing \n in tsv and txt files.
    
    * Updated according to comment
    
    * Changed to Profile_Fitting_Result_Log
    
    * Updated changelog

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -18,6 +18,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 * Fixed line region anchor rotation degree when the image has non-square pixels ([#1732](https://github.com/CARTAvis/carta-frontend/issues/1732)).
 * Fixed the blank screen when using layout with histogram widget ([#2178](https://github.com/CARTAvis/carta-frontend/issues/2178)).
 * Fixed the problem of resuming LEL images ([#1226](https://github.com/CARTAvis/carta-backend/issues/1226)).
+* Fixed tsv and txt file export naming ([#1987](https://github.com/CARTAvis/carta-frontend/issues/1987)).
 
 ## [4.0.0-beta.1]
 
--- src/utilities/export/export.ts
+++ src/utilities/export/export.ts
@@ -10,26 +10,26 @@ export function getUnixTimestamp() {
 }
 
 export function exportTsvFile(imageName: string, plotName: string, content: string) {
-    const tsvData = `data:text/tab-separated-values;charset=utf-8,${content}\n`;
+    const tsvData = `data:text/tab-separated-values;charset=utf-8,${content}\n`.trim();
     const dataURL = encodeURI(tsvData).replace(/#/g, "%23");
 
     const a = document.createElement("a") as HTMLAnchorElement;
     a.href = dataURL;
 
-    a.download = `${imageName}-${plotName.replace(" ", "-")}-${getTimestamp()}.tsv`;
+    a.download = `${imageName.replaceAll(" ", "__")}-${plotName.replaceAll(" ", "-")}-${getTimestamp()}.tsv`;
     a.dispatchEvent(new MouseEvent("click"));
 
     return null;
 }
 
 export function exportTxtFile(fileName: string, content: string) {
-    const txtData = `data:text/plain;charset=utf-8,${content}\n`;
+    const txtData = `data:text/plain;charset=utf-8,${content}\n`.trim();
     const dataURL = encodeURI(txtData).replace(/#/g, "%23");
 
     const a = document.createElement("a") as HTMLAnchorElement;
     a.href = dataURL;
 
-    a.download = `${fileName}.txt`;
+    a.download = `${fileName.replaceAll(" ", "__")}.txt`;
     a.dispatchEvent(new MouseEvent("click"));
 
     return null;
