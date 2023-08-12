92d7878202744fadb0831753a77b33adc48aded9
Kuo-Song Wang <kswang@asiaa.sinica.edu.tw>
2023-08-12 08:11:35 +0800

    reformat

--- src/components/HelpDrawer/HelpContent/AnimatorHelpContent.tsx
+++ src/components/HelpDrawer/HelpContent/AnimatorHelpContent.tsx
@@ -9,7 +9,9 @@ export const ANIMATOR_HELP_CONTENT = (
             <ImageComponent light={widgetButtonAnimator} dark={widgetButtonAnimator_d} width="90%" />
         </p>
         <p>
-        The Animator widget grants you control of the rendered context of an image cube in the image viewer. This includes the ability to designate which image, channel (if multiple channels are present within the active image file), and polarization component (if the active image file encompasses multiple Stokes parameters) you wish to explore. Moreover, you can have animation playback for any of these selections – image, channel, or polarization – using the <code>Play</code> button. To change the animating reference, the accompanying radio buttons serve as your guide. Various playback modes are available:
+            The Animator widget grants you control of the rendered context of an image cube in the image viewer. This includes the ability to designate which image, channel (if multiple channels are present within the active image file),
+            and polarization component (if the active image file encompasses multiple Stokes parameters) you wish to explore. Moreover, you can have animation playback for any of these selections – image, channel, or polarization – using
+            the <code>Play</code> button. To change the animating reference, the accompanying radio buttons serve as your guide. Various playback modes are available:
         </p>
         <ul>
             <li>
@@ -28,10 +30,11 @@ export const ANIMATOR_HELP_CONTENT = (
         <p>For channel-specific animations, the double slider empowers you to constrain the channel range eligible for animation playback.</p>
         <p>You can specify the desired frames per second (fps) through the frame rate spinbox. Please note that the actual frames per second achievable are contingent upon both the CARTA backend performance and network capabilities.</p>
         <p>
-        Facilitating channel-specific animation playback, a step value (defaulted at 1) can be established using the <code>Step</code> spinbox. By clicking the <code>Frame rate</code> dropdown, you can opt for the "Step" mode and subsequently define the preferred step value via the accompanying spinbox.
+            Facilitating channel-specific animation playback, a step value (defaulted at 1) can be established using the <code>Step</code> spinbox. By clicking the <code>Frame rate</code> dropdown, you can opt for the "Step" mode and
+            subsequently define the preferred step value via the accompanying spinbox.
         </p>
         <p>
-        The polarization slider encompasses a set of Stokes components derived from the image header. Additionally, it encompasses components derived from the Stokes parameters. These include:
+            The polarization slider encompasses a set of Stokes components derived from the image header. Additionally, it encompasses components derived from the Stokes parameters. These include:
             <ul>
                 <li>
                     <code>Ptotal</code>: representing total polarization intensity, this value is computed from Stokes QU or QUV.
@@ -51,7 +54,8 @@ export const ANIMATOR_HELP_CONTENT = (
             </ul>
         </p>
         <p>
-        In order to maintain optimal performance and effective resource utilization, the default behavior entails automatic cessation of animation playback after 5 minutes. However, this parameter can be tailored to your preferences. Simply access the Performance tab within the preferences dialog (File -&gt; Preferences) to adjust this setting. The maximum allowable playback duration stands at 30 minutes.
+            In order to maintain optimal performance and effective resource utilization, the default behavior entails automatic cessation of animation playback after 5 minutes. However, this parameter can be tailored to your preferences.
+            Simply access the Performance tab within the preferences dialog (File -&gt; Preferences) to adjust this setting. The maximum allowable playback duration stands at 30 minutes.
         </p>
     </div>
 );
