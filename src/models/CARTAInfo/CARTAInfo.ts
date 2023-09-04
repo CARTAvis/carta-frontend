dc80d65814074e98846c476e2b961c79faf72971
Anthony Moraghan <ajm@asiaa.sinica.edu.tw>
2023-09-04 16:00:43 +0800

    ajm/copyright year auto (#2244)
    * write copyright year automatically at build time
    
    * needed to run reformat
    
    * changing build_year to year
    
    * forgot to change an instance of build_year
    
    * maybe doesn the imports the wrong way
    
    * ran reformat
    
    * doing it a better way

--- src/models/CARTAInfo/CARTAInfo.ts
+++ src/models/CARTAInfo/CARTAInfo.ts
@@ -5,10 +5,12 @@ const {version} = require("../../../package.json");
 
 const build_date = preval`module.exports = new Date()`;
 const date = moment(build_date).format("D MMM YYYY");
+const year = moment(build_date).year();
 
 export const CARTA_INFO = {
     acronym: "CARTA",
     version,
     date,
+    year,
     fullName: "Cube Analysis and Rendering Tool for Astronomy"
 };
