25694d1492d328b19075af4926cfa9a0e8e70e3a
ajm-asiaa <ajm@asiaa.sinica.edu.tw>
2023-09-04 01:11:15 -0400

    ran reformat

--- src/components/Dialogs/AboutDialog/AboutDialogComponent.tsx
+++ src/components/Dialogs/AboutDialog/AboutDialogComponent.tsx
@@ -3,7 +3,7 @@ import {AnchorButton, Classes, IDialogProps, Intent} from "@blueprintjs/core";
 import {observer} from "mobx-react";
 
 import {DraggableDialogComponent} from "components/Dialogs";
-import {CARTA_INFO,year} from "models";
+import {CARTA_INFO, year} from "models";
 import {DialogStore} from "stores";
 
 import "./AboutDialogComponent.scss";
