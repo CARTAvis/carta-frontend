6f6a445ceb9aa3613f3acbf6da8b6056c10ce45e
yuhsuan <r07222045@ntu.edu.tw>
2023-09-11 16:49:51 +0800

    set ApiLink global

---
+++ docs_website/src/theme/MDXComponents.js
@@ -0,0 +1,8 @@
+import React from "react";
+import MDXComponents from "@theme-original/MDXComponents";
+import ApiLink from "@site/src/components/ApiLink";
+
+export default {
+    ...MDXComponents,
+    ApiLink
+};
