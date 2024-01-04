9cdc04b0ec924720abb170361c75cf8c23bfa430
KuanChou Hou <kchou@asiaa.sinica.edu.tw>
2024-01-04 13:47:01 +0800

    modify unit test

--- src/models/Wcs/WCSMatchingType.test.ts
+++ src/models/Wcs/WCSMatchingType.test.ts
@@ -1,13 +1,17 @@
 import {WCSMatching} from "./WCSMatchingType";
 
-describe("Expected behavior of WCSMatching", () => {
-    test("WCSMatchingType and WCSMatching", () => {
+describe("WCSMatching", () => {
+    test("WCSMatchingType enum elements are power of 2", () => {
         for (let i = 0; i < WCSMatching.MATCHING_NUMBER; i++) {
             expect(WCSMatching.MATCHING_TYPES[i]).toBe(2 ** i);
         }
+    });
 
+    test("the WCSMatching validation", () => {
         expect(WCSMatching.isTypeValid(2 ** WCSMatching.MATCHING_NUMBER + 1)).toBe(false);
+    });
 
+    test("String converts to lower case except the first letter", () => {
         expect(WCSMatching.capitalizeFirstLetter("TESTINPUT")).toBe("Testinput");
     });
 });
