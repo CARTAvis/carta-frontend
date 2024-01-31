f298a0cdcb00ef31610e4fd76dcf0f80a492a90e
yuhsuan <r07222045@ntu.edu.tw>
2024-01-31 15:03:28 +0800

    set up additional compiler

---
+++ tsc-silent.config.js
@@ -0,0 +1,12 @@
+const migratedFiles = [
+    "/"
+];
+
+module.exports = {
+    suppress: [
+        {
+            pathRegExp: `/src/(?!${migratedFiles.join("|")})`,
+            codes: []
+        }
+    ],
+};
