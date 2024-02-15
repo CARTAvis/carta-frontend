6af3a7d8fc2334acfa6c67da611b1a4c852889a0
yuhsuan <r07222045@ntu.edu.tw>
2024-02-15 14:48:39 +0800

    migrate models zoom

--- tsc-silent.config.js
+++ tsc-silent.config.js
@@ -1,30 +1,6 @@
 const migratedFiles = [
     "utilities",
-    "models/AbstractCatalogProfile",
-    "models/AngularSize",
-    "models/CARTAInfo",
-    "models/ChannelInfo",
-    "models/CompressionQuality",
-    "models/ControlMap",
-    "models/Cursor",
-    "models/Event",
-    "models/FileFilterMode",
-    "models/FrameView",
-    "models/Freq",
-    "models/ImagePanelMode",
-    "models/Layout",
-    "models/MomentDefinition",
-    "models/Point2D",
-    "models/PolarizationDefinition",
-    "models/RegionCreationMode",
-    "models/Snippet",
-    "models/Spectral",
-    "models/StatisticsDefinition",
-    "models/Theme",
-    "models/Tile",
-    "models/Transform2D",
-    "models/TypeDefinition",
-    "models/Wcs"
+    "models"
 ];
 
 module.exports = {
