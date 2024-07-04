e46779d47828c72c39147a0f01c8d8c9d9ee4de9
Tien-Hao Chang <tienhaochang703@gmail.com>
2024-07-04 12:40:37 +0800

    fix warning of searching radius in degree for pixel coordinate frames

--- src/stores/CatalogOnlineQuery/CatalogOnlineQueryConfigStore.ts
+++ src/stores/CatalogOnlineQuery/CatalogOnlineQueryConfigStore.ts
@@ -258,7 +258,7 @@ export class CatalogOnlineQueryConfigStore {
 
     @computed get searchRadiusInDegree(): number {
         const activeFrame = this.activeFrame;
-        if (activeFrame) {
+        if (activeFrame?.validWcs && OverlayStore.Instance.global.explicitSystem !== SystemType.Image) {
             const requiredFrameView = activeFrame.requiredFrameView;
             const diagonal1 = this.calculateDistanceFromPixelCoord({x: requiredFrameView.xMax, y: requiredFrameView.yMax}, {x: requiredFrameView.xMin, y: requiredFrameView.yMin}, true);
             const diagonal2 = this.calculateDistanceFromPixelCoord({x: requiredFrameView.xMin, y: requiredFrameView.yMax}, {x: requiredFrameView.xMax, y: requiredFrameView.yMin}, true);
