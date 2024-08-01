6320f41acb648b0519be7fda4b3d57f8a32a26cf
yuhsuan <r07222045@ntu.edu.tw>
2024-08-01 12:38:30 +0800

    skip raster matched frames when applying colormap set

--- src/stores/ColorBlendingStore/ColorBlendingStore.ts
+++ src/stores/ColorBlendingStore/ColorBlendingStore.ts
@@ -162,7 +162,7 @@ export class ColorBlendingStore {
     }
 
     /**
-     * Applies the specified colormap set to the layers.
+     * Applies the specified colormap set to the layers. Frames with raster scaling matching enabled are skipped.
      * - If the colormap set is a single gradient colormap, it interpolates colors along the gradient for each frame.
      * - If the colormap set is a collection of multiple colormaps, it interpolates between the indexes and selects a colormap from the collection to match the number of frames.
      * @param set - The name of the colormap set to apply. Must be a key in the `ColorBlendingStore.ColormapSets` map.
@@ -174,44 +174,47 @@ export class ColorBlendingStore {
             return;
         }
 
+        const rasterUnmatchedFrames = this.frames.filter(f => !f.rasterScalingReference);
+        const frameNum = rasterUnmatchedFrames.length;
+
         if (colormapSetConfig.type === "gradient") {
             const gradient = getColorsForValues(colormapSetConfig.colormap);
             const getHex = (index: number): string => "#" + tinycolor({r: gradient.color[index * 4], g: gradient.color[index * 4 + 1], b: gradient.color[index * 4 + 2], a: gradient.color[index * 4 + 3]}).toHex();
 
-            for (let i = 0; i < this.frames.length; i++) {
+            for (let i = 0; i < frameNum; i++) {
                 let index;
-                if (this.frames.length === 1) {
+                if (frameNum === 1) {
                     index = colormapSetConfig.inverted ? gradient.size - 1 : 0;
                 } else {
-                    index = Math.round(((colormapSetConfig.inverted ? this.frames.length - 1 - i : i) * (gradient.size - 1)) / (this.frames.length - 1));
+                    index = Math.round(((colormapSetConfig.inverted ? frameNum - 1 - i : i) * (gradient.size - 1)) / (frameNum - 1));
                 }
                 const hex = getHex(index);
 
                 let isExistingSingleColor = false;
                 for (const [key, val] of RenderConfigStore.COLOR_MAPS_MONO) {
                     if (val === hex) {
-                        this.frames[i].renderConfig.setColorMap(key);
+                        rasterUnmatchedFrames[i].renderConfig.setColorMap(key);
                         isExistingSingleColor = true;
                         break;
                     }
                 }
 
                 if (!isExistingSingleColor) {
-                    this.frames[i].renderConfig.setCustomHexEnd(hex);
-                    this.frames[i].renderConfig.setColorMap(RenderConfigStore.COLOR_MAPS_CUSTOM);
+                    rasterUnmatchedFrames[i].renderConfig.setCustomHexEnd(hex);
+                    rasterUnmatchedFrames[i].renderConfig.setColorMap(RenderConfigStore.COLOR_MAPS_CUSTOM);
                 }
             }
         } else {
             const colormaps = colormapSetConfig.colormaps;
-            for (let i = 0; i < this.frames.length; i++) {
+            for (let i = 0; i < frameNum; i++) {
                 let index;
-                if (this.frames.length === 1) {
+                if (frameNum === 1) {
                     index = 0;
                 } else {
-                    index = Math.round((i * (colormaps.length - 1)) / (this.frames.length - 1));
+                    index = Math.round((i * (colormaps.length - 1)) / (frameNum - 1));
                 }
 
-                this.frames[i].renderConfig.setColorMap(colormaps[index]);
+                rasterUnmatchedFrames[i].renderConfig.setColorMap(colormaps[index]);
             }
         }
     };
