913080d7def65d9d4ac8c13af72cda195005e622
KuanChou Hou <kchou@asiaa.sinica.edu.tw>
2024-08-16 10:49:54 +0800

    TSdoc

--- src/stores/Frame/RestFreqStore/RestFreqStore.ts
+++ src/stores/Frame/RestFreqStore/RestFreqStore.ts
@@ -32,10 +32,18 @@ export class RestFreqStore {
         this.customRestFreq = defaultRestFreq;
     }
 
+    /**
+     * Customize the rest frequency value. Set unit using {@link setCustomUnit}.
+     * @param val - Value of rest frequency.
+     */
     @action setCustomVal = (val: number) => {
         this.customRestFreq.value = val;
     };
 
+    /**
+     * Set the unit of the rest frequency. Customize the rest frequency value using {@link setCustomVal}.
+     * @param val - Unit of rest frequency in {@link FrequencyUnit}.
+     */
     @action setCustomUnit = (val: FrequencyUnit) => {
         const oriFreq = Freq.convertUnitToHz(this.customRestFreq);
         this.customRestFreq.unit = val;
@@ -44,6 +52,9 @@ export class RestFreqStore {
         this.setCustomVal(this.customRestFreq.value * convertFactor);
     };
 
+    /**
+     * Reset the rest frequency to the value and unit of the header.
+     */
     @action restoreDefaults = () => {
         this.customRestFreq = this.headerRestFreq;
     };
