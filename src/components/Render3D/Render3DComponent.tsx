2215e3aa86feb13de1c18ac36fc927da484c446b
ixakalabadie <ixaka.lab@gmail.com>
2024-09-11 11:28:10 +0200

    create histogram with histogramcomp to select datasource and renderconfig for lineplotprops. NOT WORKING.

--- src/components/Render3D/Render3DComponent.tsx
+++ src/components/Render3D/Render3DComponent.tsx
@@ -1,20 +1,19 @@
 import * as React from "react";
 // import ReactResizeDetector from "react-resize-detector";
-import {Divider, Tab, Tabs} from "@blueprintjs/core";
+import {Divider, FormGroup, HTMLSelect, NonIdealState, Tab, Tabs} from "@blueprintjs/core";
 // import {CARTA} from "carta-protobuf";
-import {computed, makeObservable} from "mobx";
+import * as _ from "lodash";
+import {action, computed, makeObservable, observable} from "mobx";
 import {observer} from "mobx-react"; 
 
 // import {TaskProgressDialogComponent} from "components/Dialogs";
-import {RegionSelectorComponent} from "components/Shared";
-// import {Point2D} from "models";
+import {LinePlotComponent, LinePlotComponentProps} from "components/Shared";
+import {Point2D} from "models";
 import {AppStore, DefaultWidgetConfig, HelpType, WidgetProps, WidgetsStore} from "stores";
 // import {FrameStore} from "stores/Frame";
-import {Render3DWidgetStore} from "stores/Widgets";
+import {RegionId, Render3DWidgetStore} from "stores/Widgets";
+import {clamp, getColorForTheme} from "utilities";
 
-import {IsoSurfaceComponent} from "./IsoSurfaceComponent/IsoSurfaceComponent";
-
-// import {clamp, getColorForTheme} from "utilities";
 // import {MultiPlotProps, TickType} from "../Shared/LinePlot/PlotContainer/PlotContainerComponent";
 import "./Render3DComponent.scss";
 
@@ -45,6 +44,9 @@ export class Render3DComponent extends React.Component<WidgetProps> {
             helpType: HelpType.RENDER_3D
         };
     }
+
+    @observable width: number;
+    @observable height: number;
 
     // find widgetStore for render 3d
     @computed get widgetStore(): Render3DWidgetStore {
@@ -77,14 +79,153 @@ export class Render3DComponent extends React.Component<WidgetProps> {
         }
     }
 
-    render() {
+    // handle changing the data source
+    private handleFrameChanged = (changeEvent: React.ChangeEvent<HTMLSelectElement>) => {
+        if (this.widgetStore.effectiveFrame) {
+            alert("frame changed");
+            const selectedFileId = parseInt(changeEvent.target.value);
+            this.widgetStore.setFileId(selectedFileId);
+            this.widgetStore.setRegionId(this.widgetStore.effectiveFrame.frameInfo.fileId, RegionId.NONE);
+        }
+    };
+
+    private handleRegionChanged = (changeEvent: React.ChangeEvent<HTMLSelectElement>) => {
+        if (this.widgetStore.effectiveFrame) {
+            const fileId = this.widgetStore.effectiveFrame.frameInfo.fileId;
+            this.widgetStore.setFileId(fileId);
+            this.widgetStore.setRegionId(fileId, parseInt(changeEvent.target.value));
+        }
+    };
+
+    @computed get plotData(): {values: Array<Point2D>; xMin: number; xMax: number; yMin: number; yMax: number} {
         const widgetStore = this.widgetStore;
-        
-        // const isoSurfacesLevelsPanel = (
-        //     <div>
-        //         Levels
-        //     </div>
-        // );
+        const frame = widgetStore.effectiveFrame;
+        if (frame && frame.renderConfig.histogram && frame.renderConfig.histogram.bins && frame.renderConfig.histogram.bins.length) {
+            const histogram = frame.renderConfig.histogram;
+            alert("histogram: " + histogram);
+            let minIndex = 0;
+            let maxIndex = histogram.bins.length - 1;
+
+            // Truncate array if zoomed in (sidestepping ChartJS bug with off-canvas rendering and speeding up layout)
+            if (!widgetStore.isAutoScaledX) {
+                minIndex = Math.floor((widgetStore.minX - histogram.firstBinCenter) / histogram.binWidth);
+                minIndex = clamp(minIndex, 0, histogram.bins.length - 1);
+                maxIndex = Math.ceil((widgetStore.maxX - histogram.firstBinCenter) / histogram.binWidth);
+                maxIndex = clamp(maxIndex, 0, histogram.bins.length - 1);
+            }
+
+            let xMin = histogram.firstBinCenter + histogram.binWidth * minIndex;
+            let xMax = histogram.firstBinCenter + histogram.binWidth * maxIndex;
+            let yMin = histogram.bins[minIndex];
+            let yMax = yMin;
+
+            let values: Array<{x: number; y: number}>;
+            const N = maxIndex - minIndex;
+            if (N > 0 && !isNaN(N)) {
+                values = new Array(maxIndex - minIndex);
+
+                for (let i = minIndex; i <= maxIndex; i++) {
+                    values[i - minIndex] = {x: histogram.firstBinCenter + histogram.binWidth * i, y: histogram.bins[i]};
+                    yMin = Math.min(yMin, histogram.bins[i]);
+                    yMax = Math.max(yMax, histogram.bins[i]);
+                }
+            }
+            return {values, xMin, xMax, yMin, yMax};
+        }
+        return null;
+    }
+
+    @action onResize = (width: number, height: number) => {
+        this.width = width;
+        this.height = height;
+    };
+
+    onGraphCursorMoved = _.throttle(x => {
+        this.widgetStore.setCursor(x);
+    }, 100);
+
+    render() {
+        const appStore = AppStore.Instance;
+        const frame = this.widgetStore.effectiveFrame;
+        const fileInfo = frame ? `${appStore.getFrameIndex(frame.frameInfo.fileId)}: ${frame.filename}` : undefined;
+        const regionInfo = this.widgetStore.effectiveRegionInfo;
+
+        let selectedValue = RegionId.NONE;
+        if (this.widgetStore.effectiveFrame?.regionSet) {
+            selectedValue = this.widgetStore.regionIdMap.get(this.widgetStore.effectiveFrame.frameInfo.fileId);
+        }
+
+        // here
+        if (!frame || !this.widgetStore) {
+            return (
+                <div className="isosurface-panel">
+                    <NonIdealState icon={"folder-open"} title={"No file loaded"} description={"Load a file using the menu"} />
+                </div>
+            );
+        }
+
+        let unit = "";
+        if (frame && frame.headerUnit) {
+            unit = frame.headerUnit;
+        }
+
+        let linePlotProps: LinePlotComponentProps = {
+            xLabel: unit ? `Value (${unit})` : "Value",
+            darkMode: appStore.darkTheme,
+            logY: this.widgetStore.logScaleY,
+            plotType: this.widgetStore.plotType,
+            showYAxisTicks: false,
+            showYAxisLabel: false,
+            // graphClicked: this.handleGraphClicked,
+            // graphRightClicked: this.handleGraphRightClicked,
+            graphZoomedX: this.widgetStore.setXBounds,
+            graphZoomedY: this.widgetStore.setYBounds,
+            graphZoomedXY: this.widgetStore.setXYBounds,
+            graphZoomReset: this.widgetStore.clearXYBounds,
+            graphCursorMoved: this.onGraphCursorMoved,
+            scrollZoom: true,
+            borderWidth: this.widgetStore.lineWidth,
+            pointRadius: this.widgetStore.linePlotPointSize,
+            zeroLineWidth: 2
+        }
+
+        if (frame.renderConfig.histogram && frame.renderConfig.histogram.bins && frame.renderConfig.histogram.bins.length) {
+            const currentPlotData = this.plotData;
+            if (currentPlotData) {
+                linePlotProps.data = currentPlotData.values;
+                alert(currentPlotData.values);
+
+                // set line color
+                linePlotProps.lineColor = getColorForTheme(this.widgetStore.primaryLineColor);
+
+                // Determine scale in X and Y directions. If auto-scaling, use the bounds of the current data
+                if (this.widgetStore.isAutoScaledX) {
+                    linePlotProps.xMin = currentPlotData.xMin;
+                    linePlotProps.xMax = currentPlotData.xMax;
+                } else {
+                    linePlotProps.xMin = this.widgetStore.minX;
+                    linePlotProps.xMax = this.widgetStore.maxX;
+                }
+
+                if (this.widgetStore.isAutoScaledY) {
+                    linePlotProps.yMin = currentPlotData.yMin;
+                    linePlotProps.yMax = currentPlotData.yMax;
+                } else {
+                    linePlotProps.yMin = this.widgetStore.minY;
+                    linePlotProps.yMax = this.widgetStore.maxY;
+                }
+                // Fix log plot min bounds for entries with zeros in them
+                if (this.widgetStore.logScaleY && linePlotProps.yMin <= 0) {
+                    linePlotProps.yMin = 0.5;
+                }
+            }
+        }        
+
+        const isoSurfacesLevelsPanel = (
+            <div className="histogram-plot">
+                <LinePlotComponent {...linePlotProps} />
+            </div>
+        );
 
         const isoSurfacesConfigurationPanel = (
             <div>configuration</div>
@@ -97,7 +238,7 @@ export class Render3DComponent extends React.Component<WidgetProps> {
         const isoSurfacesPanel = (
             <div>
                 <Tabs defaultSelectedTabId={IsoSurfaceTabs.Levels} renderActiveTabPanelOnly={false}>
-                    <Tab id={IsoSurfaceTabs.Levels} title="Levels" panel={<IsoSurfaceComponent widgetStore={this.widgetStore}/>} panelClassName="render-3d-isosurfaces-levels-panel" data-testid="render-3d-isosurfaces-levels-tab-title" />
+                    <Tab id={IsoSurfaceTabs.Levels} title="Levels" panel={isoSurfacesLevelsPanel} panelClassName="render-3d-isosurfaces-levels-panel" data-testid="render-3d-isosurfaces-levels-tab-title" />
                     <Tab id={IsoSurfaceTabs.Configuration} title="Configuration" panel={isoSurfacesConfigurationPanel} panelClassName="render-3d-isosurfaces-configuration-panel" data-testid="render-3d-isosurfaces-configuration-tab-title" />
                     <Tab id={IsoSurfaceTabs.Styling} title="Styling" panel={isoSurfacesStylingPanel} panelClassName="render-3d-isosurfaces-styling-panel" data-testid="render-3d-isosurfaces-styling-tab-title" />
                 </Tabs>
@@ -111,10 +252,31 @@ export class Render3DComponent extends React.Component<WidgetProps> {
 
         return (
             <div className="render-3d-widget">
-                <div className="spectral-profiler-toolbar">
-                    <RegionSelectorComponent widgetStore={widgetStore} />
-                </div>
                 <div className="render-3d-panel">
+                <FormGroup
+                    className="label-info-group"
+                    inline={true}
+                    label="Data source"
+                    labelInfo={
+                        <span className="label-info" title={fileInfo}>
+                            {fileInfo ? `(${fileInfo})` : ""}
+                        </span>
+                    }
+                >
+                    <HTMLSelect value={this.widgetStore.fileId} options={this.widgetStore.frameOptions} onChange={this.handleFrameChanged} data-testid="render-3d-image-dropdown" />
+                </FormGroup>
+                <FormGroup
+                    className="label-info-group"
+                    inline={true}
+                    label="Region"
+                    labelInfo={
+                        <span className="label-info" title={regionInfo}>
+                            {regionInfo ? `(${regionInfo})` : ""}
+                        </span>
+                    }
+                >
+                    <HTMLSelect value={selectedValue} options={this.widgetStore.regionOptions} onChange={this.handleRegionChanged} data-testid="render-3d-region-dropdown" />
+                </FormGroup>
                 <Divider />
                 <Tabs defaultSelectedTabId={Render3DTabs.IsoSurfaces} renderActiveTabPanelOnly={false}>
                         <Tab id={Render3DTabs.IsoSurfaces} title="Iso-surfaces" panel={isoSurfacesPanel} panelClassName="render-3d-isosurfaces-panel" data-testid="render-3d-isosurfaces-tab-title" />
--- src/stores/Widgets/Render3DWidgetStore/Render3DWidgetStore.ts
+++ src/stores/Widgets/Render3DWidgetStore/Render3DWidgetStore.ts
@@ -1,20 +1,14 @@
-// import {OptionProps} from "@blueprintjs/core";
-// import {CARTA} from "carta-protobuf";
+import {OptionProps} from "@blueprintjs/core";
+import {CARTA} from "carta-protobuf";
 import {action, computed, makeObservable, observable} from "mobx";
 
 import {LineSettings, PlotType} from "components/Shared";
+import {AppStore} from "stores";
 
-// import {AppStore} from "stores";
-import {RegionsType, RegionWidgetStore} from "../RegionWidgetStore/RegionWidgetStore";
+import {ACTIVE_FILE_ID, RegionId, RegionsType, RegionWidgetStore} from "../RegionWidgetStore/RegionWidgetStore";
 
 export class Render3DWidgetStore extends RegionWidgetStore {
 
-    // Histogram settings
-    @observable logScaleY: boolean;
-    @observable plotType: PlotType;
-    @observable primaryLineColor: string;
-    @observable lineWidth: number;
-    @observable linePlotPointSize: number;
     @observable markerTextVisible: boolean;
     @observable meanRmsVisible: boolean;
     @observable linePlotInitXYBoundaries: {minXVal: number; maxXVal: number; minYVal: number; maxYVal: number};
@@ -24,6 +18,32 @@ export class Render3DWidgetStore extends RegionWidgetStore {
     @observable minY: number | undefined;
     @observable maxY: number | undefined;
     @observable cursorX: number;
+
+    // settings
+    @observable logScaleY: boolean;
+    @observable plotType: PlotType;
+    @observable primaryLineColor: string;
+    @observable lineWidth: number;
+    @observable linePlotPointSize: number;
+
+    @computed get regionOptions(): OptionProps[] {
+        const appStore = AppStore.Instance;
+        let regionOptions: OptionProps[] = [{value: RegionId.NONE, label: "None"}];
+        if (appStore.frames) {
+            const selectedFrame = appStore.getFrame(this.fileId);
+            if (selectedFrame?.regionSet) {
+                const validRegionOptions = selectedFrame.regionSet.regions
+                    ?.filter(r => !r.isTemporary && r.regionType === CARTA.RegionType.RECTANGLE)
+                    ?.map(region => {
+                        return {value: region?.regionId, label: region?.nameString};
+                    });
+                if (validRegionOptions) {
+                    regionOptions = regionOptions.concat(validRegionOptions);
+                }
+            }
+        }
+        return regionOptions;
+    }
 
     @action setXBounds = (minVal: number, maxVal: number) => {
         this.minX = minVal;
@@ -59,6 +79,14 @@ export class Render3DWidgetStore extends RegionWidgetStore {
         this.maxY = undefined;
     };
 
+    @computed get isAutoScaledX() {
+        return this.minX === undefined || this.maxX === undefined;
+    }
+
+    @computed get isAutoScaledY() {
+        return this.minY === undefined || this.maxY === undefined;
+    }
+
     @action setLogScale = (logScale: boolean) => {
         this.logScaleY = logScale;
     };
@@ -74,6 +102,7 @@ export class Render3DWidgetStore extends RegionWidgetStore {
     constructor() {
         super(RegionsType.CLOSED);
         makeObservable(this);
+        this.regionIdMap.set(ACTIVE_FILE_ID, RegionId.NONE);
         this.logScaleY = true;
         this.plotType = PlotType.STEPS;
         this.markerTextVisible = true;
@@ -84,100 +113,25 @@ export class Render3DWidgetStore extends RegionWidgetStore {
         this.linePlotInitXYBoundaries = {minXVal: 0, maxXVal: 0, minYVal: 0, maxYVal: 0};
     } // endconstructor
 
-    @computed get isAutoScaledX() {
-        return this.minX === undefined || this.maxX === undefined;
-    }
-
-    @computed get isAutoScaledY() {
-        return this.minY === undefined || this.maxY === undefined;
-    }
-
-    // settings
-    @action setPrimaryLineColor = (color: string) => {
-        this.primaryLineColor = color;
-    };
-
-    @action setLineWidth = (val: number) => {
-        if (val >= LineSettings.MIN_WIDTH && val <= LineSettings.MAX_WIDTH) {
-            this.lineWidth = val;
-        }
-    };
-
-    @action setLinePlotPointSize = (val: number) => {
-        if (val >= LineSettings.MIN_POINT_SIZE && val <= LineSettings.MAX_POINT_SIZE) {
-            this.linePlotPointSize = val;
-        }
-    };
-
-    @action initXYBoundaries(minXVal: number, maxXVal: number, minYVal: number, maxYVal: number) {
-        this.linePlotInitXYBoundaries = {minXVal: minXVal, maxXVal: maxXVal, minYVal: minYVal, maxYVal: maxYVal};
-    }
-
-    // this is used to create the styling panel in setting (e.g. histogram settings).
-    // import tinycolor from "tinycolor2";
-    // import {isAutoColor} from "utilities";
-    
-    // public init = (widgetSettings): void => {
-    //     if (!widgetSettings) {
-    //         return;
-    //     }
-    //     const lineColor = tinycolor(widgetSettings.primaryLineColor);
-    //     if (lineColor.isValid() || isAutoColor(widgetSettings.primaryLineColor)) {
-    //         this.primaryLineColor = widgetSettings.primaryLineColor;
-    //     }
-    //     if (typeof widgetSettings.lineWidth === "number" && widgetSettings.lineWidth >= LineSettings.MIN_WIDTH && widgetSettings.lineWidth <= LineSettings.MAX_WIDTH) {
-    //         this.lineWidth = widgetSettings.lineWidth;
-    //     }
-    //     if (typeof widgetSettings.linePlotPointSize === "number" && widgetSettings.linePlotPointSize >= LineSettings.MIN_POINT_SIZE && widgetSettings.linePlotPointSize <= LineSettings.MAX_POINT_SIZE) {
-    //         this.linePlotPointSize = widgetSettings.linePlotPointSize;
-    //     }
-    //     if (typeof widgetSettings.logScaleY === "boolean") {
-    //         this.logScaleY = widgetSettings.logScaleY;
-    //     }
-    //     if (typeof widgetSettings.markerTextVisible === "boolean") {
-    //         this.markerTextVisible = widgetSettings.markerTextVisible;
-    //     }
-    //     if (typeof widgetSettings.meanRmsVisible === "boolean") {
-    //         this.meanRmsVisible = widgetSettings.meanRmsVisible;
-    //     }
-    //     if (typeof widgetSettings.plotType === "string" && (widgetSettings.plotType === PlotType.STEPS || widgetSettings.plotType === PlotType.LINES || widgetSettings.plotType === PlotType.POINTS)) {
-    //         this.plotType = widgetSettings.plotType;
-    //     }
-    //     if (typeof widgetSettings.minXVal === "number") {
-    //         this.linePlotInitXYBoundaries.minXVal = widgetSettings.minXVal;
-    //     }
-    //     if (typeof widgetSettings.maxXVal === "number") {
-    //         this.linePlotInitXYBoundaries.maxXVal = widgetSettings.maxXVal;
-    //     }
-    //     if (typeof widgetSettings.minYVal === "number") {
-    //         this.linePlotInitXYBoundaries.minYVal = widgetSettings.minYVal;
-    //     }
-    //     if (typeof widgetSettings.maxYVal === "number") {
-    //         this.linePlotInitXYBoundaries.maxYVal = widgetSettings.maxYVal;
-    //     }
-    // };
-
-    public toConfig = () => {
-        return {
-            primaryLineColor: this.primaryLineColor,
-            lineWidth: this.lineWidth,
-            linePlotPointSize: this.linePlotPointSize,
-            logScaleY: this.logScaleY,
-            markerTextVisible: this.markerTextVisible,
-            meanRmsVisible: this.meanRmsVisible,
-            plotType: this.plotType,
-            minXVal: this.linePlotInitXYBoundaries.minXVal,
-            maxXVal: this.linePlotInitXYBoundaries.maxXVal,
-            minYVal: this.linePlotInitXYBoundaries.minYVal,
-            maxYVal: this.linePlotInitXYBoundaries.maxYVal
+        // settings
+        @action setPrimaryLineColor = (color: string) => {
+            this.primaryLineColor = color;
         };
-    };
-
-    // this is used to create the styling panel in setting (e.g. histogram settings). must add widgetstore in WidgetStore.ts, search for "toConfig"
-    // public toConfig = () => {
-    //     return {
-
-    //     };
-    // };
+    
+        @action setLineWidth = (val: number) => {
+            if (val >= LineSettings.MIN_WIDTH && val <= LineSettings.MAX_WIDTH) {
+                this.lineWidth = val;
+            }
+        };
+    
+        @action setLinePlotPointSize = (val: number) => {
+            if (val >= LineSettings.MIN_POINT_SIZE && val <= LineSettings.MAX_POINT_SIZE) {
+                this.linePlotPointSize = val;
+            }
+        };
+    
+        @action initXYBoundaries(minXVal: number, maxXVal: number, minYVal: number, maxYVal: number) {
+            this.linePlotInitXYBoundaries = {minXVal: minXVal, maxXVal: maxXVal, minYVal: minYVal, maxYVal: maxYVal};
+        }
 
 }
