e77579cb4cb002baaa4f29c5e18a53f870d25677
Kuo-Song Wang <kswang@asiaa.sinica.edu.tw>
2024-10-22 13:59:15 +0800

    testing online manual color theme

--- src/components/HelpDrawer/HelpContent/AnimatorHelpContent.tsx
+++ src/components/HelpDrawer/HelpContent/AnimatorHelpContent.tsx
@@ -1,5 +1,5 @@
 export const ANIMATOR_HELP_CONTENT = (
     <div>
-        <iframe src="https://carta.readthedocs.io/en/latest/catalogue_visualization.html"></iframe>
+        <iframe src="https://carta.readthedocs.io/en/5.0/catalogue_visualization.html"></iframe>
     </div>
 );
