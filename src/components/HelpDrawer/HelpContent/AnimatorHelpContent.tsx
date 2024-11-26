61bc7c23214c6325ac1384a1161d44d0adf66976
Kuo-Song Wang <kswang@asiaa.sinica.edu.tw>
2024-11-26 10:48:31 +0800

    remove in-app help content from codebase

--- src/components/HelpDrawer/HelpContent/AnimatorHelpContent.tsx
+++
@@ -1,5 +0,0 @@
-export const ANIMATOR_HELP_CONTENT = (
-    <div>
-        <iframe src="https://carta.readthedocs.io/en/5.0/catalogue_visualization.html"></iframe>
-    </div>
-);
--- src/components/HelpDrawer/HelpContent/PlaceholderHelpContent.tsx
+++
@@ -1,8 +0,0 @@
-import underConstruction from "static/help/under_construction.png";
-
-export const PLACE_HOLDER_HELP_CONTENT = (
-    <div>
-        <p>To be added.</p>
-        <img src={underConstruction} style={{width: "20%", height: "auto"}} />
-    </div>
-);
