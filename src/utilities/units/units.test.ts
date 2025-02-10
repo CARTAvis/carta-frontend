98c4c0d65e802b7287c9aa66c08348aaf76bd814
yuhsuan <r07222045@ntu.edu.tw>
2025-02-10 11:50:21 +0800

    handle MJy/sr

--- src/utilities/units/units.test.ts
+++ src/utilities/units/units.test.ts
@@ -6,6 +6,7 @@ describe("pixelToFluxDensityUnit", () => {
     it("removes specific substrings from the pixel unit string", () => {
         expect(pixelToFluxDensityUnit("Jy/beam")).toBe("Jy");
         expect(pixelToFluxDensityUnit("Jy/arcsec^2")).toBe("Jy");
+        expect(pixelToFluxDensityUnit("MJy/sr")).toBe("MJy");
     });
 
     it("returns the original string if '/beam' is not present", () => {
