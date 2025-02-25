aea0ac0e47d60640c974d0b181114db787f268bc
yuhsuan <r07222045@ntu.edu.tw>
2025-02-25 14:05:51 +0800

    add rsbuild plugin

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -1,6 +1,9 @@
-import { defineConfig } from '@rsbuild/core';
-import { pluginReact } from '@rsbuild/plugin-react';
+import {defineConfig} from '@rsbuild/core';
+import {pluginReact} from '@rsbuild/plugin-react';
+import {pluginSass} from '@rsbuild/plugin-sass';
+import {pluginNodePolyfill} from "@rsbuild/plugin-node-polyfill";
+import {pluginGlsl} from 'rsbuild-plugin-glsl';
 
 export default defineConfig({
-    plugins: [pluginReact()],
+    plugins: [pluginReact(), pluginSass(), pluginNodePolyfill(), pluginGlsl()],
 });
