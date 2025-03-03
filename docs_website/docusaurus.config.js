ddcd26828749aba30b4cfcd588a88a4c810aabc7
yuhsuan <r07222045@ntu.edu.tw>
2025-03-03 14:45:16 +0800

    update to 5.0.0-beata.1c

--- docs_website/docusaurus.config.js
+++ docs_website/docusaurus.config.js
@@ -62,7 +62,7 @@ const config = {
             ({
                 docs: {
                     versions: {
-                        "5.0.0-beta.1b": {
+                        "5.0.0-beta.1c": {
                             banner: "none",
                         }
                     },
