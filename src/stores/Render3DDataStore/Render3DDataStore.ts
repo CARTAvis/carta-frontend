84cc030dcd71e8f88af6577ed67eaa33328a4811
ixakalabadie <ixaka.lab@gmail.com>
2025-03-25 15:46:10 +0200

    previous commit

--- src/stores/Render3DDataStore/Render3DDataStore.ts
+++ src/stores/Render3DDataStore/Render3DDataStore.ts
@@ -1,49 +1,46 @@
 import { CARTA } from "carta-protobuf";
 import {action, makeObservable, observable} from "mobx";
 
+import { TileService } from "services";
+
 // import { RENDER3D_FILEID } from "stores";
 
 export class Render3DDataStore {
     readonly fileId: number;
     readonly regionId: number;
+    readonly width: number;
+    readonly height: number;
+    readonly depth: number;
     @observable datacube: Float32Array;
 
-    constructor(fileId: number = 0, regionId: number = 0) {
+    constructor(fileId: number = 0, regionId: number = 0, width: number = 0, height:number = 0, depth: number = 0) {
         makeObservable(this);
         this.fileId = fileId;
         this.regionId = regionId;
-        this.datacube = new Float32Array()
+        this.width = width;
+        this.height = height;
+        this.depth = depth;
+        this.datacube = new Float32Array(height * width * depth);
     }
 
-    @action updateRender3DData = (datacube: CARTA.Render3DData) => {
-        this.decompressRender3DData(datacube);
-    }
+    @action updateRender3DData = (render3DData: CARTA.Render3DData) => {
+        if (render3DData.compressionType === CARTA.CompressionType.NONE) {
+            console.log("Render3D data is not compressed");
+            const decompressedData = new Float32Array(render3DData.imageData.buffer.slice(render3DData.imageData.byteOffset, render3DData.imageData.byteOffset + render3DData.imageData.byteLength));
+            console.log('decompresseddata = ', decompressedData);
+            console.log("index = ", this.width * this.height * render3DData.slice);
+            this.setDecompressed3DData(decompressedData, render3DData.slice);
 
-    public decompressRender3DData(render3DData: CARTA.Render3DData) {
-            const compressedArray = render3DData.imageData;
-            // const nanEncodings32 = new Int32Array(render3DData.nanEncodings.slice(0).buffer);
-            let compressedView = new Uint8Array(Math.max(compressedArray.byteLength, render3DData.width * render3DData.height * render3DData.depth * 4));
-            compressedView.set(compressedArray);
-    
-            // const eventArgs = {
-            //     fileId: RENDER3D_FILEID,
-            //     channel: 0,
-            //     stokes: 0,
-            //     width: render3DData.width,
-            //     depth: render3DData.depth,
-            //     subsetHeight: render3DData.height,
-            //     subsetLength: compressedArray.byteLength,
-            //     compression: render3DData.compressionQuality,
-            //     nanEncodings: nanEncodings32,
-            //     tileCoordinate: 0,
-            //     layer: 0,
-            //     requestId: 0,
-            //     viewerId: render3DData.viewerId
-            // };
-    
-            console.log("Decompressing render3D data");
-            // this.workers[0].postMessage(["render3d decompress", compressedView.buffer, eventArgs, render3DData], [compressedView.buffer, nanEncodings32.buffer]);
+        } else if (render3DData.compressionType === CARTA.CompressionType.ZFP) {
+            console.log("Render3D data is compressed with ZFP");
+            // the next function calls setDecompressed3DData which updates the datacube
+            TileService.Instance.decompressRender3DData(render3DData);
         }
-    
+        console.log("datastore array: ", this.datacube);
+    }
+
+    @action setDecompressed3DData = (decompressedData: Float32Array, slice: number) => {
+        this.datacube.set(decompressedData, this.width * this.height * slice);
+    }
     
 }
