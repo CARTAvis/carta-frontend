7ccc80439d1f794f7c6534b9884dd5b864b64854
KuanChou Hou <kchou@asiaa.sinica.edu.tw>
2025-04-29 12:06:42 +0800

    replace " with ” in custom title

--- src/components/ImageView/Overlay/OverlayComponent.tsx
+++ src/components/ImageView/Overlay/OverlayComponent.tsx
@@ -149,9 +149,9 @@ export class OverlayComponent extends React.Component<OverlayComponentProps> {
             }
 
             if (!settings.title.customText) {
-                currentStyleString += `, Title="${this.props.image?.store?.filename.replace(/%/g, "%%%%")}"`;
+                currentStyleString += `, Title="${this.props.image?.store?.filename.replace(/%/g, "%%%%").replace(/"/g, "”")}"`;
             } else if (this.props.image?.store?.titleCustomText?.length) {
-                currentStyleString += `, Title="${this.props.image?.store?.titleCustomText.replace(/%/g, "%%%%")}"`;
+                currentStyleString += `, Title="${this.props.image?.store?.titleCustomText.replace(/%/g, "%%%%").replace(/"/g, "”")}"`;
             } else {
                 currentStyleString += `, Title=${""}`;
             }
