6fc39b5a1a5a2e8fbff4c82dc4d6ca87f2e65445
KuanChou Hou <kchou@asiaa.sinica.edu.tw>
2025-05-29 15:43:37 +0800

    tsc-silent.config

--- tsc-silent.config.js
+++ tsc-silent.config.js
@@ -14,6 +14,7 @@ const migratedFiles = [
     "stores/HelpStore",
     "stores/HipsQueryStore",
     "stores/ImageFittingStore",
+    "stores/ImageViewConfigStore",
     "stores/LayoutStore",
     "stores/LogStore",
     "stores/PreferenceStore",
