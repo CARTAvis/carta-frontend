30d280c783f9ad7046a198791337adecdbc68b50
Zhen-Kai Gao <zkgao@asiaa.sinica.edu.tw>
2025-09-01 16:45:28 +0800

    Wrap GoldenLayout roots with HotkeysProvider and OverlaysProvider to prevent "[Blueprint] useHotkeys() was used outside of a <HotkeysProvider> context" warnings

--- src/index.tsx
+++ src/index.tsx
@@ -27,7 +27,15 @@ FocusStyleManager.onlyShowFocusOnTabs();
 // GoldenLayout requires these in the global namespace
 window["React"] = React; // tslint:disable-line
 // Ensure any roots created by GoldenLayout are wrapped with providers
-window["createRoot"] = createRoot; // tslint:disable-line
+window["createRoot"] = (container: Element | DocumentFragment) => {
+    const root = createRoot(container);
+    const originalRender = root.render.bind(root);
+    root.render = (children: React.ReactNode) => {
+        const wrappedChildren = React.createElement(HotkeysProvider, {renderDialog: () => React.createElement(React.Fragment)}, React.createElement(OverlaysProvider, null, children));
+        originalRender(wrappedChildren);
+    };
+    return root;
+}; // tslint:disable-line
 
 async function fetchConfig() {
     const baseUrl = window.location.href.replace(window.location.search, "").replace("index.html", "");
