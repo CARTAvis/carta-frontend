4c18396d99fde15322cb86cf1ed7fd77f49b2133
KuanChou Hou <kchou@asiaa.sinica.edu.tw>
2025-12-16 15:22:16 +0800

    fix the deprecated options

--- docs_website/docusaurus.config.js
+++ docs_website/docusaurus.config.js
@@ -45,7 +45,12 @@ const config = {
     trailingSlash: false,
 
     onBrokenLinks: "warn",
-    onBrokenMarkdownLinks: "warn",
+
+    markdown: {
+        hooks: {
+            onBrokenMarkdownLinks: "warn"
+        }
+    },
 
     // Even if you don't use internalization, you can use this field to set useful
     // metadata like html lang. For example, if your site is Chinese, you may want
