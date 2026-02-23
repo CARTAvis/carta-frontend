790aff7b1b1908a292a9b501f4a75fae1273eff0
KuanChou Hou <kchou@asiaa.sinica.edu.tw>
2026-02-23 21:27:19 +0800

    fix in app help url

--- src/components/HelpDrawer/HelpDrawerComponent.tsx
+++ src/components/HelpDrawer/HelpDrawerComponent.tsx
@@ -4,12 +4,12 @@ import classNames from "classnames";
 import {observer} from "mobx-react";
 
 import {HelpType} from "enums";
+import {CARTA_INFO} from "models";
 import {AppStore, HelpStore} from "stores";
 
 import "./HelpDrawerComponent.scss";
 
-const url_version = "5.0";
-const url_prefix = `https://carta.readthedocs.io/en/${url_version}`;
+const url_prefix = `https://carta.readthedocs.io/en/${CARTA_INFO.docsVersion}`;
 
 // note for v5-beta release: URLs are not final. The readthedocs user manual needs a major reorganization of topics to support online in-app help
 // dialog URLs
