b02eaec0d288f9a00d7e518c51c66bed98ee57bb
KuanChou Hou <kchou@asiaa.sinica.edu.tw>
2026-03-05 17:18:29 +0800

    fix assetPrefix for public index.html and update package-lock

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -34,7 +34,8 @@ export default defineConfig({
     output: {
         distPath: {
             root: "build"
-        }
+        },
+        assetPrefix: "./"
     },
     server: {
         open: true,
