c426ee9e0383a631f2b3bc0d2804abc212490072
Kuan-Chou Hou <kchou@asiaa.sinica.edu.tw>
2026-03-09 13:26:18 +0800

    fix index.html (#2713)
    * fix index.html
    
    * set assetPrefix to auto

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -35,7 +35,7 @@ export default defineConfig({
         distPath: {
             root: "build"
         },
-        assetPrefix: "./"
+        assetPrefix: "auto"
     },
     server: {
         open: true,
