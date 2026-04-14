b17fca61d5e4aa165988607205c8faacdafd9c8e
KuanChou Hou <kchou@asiaa.sinica.edu.tw>
2026-04-14 11:54:36 +0800

    fix: enable popover dismiss in profiler widgets

--- src/components/SpectralProfiler/SpectralProfilerToolbarComponent/SpectralProfilerToolbarComponent.tsx
+++ src/components/SpectralProfiler/SpectralProfilerToolbarComponent/SpectralProfilerToolbarComponent.tsx
@@ -73,7 +73,7 @@ class ProfileSelectionButtonComponent extends React.Component<ProfileSelectionBu
                                     intent={item.hightlight ? Intent.PRIMARY : Intent.NONE}
                                     onClick={ev => this.props.onItemSelect(item.value, index)}
                                     icon={this.props.itemSelected?.includes(item.value) ? "tick" : "blank"}
-                                    shouldDismissPopover={false}
+                                    shouldDismissPopover={true}
                                     data-testid={"spectral-profiler-" + this.props.categoryName.toLowerCase() + "-dropdown-" + (item.label || "").split(" ").join("-").toLowerCase()}
                                 />
                             ))}
