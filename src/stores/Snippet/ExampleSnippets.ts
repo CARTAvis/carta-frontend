1015b0bcde30f0e1e71e0ce121f78edd79cd6db7
Zhen-Kai Gao <zkgao@asiaa.sinica.edu.tw>
2026-04-21 22:58:56 +0800

    Update method name from `setActiveFrameById` to `setActiveFrameByFileId` in example snippet

--- src/stores/Snippet/ExampleSnippets.ts
+++ src/stores/Snippet/ExampleSnippets.ts
@@ -38,9 +38,9 @@ const file2 = await app.appendFile("testfile2.fits");
 const file3 = await app.appendFile("testfile3.fits");
 
 // The active image can be changed with "setActiveFrame", as well as the wrapper
-// functions "setActiveFrameById" and "setActiveFrameByIndex".
+// functions "setActiveFrameByFileId" and "setActiveFrameByIndex".
 app.setActiveFrameByIndex(0);
-app.setActiveFrameById(file2.frameInfo.fileId);
+app.setActiveFrameByFileId(file2.frameInfo.fileId);
 app.setActiveFrame(file3);
 
 // # Closing images
