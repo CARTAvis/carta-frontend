e5eb3b9ddbeefb528eb7d9c46919d475efcfaef0
KuanChou Hou <kchou@asiaa.sinica.edu.tw>
2026-05-20 12:46:39 +0800

    fix: merge conflict

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -16,7 +16,6 @@ export default [
             parserOptions: {
                 ecmaVersion: "latest",
                 sourceType: "module",
-                project: ["./tsconfig.eslint.json"],
                 project: ["./tsconfig.eslint.json"]
             }
         },
