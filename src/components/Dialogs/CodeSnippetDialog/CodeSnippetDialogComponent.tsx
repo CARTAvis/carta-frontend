a4412021b5fe62029121350e616b1ade7cd1d907
KuanChou Hou <kchou@asiaa.sinica.edu.tw>
2026-05-22 17:14:08 +0800

    refactor: SnippetStore (no change in LogStore)

--- src/components/Dialogs/CodeSnippetDialog/CodeSnippetDialogComponent.tsx
+++ src/components/Dialogs/CodeSnippetDialog/CodeSnippetDialogComponent.tsx
@@ -76,7 +76,7 @@ export class CodeSnippetDialogComponent extends React.Component {
     handleExecuteClicked = async () => {
         const snippetStore = SnippetStore.Instance;
 
-        if (snippetStore.validInput && !snippetStore.isExecuting) {
+        if (snippetStore.isInputValid && !snippetStore.isExecuting) {
             const success = await snippetStore.executeCurrentSnippet();
             if (!success) {
                 AppToaster.show(WarningToast("Error encountered while executing snippet. See JavaScript console for details."));
@@ -162,7 +162,7 @@ export class CodeSnippetDialogComponent extends React.Component {
                 </div>
                 <div className={Classes.DIALOG_FOOTER}>
                     <div className={Classes.DIALOG_FOOTER_ACTIONS}>
-                        <AnchorButton icon="play" intent={Intent.SUCCESS} onClick={this.handleExecuteClicked} disabled={!snippetStore.validInput || snippetStore.isExecuting} text="Execute" />
+                        <AnchorButton icon="play" intent={Intent.SUCCESS} onClick={this.handleExecuteClicked} disabled={!snippetStore.isInputValid || snippetStore.isExecuting} text="Execute" />
                     </div>
                     <div className="spacer" />
                     <div className={Classes.DIALOG_FOOTER_ACTIONS}>
