07d10008a3a632f38fb166c72afb113b090a3e09
KuanChou Hou <kchou@asiaa.sinica.edu.tw>
2026-05-30 21:37:44 +0800

    refactor: replace WIDGET_MAP with getWidgetMap function for passing unit test

--- src/models/Widget/Widget.ts
+++ src/models/Widget/Widget.ts
@@ -59,23 +59,33 @@ export interface WidgetRegistration {
     component: ComponentType<DockedWidgetComponentProps>;
 }
 
-export const WIDGET_MAP = new Map<string, WidgetRegistration>([
-    [ImageViewComponent.WidgetConfig.type, {config: ImageViewComponent.WidgetConfig, settingsConfig: ImageViewSettingsPanelComponent.WidgetConfig, component: ImageViewComponent}],
-    [RenderConfigComponent.WidgetConfig.type, {config: RenderConfigComponent.WidgetConfig, settingsConfig: RenderConfigSettingsPanelComponent.WidgetConfig, component: RenderConfigComponent}],
-    [LayerListComponent.WidgetConfig.type, {config: LayerListComponent.WidgetConfig, settingsConfig: LayerListSettingsPanelComponent.WidgetConfig, component: LayerListComponent}],
-    [LogComponent.WidgetConfig.type, {config: LogComponent.WidgetConfig, component: LogComponent}],
-    [AnimatorComponent.WidgetConfig.type, {config: AnimatorComponent.WidgetConfig, component: AnimatorComponent}],
-    [ChannelMapControlComponent.WidgetConfig.type, {config: ChannelMapControlComponent.WidgetConfig, component: ChannelMapControlComponent}],
-    [SpatialProfilerComponent.WidgetConfig.type, {config: SpatialProfilerComponent.WidgetConfig, settingsConfig: SpatialProfilerSettingsPanelComponent.WidgetConfig, component: SpatialProfilerComponent}],
-    [SpectralProfilerComponent.WidgetConfig.type, {config: SpectralProfilerComponent.WidgetConfig, settingsConfig: SpectralProfilerSettingsPanelComponent.WidgetConfig, component: SpectralProfilerComponent}],
-    [StatsComponent.WidgetConfig.type, {config: StatsComponent.WidgetConfig, component: StatsComponent}],
-    [HistogramComponent.WidgetConfig.type, {config: HistogramComponent.WidgetConfig, settingsConfig: HistogramSettingsPanelComponent.WidgetConfig, component: HistogramComponent}],
-    [RegionListComponent.WidgetConfig.type, {config: RegionListComponent.WidgetConfig, component: RegionListComponent}],
-    [StokesAnalysisComponent.WidgetConfig.type, {config: StokesAnalysisComponent.WidgetConfig, settingsConfig: StokesAnalysisSettingsPanelComponent.WidgetConfig, component: StokesAnalysisComponent}],
-    [CatalogOverlayComponent.WidgetConfig.type, {config: CatalogOverlayComponent.WidgetConfig, settingsConfig: CatalogOverlayPlotSettingsPanelComponent.WidgetConfig, component: CatalogOverlayComponent}],
-    [CatalogPlotComponent.WidgetConfig.type, {config: CatalogPlotComponent.WidgetConfig, component: CatalogPlotComponent}],
-    [SpectralLineQueryComponent.WidgetConfig.type, {config: SpectralLineQueryComponent.WidgetConfig, component: SpectralLineQueryComponent}],
-    [CursorInfoComponent.WidgetConfig.type, {config: CursorInfoComponent.WidgetConfig, component: CursorInfoComponent}],
-    [PvGeneratorComponent.WidgetConfig.type, {config: PvGeneratorComponent.WidgetConfig, settingsConfig: PvPreviewComponent.WidgetConfig, component: PvGeneratorComponent}],
-    [PvPreviewComponent.WidgetConfig.type, {config: PvPreviewComponent.WidgetConfig, component: PvPreviewComponent}]
-]);
+const CreateWidgetMap = () =>
+    new Map<string, WidgetRegistration>([
+        [ImageViewComponent.WidgetConfig.type, {config: ImageViewComponent.WidgetConfig, settingsConfig: ImageViewSettingsPanelComponent.WidgetConfig, component: ImageViewComponent}],
+        [RenderConfigComponent.WidgetConfig.type, {config: RenderConfigComponent.WidgetConfig, settingsConfig: RenderConfigSettingsPanelComponent.WidgetConfig, component: RenderConfigComponent}],
+        [LayerListComponent.WidgetConfig.type, {config: LayerListComponent.WidgetConfig, settingsConfig: LayerListSettingsPanelComponent.WidgetConfig, component: LayerListComponent}],
+        [LogComponent.WidgetConfig.type, {config: LogComponent.WidgetConfig, component: LogComponent}],
+        [AnimatorComponent.WidgetConfig.type, {config: AnimatorComponent.WidgetConfig, component: AnimatorComponent}],
+        [ChannelMapControlComponent.WidgetConfig.type, {config: ChannelMapControlComponent.WidgetConfig, component: ChannelMapControlComponent}],
+        [SpatialProfilerComponent.WidgetConfig.type, {config: SpatialProfilerComponent.WidgetConfig, settingsConfig: SpatialProfilerSettingsPanelComponent.WidgetConfig, component: SpatialProfilerComponent}],
+        [SpectralProfilerComponent.WidgetConfig.type, {config: SpectralProfilerComponent.WidgetConfig, settingsConfig: SpectralProfilerSettingsPanelComponent.WidgetConfig, component: SpectralProfilerComponent}],
+        [StatsComponent.WidgetConfig.type, {config: StatsComponent.WidgetConfig, component: StatsComponent}],
+        [HistogramComponent.WidgetConfig.type, {config: HistogramComponent.WidgetConfig, settingsConfig: HistogramSettingsPanelComponent.WidgetConfig, component: HistogramComponent}],
+        [RegionListComponent.WidgetConfig.type, {config: RegionListComponent.WidgetConfig, component: RegionListComponent}],
+        [StokesAnalysisComponent.WidgetConfig.type, {config: StokesAnalysisComponent.WidgetConfig, settingsConfig: StokesAnalysisSettingsPanelComponent.WidgetConfig, component: StokesAnalysisComponent}],
+        [CatalogOverlayComponent.WidgetConfig.type, {config: CatalogOverlayComponent.WidgetConfig, settingsConfig: CatalogOverlayPlotSettingsPanelComponent.WidgetConfig, component: CatalogOverlayComponent}],
+        [CatalogPlotComponent.WidgetConfig.type, {config: CatalogPlotComponent.WidgetConfig, component: CatalogPlotComponent}],
+        [SpectralLineQueryComponent.WidgetConfig.type, {config: SpectralLineQueryComponent.WidgetConfig, component: SpectralLineQueryComponent}],
+        [CursorInfoComponent.WidgetConfig.type, {config: CursorInfoComponent.WidgetConfig, component: CursorInfoComponent}],
+        [PvGeneratorComponent.WidgetConfig.type, {config: PvGeneratorComponent.WidgetConfig, settingsConfig: PvPreviewComponent.WidgetConfig, component: PvGeneratorComponent}],
+        [PvPreviewComponent.WidgetConfig.type, {config: PvPreviewComponent.WidgetConfig, component: PvPreviewComponent}]
+    ]);
+
+let widgetMap: Map<string, WidgetRegistration> | undefined;
+
+export function getWidgetMap() {
+    if (!widgetMap) {
+        widgetMap = CreateWidgetMap();
+    }
+    return widgetMap;
+}
