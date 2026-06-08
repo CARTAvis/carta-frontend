4815cdb15f9b0b41b12a579c484ee69a58e79c97
Zhen-Kai Gao <zkgao@asiaa.sinica.edu.tw>
2026-06-08 13:55:59 +0800

    refactor: enable TypeScript deprecated API warnings in ESLint configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -115,6 +115,7 @@ export default [
                     format: null,
                 },
             ],
+            "@typescript-eslint/no-deprecated": "warn",
             "@typescript-eslint/no-unused-expressions": "error",
             "@typescript-eslint/consistent-type-imports": [
                 "error",
