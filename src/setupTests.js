f117d9dcedd25fc712335d448d3b94651ef291b6
KuanChou Hou <kchou@asiaa.sinica.edu.tw>
2026-06-18 17:12:08 +0800

    chore: fix naming convention for TextEncoder and TextDecoder polyfill

--- src/setupTests.js
+++ src/setupTests.js
@@ -2,11 +2,13 @@ import "@testing-library/jest-dom";
 import "jest-canvas-mock";
 
 // Polyfill TextEncoder and TextDecoder for jsdom environment
+/* eslint-disable @typescript-eslint/naming-convention */
 if (typeof global.TextEncoder === "undefined") {
-    const {textEncoder, textDecoder} = require("util");
-    global.TextEncoder = textEncoder;
-    global.TextDecoder = textDecoder;
+    const {TextEncoder, TextDecoder} = require("util");
+    global.TextEncoder = TextEncoder;
+    global.TextDecoder = TextDecoder;
 }
+/* eslint-enable @typescript-eslint/naming-convention */
 
 // Set up global mocks
 window.URL.createObjectURL = () => {};
