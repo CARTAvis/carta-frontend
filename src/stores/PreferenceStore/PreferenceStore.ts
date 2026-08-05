364cf4be4a86b7d3cd61691a5cb677f384d574b0
Zhen-Kai Gao <zkgao@asiaa.sinica.edu.tw>
2026-08-05 09:51:57 +0800

    fix(PreferenceStore): remove duplicate PREFERENCES_SCHEMA import

--- src/stores/PreferenceStore/PreferenceStore.ts
+++ src/stores/PreferenceStore/PreferenceStore.ts
@@ -9,8 +9,6 @@ import {getScalingForParameterPreference, getScalingParameterConfig, isSupported
 
 const PREFERENCES_SCHEMA = require("carta-schemas/preferences_schema_2.json");
 
-const PREFERENCES_SCHEMA = require("carta-schemas/preferences_schema_2.json");
-
 const DEFAULTS = {
     SILENT: {
         fileSortingString: "-date",
