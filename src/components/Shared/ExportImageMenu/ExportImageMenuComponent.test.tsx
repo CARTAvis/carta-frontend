a9545cf264f0dcceeb7641493986292de1497954
KuanChou Hou <kchou@asiaa.sinica.edu.tw>
2026-09-11 12:12:40 +0800

    feat(export): improve export controls and hotkeys

--- src/components/Shared/ExportImageMenu/ExportImageMenuComponent.test.tsx
+++ src/components/Shared/ExportImageMenu/ExportImageMenuComponent.test.tsx
@@ -6,50 +6,43 @@ import {AppStore} from "stores";
 import {ExportImageMenuComponent} from "./ExportImageMenuComponent";
 
 describe("ExportImageMenuComponent", () => {
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    let mockModifierString: jest.SpyInstance; // Indirectly used when rendering
     let mockExportImage: jest.SpyInstance;
     let mockExportSvgImage: jest.SpyInstance;
 
     beforeEach(() => {
-        mockModifierString = jest.spyOn(AppStore.prototype, "modifierString", "get").mockImplementation(() => "ctrl + ");
         mockExportImage = jest.spyOn(AppStore.Instance, "exportImage");
         mockExportSvgImage = jest.spyOn(AppStore.Instance, "exportSvgImage");
     });
 
-    test("renders two menu dividers and six menu items", () => {
+    test("renders resolution radios and PNG/SVG buttons", () => {
         render(<ExportImageMenuComponent />);
 
         const separators = screen.getAllByRole("separator");
-        expect(separators).toHaveLength(2);
-        expect(separators[0]).toHaveTextContent(/^PNG$/);
-        expect(separators[1]).toHaveTextContent(/^SVG$/);
+        expect(separators).toHaveLength(1);
+        expect(separators[0]).toHaveTextContent(/^Resolution$/);
 
-        const menuitems = screen.getAllByRole("menuitem");
-        expect(menuitems?.length).toEqual(6);
-        expect(menuitems?.[0]).toHaveTextContent(/^Normal \(100%\)ctrl \+ E$/);
-        expect(menuitems?.[1]).toHaveTextContent(/^High \(200%\)$/);
-        expect(menuitems?.[2]).toHaveTextContent(/^Highest \(400%\)$/);
-        expect(menuitems?.[3]).toHaveTextContent(/^Normal \(100%\)$/);
-        expect(menuitems?.[4]).toHaveTextContent(/^High \(200%\)$/);
-        expect(menuitems?.[5]).toHaveTextContent(/^Highest \(400%\)$/);
+        const radios = screen.getAllByRole("radio");
+        expect(radios).toHaveLength(3);
+        expect(radios[0]).toBeChecked();
+        expect(screen.getAllByRole("button")).toHaveLength(2);
+        expect(screen.getByRole("button", {name: "PNG"})).toBeInTheDocument();
+        expect(screen.getByRole("button", {name: "SVG"})).toBeInTheDocument();
     });
 
     test("calls the expected export method when clicked", () => {
         render(<ExportImageMenuComponent />);
-        const menuitems = screen.getAllByRole("menuitem");
+        const pngButton = screen.getByRole("button", {name: "PNG"});
+        const svgButton = screen.getByRole("button", {name: "SVG"});
 
-        fireEvent.click(menuitems[0]);
+        fireEvent.click(pngButton);
         expect(mockExportImage).toHaveBeenCalledWith(1);
-        fireEvent.click(menuitems[1]);
-        expect(mockExportImage).toHaveBeenCalledWith(2);
-        fireEvent.click(menuitems[2]);
-        expect(mockExportImage).toHaveBeenCalledWith(4);
-        fireEvent.click(menuitems[3]);
+        fireEvent.click(svgButton);
         expect(mockExportSvgImage).toHaveBeenCalledWith(1);
-        fireEvent.click(menuitems[4]);
-        expect(mockExportSvgImage).toHaveBeenCalledWith(2);
-        fireEvent.click(menuitems[5]);
+
+        fireEvent.click(screen.getByRole("radio", {name: "400%"}));
+        fireEvent.click(pngButton);
+        fireEvent.click(svgButton);
+        expect(mockExportImage).toHaveBeenCalledWith(4);
         expect(mockExportSvgImage).toHaveBeenCalledWith(4);
     });
 });
