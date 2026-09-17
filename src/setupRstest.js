bab3884a3aa93469b9203b977bc95c4e821daa92
KuanChou Hou <kchou@asiaa.sinica.edu.tw>
2026-09-17 10:22:35 +0800

    build(test): add Rstest migration foundation

--- src/setupRstest.js
+++ src/setupRstest.js
@@ -1,3 +1,3 @@
 import "@testing-library/jest-dom";
-import "jest-canvas-mock";
+import "rstest-canvas-mock";
 import "./setupTestsBase";
---
+++ src/setupTestsBase.js
@@ -0,0 +1,39 @@
+// Polyfill TextEncoder and TextDecoder for jsdom environment
+/* eslint-disable @typescript-eslint/naming-convention */
+if (typeof global.TextEncoder === "undefined") {
+    const {TextEncoder, TextDecoder} = require("util");
+    global.TextEncoder = TextEncoder;
+    global.TextDecoder = TextDecoder;
+}
+/* eslint-enable @typescript-eslint/naming-convention */
+
+// Set up global mocks
+window.URL.createObjectURL = () => {};
+global.WebGL2RenderingContext = null;
+
+// jsdom doesn't implement WebGL contexts; avoid noisy console.error logs when
+// app code probes for WebGL2 support during unit tests.
+if (typeof HTMLCanvasElement !== "undefined" && HTMLCanvasElement.prototype?.getContext) {
+    const originalGetContext = HTMLCanvasElement.prototype.getContext;
+    HTMLCanvasElement.prototype.getContext = function (type, ...args) {
+        if (type === "webgl2" || type === "experimental-webgl2") {
+            return null;
+        }
+        return originalGetContext.call(this, type, ...args);
+    };
+}
+
+// Mock matchMedia for Blueprint.js components
+Object.defineProperty(window, "matchMedia", {
+    writable: true,
+    value: query => ({
+        matches: false,
+        media: query,
+        onchange: null,
+        addListener: () => {}, // deprecated
+        removeListener: () => {}, // deprecated
+        addEventListener: () => {},
+        removeEventListener: () => {},
+        dispatchEvent: () => {}
+    })
+});
