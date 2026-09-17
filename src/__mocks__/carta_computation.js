64cdd7d70f852834197d0a27701a6c5ff814e19e
KuanChou Hou <kchou@asiaa.sinica.edu.tw>
2026-09-17 10:40:02 +0800

    test(rstest): migrate carta_computation mock

--- src/__mocks__/carta_computation.js
+++ src/__mocks__/carta_computation.js
@@ -1,25 +1,27 @@
 // Mock for carta_computation WASM module
 
+const {rs: RS} = require("@rstest/core");
+
 const MOCK_CARTA_COMPUTATION = {
     onReady: Promise.resolve(),
     ZstdReady: true,
     runtimeInit: true,
 
     // Mock computation functions
-    Decompress: typeof jest !== "undefined" ? jest.fn(() => new Uint8Array([0])) : () => new Uint8Array([0]),
-    Decode: typeof jest !== "undefined" ? jest.fn(() => new Float32Array([0])) : () => new Float32Array([0]),
-    GenerateVertexData: typeof jest !== "undefined" ? jest.fn(() => new Float32Array([0])) : () => new Float32Array([0]),
-    CalculateCatalogSize: typeof jest !== "undefined" ? jest.fn(() => new Float32Array([0])) : () => new Float32Array([0]),
-    CalculateCatalogColor: typeof jest !== "undefined" ? jest.fn(() => new Float32Array([0])) : () => new Float32Array([0]),
-    CalculateCatalogOrientation: typeof jest !== "undefined" ? jest.fn(() => new Float32Array([0])) : () => new Float32Array([0]),
-    ConvertInt64Array: typeof jest !== "undefined" ? jest.fn(() => new Float64Array([0])) : () => new Float64Array([0]),
+    Decompress: typeof RS !== "undefined" ? RS.fn(() => new Uint8Array([0])) : () => new Uint8Array([0]),
+    Decode: typeof RS !== "undefined" ? RS.fn(() => new Float32Array([0])) : () => new Float32Array([0]),
+    GenerateVertexData: typeof RS !== "undefined" ? RS.fn(() => new Float32Array([0])) : () => new Float32Array([0]),
+    CalculateCatalogSize: typeof RS !== "undefined" ? RS.fn(() => new Float32Array([0])) : () => new Float32Array([0]),
+    CalculateCatalogColor: typeof RS !== "undefined" ? RS.fn(() => new Float32Array([0])) : () => new Float32Array([0]),
+    CalculateCatalogOrientation: typeof RS !== "undefined" ? RS.fn(() => new Float32Array([0])) : () => new Float32Array([0]),
+    ConvertInt64Array: typeof RS !== "undefined" ? RS.fn(() => new Float64Array([0])) : () => new Float64Array([0]),
 
     // Mock WASM functions
-    _malloc: typeof jest !== "undefined" ? jest.fn(() => 0) : () => 0,
-    _free: typeof jest !== "undefined" ? jest.fn() : () => {},
-    HEAPU8: {buffer: new ArrayBuffer(8), set: typeof jest !== "undefined" ? jest.fn() : () => {}},
-    HEAPF32: {buffer: new ArrayBuffer(4), set: typeof jest !== "undefined" ? jest.fn() : () => {}},
-    HEAPF64: {buffer: new ArrayBuffer(8), set: typeof jest !== "undefined" ? jest.fn() : () => {}},
+    _malloc: typeof RS !== "undefined" ? RS.fn(() => 0) : () => 0,
+    _free: typeof RS !== "undefined" ? RS.fn() : () => {},
+    HEAPU8: {buffer: new ArrayBuffer(8), set: typeof RS !== "undefined" ? RS.fn() : () => {}},
+    HEAPF32: {buffer: new ArrayBuffer(4), set: typeof RS !== "undefined" ? RS.fn() : () => {}},
+    HEAPF64: {buffer: new ArrayBuffer(8), set: typeof RS !== "undefined" ? RS.fn() : () => {}},
 
     // Mock internal properties
     srcAllocated: 0,
